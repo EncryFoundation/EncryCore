75c9cea0aac6332fdee42e9c9320c0382cb7fce3
Ilya Oskin <ilya.arcadich@gmail.com>
2017-12-15 21:50:32 +0300

    UTXO Storage.

---
+++ src/main/scala/encry/view/mempool/EncryBaseMemoryPool.scala
@@ -0,0 +1,40 @@
+package encry.modifiers.mempool
+
+import scorex.core.{ModifierId, NodeViewComponent}
+
+import scala.util.Try
+
+trait EncryBaseMemoryPool[M <: EncryBaseMemoryPool[M]] extends NodeViewComponent {
+
+  def getById(id: ModifierId): Option[EncryBaseTransaction]
+
+  def contains(id: ModifierId): Boolean
+
+  //get ids from Seq, not presenting in mempool
+  def notIn(ids: Seq[ModifierId]): Seq[ModifierId] = ids.filter(id => !contains(id))
+
+  def getAll(ids: Seq[ModifierId]): Seq[EncryBaseTransaction]
+
+  /**
+    * Method to put a transaction into the memory pool. Validation of tha transactions against
+    * the state is done in NodeVieHolder. This put() method can check whether a transaction is valid
+    * @param tx
+    * @return Success(updatedPool), if transaction successfully added to the pool, Failure(_) otherwise
+    */
+  def put(tx: EncryBaseTransaction): Try[M]
+
+  def put(txs: Iterable[EncryBaseTransaction]): Try[M]
+
+  def putWithoutCheck(txs: Iterable[EncryBaseTransaction]): M
+
+  def remove(tx: EncryBaseTransaction): M
+
+  def take(limit: Int): Iterable[EncryBaseTransaction]
+
+  def filter(txs: Seq[EncryBaseTransaction]): M = filter(t => !txs.exists(_.id sameElements t.id))
+
+  def filter(condition: EncryBaseTransaction => Boolean): M
+
+  def size: Int
+
+}
