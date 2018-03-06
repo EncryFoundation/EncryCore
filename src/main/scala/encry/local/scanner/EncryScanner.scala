140060c2f7258a26b80a7b2fc816b844404f2c49
Ilya Oskin <ilya.arcadich@gmail.com>
2018-03-06 21:16:53 +0300

    Scanning features of EncryScanner implementation.

--- src/main/scala/encry/local/scanner/EncryScanner.scala
+++ src/main/scala/encry/local/scanner/EncryScanner.scala
@@ -10,14 +10,19 @@ import encry.modifiers.state.box.{EncryBaseBox, EncryBox}
 import encry.settings.EncryAppSettings
 import encry.storage.codec.FixLenComplexValueCodec
 import io.iohk.iodb.{ByteArrayWrapper, Store}
-import scorex.core.NodeViewHolder
+import scorex.core.{NodeViewHolder, VersionTag}
 import scorex.core.NodeViewHolder.Subscribe
 import scorex.core.utils.ScorexLogging
 import scorex.crypto.authds.ADKey
 
 import scala.collection.mutable
 
-class EncryScanner(settings: EncryAppSettings,
+case class ScanningResult(indexChunk: Seq[(ByteArrayWrapper, Seq[ADKey])],
+                          toInsert: Seq[EncryBaseBox],
+                          toRemove: Seq[ADKey])
+
+class EncryScanner(val version: VersionTag,
+                   settings: EncryAppSettings,
                    viewHolderRef: ActorRef,
                    indexStore: Store) extends Actor with ScorexLogging {
 
@@ -32,40 +37,46 @@ class EncryScanner(settings: EncryAppSettings,
 
   override def receive: Receive = ???
 
-  private def scanPersistent(mod: EncryPersistentModifier): Unit = {
+  private def scanPersistent(mod: EncryPersistentModifier): EncryScanner = mod match {
+      case block: EncryBlock =>
+        val version = VersionTag @@ block.id
+        updateIndex(version, scanTransactions(block.payload.transactions))
+        new EncryScanner(VersionTag @@ block.id, settings, viewHolderRef, indexStore)
+      case payload: EncryBlockPayload =>
+        val version = VersionTag @@ payload.headerId
+        updateIndex(version, scanTransactions(payload.transactions))
+        new EncryScanner(VersionTag @@ payload.headerId, settings, viewHolderRef, indexStore)
+    }
 
-    def scanTransactions(txs: Seq[EncryBaseTransaction]): Unit = {
-      val boxIdsToRemove = txs.flatMap(_.unlockers.map(_.boxId))
-      val (indexChunk, boxesToInsert) = txs.flatMap(_.newBoxes)
-        .foldLeft(mutable.TreeMap[ByteArrayWrapper, Seq[ADKey]](), Seq[EncryBaseBox]()) { case ((cache, bxs), bx) =>
-          if (!boxIdsToRemove.exists(_.sameElements(bx.id))) {
-            cache.get(keyByProposition(bx.proposition)) match {
-              case Some(ids) => cache.update(keyByProposition(bx.proposition), ids :+ bx.id)
-              case _ => cache.update(keyByProposition(bx.proposition), Seq(bx.id))
-            }
-            cache -> (bxs :+ bx)
-          } else {
-            cache -> bxs
+  private def scanTransactions(txs: Seq[EncryBaseTransaction]): ScanningResult = {
+    val boxIdsToRemove = txs.flatMap(_.unlockers.map(_.boxId))
+    val (indexChunk, boxesToInsert) = txs.flatMap(_.newBoxes)
+      .foldLeft(mutable.TreeMap[ByteArrayWrapper, Seq[ADKey]](), Seq[EncryBaseBox]()) { case ((cache, bxs), bx) =>
+        if (!boxIdsToRemove.exists(_.sameElements(bx.id))) {
+          cache.get(keyByProposition(bx.proposition)) match {
+            case Some(ids) => cache.update(keyByProposition(bx.proposition), ids :+ bx.id)
+            case _ => cache.update(keyByProposition(bx.proposition), Seq(bx.id))
           }
+          cache -> (bxs :+ bx)
+        } else {
+          cache -> bxs
         }
-      updateIndex(indexChunk.toSeq, boxesToInsert, boxIdsToRemove)
-    }
-
-    mod match {
-      case block: EncryBlock => scanTransactions(block.payload.transactions)
-      case payload: EncryBlockPayload => scanTransactions(payload.transactions)
-    }
+      }
+    ScanningResult(indexChunk.toSeq, boxesToInsert, boxIdsToRemove)
   }
 
-  private def updateIndex(indexChunk: Seq[(ByteArrayWrapper, Seq[ADKey])],
-                          toInsert: Seq[EncryBaseBox],
-                          toRemove: Seq[ADKey]): Unit = {
-    val currentIndexes = indexChunk ++ toRemove.foldLeft(Seq[(ByteArrayWrapper, Seq[ADKey])]())((acc, id) =>
+  private def updateIndex(version: VersionTag, sr: ScanningResult): Unit = {
+    val currentIndexes = sr.indexChunk ++ sr.toRemove.foldLeft(Seq[(ByteArrayWrapper, Seq[ADKey])]())((acc, id) =>
       storage.get(keyByBoxId(id)).map(r => acc :+ ByteArrayWrapper(r) -> Seq.empty).getOrElse(acc))
     val indexChunkFinal = currentIndexes.foldLeft(Seq[(ByteArrayWrapper, Seq[ADKey])]()) { case (acc, (pk, ids))  =>
       storage.get(pk).map(r => FixLenComplexValueCodec.parseComplexValue(r, EncryBox.BoxIdSize)
-        .map(bxIds => bxIds.filter(id => !toRemove.exists(_.sameElements(id))) ++ ids)).flatten.getOrElse(acc)
+        .map(bxIds => bxIds.filter(id => !sr.toRemove.exists(_.sameElements(id))) ++ ids)).flatten.getOrElse(acc)
     }
-    // TODO: Storage update.
+    val toInsert = indexChunkFinal.map { case (pk, ids) =>
+      pk -> FixLenComplexValueCodec.toComplexValue(ids)
+    } ++ sr.toInsert.map(bx => keyByBoxId(bx.id) -> ByteArrayWrapper(bx.bytes))
+    val toRemove = sr.toRemove.map(ByteArrayWrapper.apply)
+
+    storage.update(ByteArrayWrapper(version), toRemove, toInsert)
   }
 }
