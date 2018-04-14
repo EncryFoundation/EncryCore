37d46e9e8dd40c8bd3d5839bc9aba5e0110ab654
Ilya Oskin <ilya.arcadich@gmail.com>
2018-04-14 17:25:41 +0300

    Command refactoring.

--- src/main/scala/encry/cli/commands/Command.scala
+++ src/main/scala/encry/cli/commands/Command.scala
@@ -18,21 +18,9 @@ object Command {
       case _ => throw new Error("Wrong argument type.")
     }.getOrElse(throw new Error(s"Argument $n not found."))
 
-    /**
-      * def getOrElse - try to get v by key. Return v if v exist and v is instance of default
-      * return default if v doesn't exist or v is not instance of default
-      * @param key
-      * @param default
-      * @tparam VT
-      * @return
-      */
-    def getOrElse[VT <: Ast.Value](key: String, default: => VT): VT =
-      args.get(key) match {
-      case Some(v) => v match {
-        case vt: VT@unchecked => vt
-        case _ => default
-      }
-      case None => default
+    def getOrElse[VT <: Ast.Value](key: String, default: => VT): VT = args.get(key) match {
+      case Some(v: VT@unchecked) => v
+      case _ => default
     }
   }
 }
