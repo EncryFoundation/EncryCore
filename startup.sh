f6a580947c035f0848962c8aa70d18e14fe9f3ca
Ilya Oskin <ilya.arcadich@gmail.com>
2018-04-26 18:17:17 +0300

    Fix application confs.
    # Conflicts:
    #	startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo Starting EncryCore client ...
+echo Building EncryCore client ...
 sbt assembly
 zip -d `find . -name EncryCore-assembly*.jar` META-INF/*.RSA META-INF/*.DSA META-INF/*.SF
-java -jar `find . -name EncryCore-assembly*.jar`
+echo Starting EncryCore client ...
+if [ -z "$1" ];then
+java -jar `find . -name EncryCore-assembly*.jar` "src/main/resources/application.conf"
+else
+java -jar `find . -name EncryCore-assembly*.jar` $1
+fi
