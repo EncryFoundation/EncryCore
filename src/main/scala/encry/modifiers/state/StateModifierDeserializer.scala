695796cff8ba6a4d851c1d2fab206025527173e0
Ilya Oskin <ilya.arcadich@gmail.com>
2018-05-15 12:18:18 +0300

    DataBox implementation.

--- src/main/scala/encry/modifiers/state/StateModifierDeserializer.scala
+++ src/main/scala/encry/modifiers/state/StateModifierDeserializer.scala
@@ -7,10 +7,11 @@ import scala.util.{Failure, Try}
 object StateModifierDeserializer {
 
   def parseBytes(bytes: Array[Byte], typeId: Byte): Try[EncryBaseBox] = typeId match {
-    case CoinbaseBox.`typeId` => CoinbaseBoxSerializer.parseBytes(bytes)
-    case AssetBox.TypeId => AssetBoxSerializer.parseBytes(bytes)
-    case AssetIssuingBox.TypeId => AssetIssuingBoxSerializer.parseBytes(bytes)
-    case AssetCreationBox.TypeId => AssetCreationBoxSerializer.parseBytes(bytes)
+    case CoinbaseBox.`TypeId` => CoinbaseBoxSerializer.parseBytes(bytes)
+    case AssetBox.`TypeId` => AssetBoxSerializer.parseBytes(bytes)
+    case AssetIssuingBox.`TypeId` => AssetIssuingBoxSerializer.parseBytes(bytes)
+    case AssetCreationBox.`TypeId` => AssetCreationBoxSerializer.parseBytes(bytes)
+    case DataBox.`TypeId` => DataBoxSerializer.parseBytes(bytes)
     case t => Failure(new Error(s"Got unknown typeId: $t"))
   }
 }
---
+++ src/main/scala/encry/modifiers/state/box/DataBox.scala
@@ -0,0 +1,71 @@
+package encry.modifiers.state.box
+
+import com.google.common.primitives.{Bytes, Longs, Shorts}
+import encry.modifiers.state.box.EncryBox.BxTypeId
+import encry.modifiers.state.box.proposition.{EncryProposition, PropositionSerializer}
+import encry.settings.Algos
+import encrywm.lang.backend.env.{ESObject, ESValue}
+import encrywm.lib.Types
+import io.circe.Encoder
+import io.circe.syntax._
+import scorex.core.serialization.Serializer
+
+import scala.util.Try
+
+/**
+  * Stores arbitrary data in EncryTL binary format.
+  */
+case class DataBox(override val proposition: EncryProposition,
+                   override val nonce: Long,
+                   data: Array[Byte])
+  extends EncryBox[EncryProposition] {
+
+  override type M = DataBox
+
+  override val typeId: BxTypeId = DataBox.TypeId
+
+  override def serializer: Serializer[M] = DataBoxSerializer
+
+  override val esType: Types.ESProduct = ???
+
+  override def asVal: ESValue = ???
+
+  override def convert: ESObject = ???
+}
+
+object DataBox {
+
+  val TypeId: BxTypeId = 4.toByte
+
+  implicit val jsonEncoder: Encoder[DataBox] = (bx: DataBox) => Map(
+    "type" -> TypeId.asJson,
+    "id" -> Algos.encode(bx.id).asJson,
+    "proposition" -> bx.proposition.asJson,
+    "nonce" -> bx.nonce.asJson,
+    "data" -> Algos.encode(bx.data).asJson,
+  ).asJson
+}
+
+object DataBoxSerializer extends Serializer[DataBox] {
+
+  override def toBytes(obj: DataBox): Array[Byte] = {
+    val propBytes = PropositionSerializer.toBytes(obj.proposition)
+    Bytes.concat(
+      Shorts.toByteArray(propBytes.length.toShort),
+      propBytes,
+      Longs.toByteArray(obj.nonce),
+      Shorts.toByteArray(obj.data.length.toShort),
+      obj.data
+    )
+  }
+
+  override def parseBytes(bytes: Array[Byte]): Try[DataBox] = Try {
+    val propositionLen = Shorts.fromByteArray(bytes.take(2))
+    val iBytes = bytes.drop(2)
+    val proposition = PropositionSerializer.parseBytes(iBytes.take(propositionLen)).get
+    val nonce = Longs.fromByteArray(iBytes.slice(propositionLen, propositionLen + 8))
+    val dataLen = Shorts.fromByteArray(iBytes.slice(propositionLen + 8, propositionLen + 8 + 2))
+    val data = iBytes.takeRight(dataLen)
+    DataBox(proposition, nonce, data)
+  }
+}
