050ae4da4ec9ea45d790ece6e413e08ede8d40ff
kapinuss <kapinuss@rambler.ru>
2018-05-16 15:42:13 +0300

    more simple commands

--- src/main/scala/encry/cli/ConsolePromptListener.scala
+++ src/main/scala/encry/cli/ConsolePromptListener.scala
@@ -1,47 +1,36 @@
 package encry.cli
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.Actor
 import encry.cli.commands._
 import encry.settings.EncryAppSettings
-import jline.console.ConsoleReader
 import scorex.core.utils.ScorexLogging
-import encry.EncryApp.{nodeViewHolder, miner}
+import encry.EncryApp.{miner, nodeViewHolder, reader}
+
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Success
+import scala.util.{Success, Failure}
 
 class ConsolePromptListener(settings: EncryAppSettings) extends Actor with ScorexLogging {
 
   import ConsolePromptListener._
 
-  private val prompt = "$> "
-
-  private val reader = new ConsoleReader()
-
   override def receive: Receive = {
-    case StartListening =>
-      Iterator.continually(reader.readLine(prompt)).foreach { input =>
-        InputParser.parse(input) match {
-          case Success(command) =>
-            getCommand(command.category.name, command.ident.name) match {
-              case Some(cmd) =>
-                  cmd.execute(
-                    nodeViewHolder,
-                    miner,
-                    Command.Args(command.params.map(p => p.ident.name -> p.value).toMap),
-                    settings
-                  ).map {
-                    case Some(x) =>
-                      println(x.msg)
-                      print(prompt)
-                    case None =>
-                  }
-              case _ => println("Unsupported command. Type 'app help' to get commands list")
-            }
-          case _ => println("Bad input")
-        }
+    case input: String =>
+      InputParser.parse(input) match {
+        case Success(command) =>
+          getCommand(command.category.name, command.ident.name) match {
+            case Some(cmd) =>
+              cmd.execute(Command.Args(command.params.map(p => p.ident.name -> p.value).toMap), settings)
+                .map {
+                  case Some(x) => reader.println(x.msg)
+                  case None =>
+                }
+            case None => reader.println("Unsupported command. Type 'app help' to get commands list")
+          }
+        case Failure(e) => reader.println("Bad input")
       }
   }
 }
+
 
 object ConsolePromptListener {
 
--- src/main/scala/encry/cli/commands/AddKey.scala
+++ src/main/scala/encry/cli/commands/AddKey.scala
@@ -1,6 +1,5 @@
 package encry.cli.commands
 
-import akka.actor.ActorRef
 import akka.pattern._
 import akka.util.Timeout
 import encry.cli.{Ast, Response}
@@ -9,17 +8,16 @@ import encry.view.history.EncryHistory
 import encry.view.mempool.EncryMempool
 import encry.view.state.UtxoState
 import encry.view.wallet.EncryWallet
+import encry.EncryApp._
 import scorex.core.NodeViewHolder.ReceivableMessages.GetDataFromCurrentView
 import scala.concurrent.Future
 import scala.util.Try
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object AddKey extends Command {
 
-  override def execute(nodeViewHolderRef: ActorRef, miner: ActorRef,
-                       args: Command.Args, settings: EncryAppSettings): Future[Option[Response]] = Try {
+  override def execute(args: Command.Args, settings: EncryAppSettings): Future[Option[Response]] = Try {
     implicit val timeout: Timeout = Timeout(settings.scorexSettings.restApi.timeout)
-    nodeViewHolderRef ?
+    nodeViewHolder ?
       GetDataFromCurrentView[EncryHistory, UtxoState, EncryWallet, EncryMempool, Unit] { view =>
         view.vault.keyManager.createNewKey()
       }
