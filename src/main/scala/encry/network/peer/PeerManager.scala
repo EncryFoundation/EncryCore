e3342aa615251152a1c4d5f153ac33f3fad6acf6
kapinuss <kapinuss@rambler.ru>
2018-05-28 17:35:13 +0300

    refactoring peerManager

--- src/main/scala/encry/network/peer/PeerManager.scala
+++ src/main/scala/encry/network/peer/PeerManager.scala
@@ -2,17 +2,14 @@ package encry.network.peer
 
 import java.net.InetSocketAddress
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef}
 import scorex.core.network._
 import scorex.core.utils.ScorexLogging
 import encry.EncryApp._
+
 import scala.collection.mutable
 import scala.util.Random
 
-/**
-  * Peer manager takes care of peers connected and in process, and also chooses a random peer to connect
-  * Must be singleton
-  */
 class PeerManager extends Actor with ScorexLogging {
 
   import PeerManager.ReceivableMessages._
@@ -30,7 +27,7 @@ class PeerManager extends Actor with ScorexLogging {
 
   var lastIdUsed: Int = 0
 
-  if (peerDatabase.isEmpty()) settings.network.knownPeers.foreach { address =>
+  if (peerDatabase.isEmpty) settings.network.knownPeers.foreach { address =>
     if (!isSelf(address, None)) peerDatabase.addOrUpdateKnownPeer(address, PeerInfo(timeProvider.time(), None))
   }
 
@@ -40,60 +37,51 @@ class PeerManager extends Actor with ScorexLogging {
     else None
   }
 
-  def peerListOperations: Receive = {
+  /**
+    * Given a peer's address and declared address, returns `true` iff the peer is the same is this node.
+    */
+  def isSelf(address: InetSocketAddress, declaredAddress: Option[InetSocketAddress]): Boolean =
+    settings.network.bindAddress == address ||
+      settings.network.declaredAddress.exists(da => declaredAddress.contains(da)) ||
+      declaredAddress.contains(settings.network.bindAddress) ||
+      settings.network.declaredAddress.contains(address)
 
+  override def receive: Receive = peerListOperations orElse apiInterface orElse connectOps
+
+  def apiInterface: Receive = {
+    case GetConnectedPeers => sender() ! (connectedPeers.values.map(_.handshake).toSeq: Seq[Handshake])
+    case GetAllPeers => sender() ! peerDatabase.knownPeers()
+    case GetBlacklistedPeers => sender() ! peerDatabase.blacklistedPeers()
+  }
+
+  def peerListOperations: Receive = {
     case AddOrUpdatePeer(address, peerNameOpt, connTypeOpt) =>
-      if (!isSelf(address, None)) peerDatabase.addOrUpdateKnownPeer(address, PeerInfo(timeProvider.time(), peerNameOpt, connTypeOpt))
+      if (!isSelf(address, None))
+        peerDatabase.addOrUpdateKnownPeer(address, PeerInfo(timeProvider.time(), peerNameOpt, connTypeOpt))
     case KnownPeers => sender() ! peerDatabase.knownPeers().keys.toSeq
     case RandomPeer => sender() ! randomPeer()
     case RandomPeers(howMany: Int) => sender() ! Random.shuffle(peerDatabase.knownPeers().keys.toSeq).take(howMany)
     case FilterPeers(sendingStrategy: SendingStrategy) => sender() ! sendingStrategy.choose(connectedPeers.values.toSeq)
   }
 
-  def apiInterface: Receive = {
-
-    case GetConnectedPeers => sender() ! (connectedPeers.values.map(_.handshake).toSeq: Seq[Handshake])
-    case GetAllPeers => sender() ! peerDatabase.knownPeers()
-    case GetBlacklistedPeers => sender() ! peerDatabase.blacklistedPeers()
-  }
-
-  /**
-    * Given a peer's address and declared address, returns `true` iff the peer is the same is this node.
-    */
-  def isSelf(address: InetSocketAddress, declaredAddress: Option[InetSocketAddress]): Boolean =
-  // TODO: should the peer really be considered the same as self iff one of the following conditions hold?? Check carefully.
-    settings.network.bindAddress == address ||
-      settings.network.declaredAddress.exists(da => declaredAddress.contains(da)) ||
-      declaredAddress.contains(settings.network.bindAddress) ||
-      settings.network.declaredAddress.contains(address)
-
-  def peerCycle: Receive = connecting orElse handshaked orElse disconnected
-
-  def connecting: Receive = {
+  def connectOps: Receive = {
     case DoConnecting(remote, direction) =>
-      if (peerDatabase.isBlacklisted(remote)) {
-        log.info(s"Got incoming connection from blacklisted $remote")
-      } else {
-        val peerHandlerRef = sender
-        val isIncoming = direction == Incoming
-        val isAlreadyConnecting = connectingPeers.contains(remote)
-        if (isAlreadyConnecting && !isIncoming) {
+      if (peerDatabase.isBlacklisted(remote)) log.info(s"Got incoming connection from blacklisted $remote")
+      else {
+        val isIncoming: Boolean = direction == Incoming
+        if (connectingPeers.contains(remote) && !isIncoming) {
           log.info(s"Trying to connect twice to $remote, going to drop the duplicate connection")
-          peerHandlerRef ! CloseConnection
+          sender() ! CloseConnection
         } else {
           if (!isIncoming) {
             log.info(s"Connecting to $remote")
             connectingPeers += remote
           }
-          peerHandlerRef ! StartInteraction
+          sender() ! StartInteraction
           lastIdUsed += 1
         }
       }
-  }
-
-  def handshaked: Receive = {
     case Handshaked(peer) =>
-      //todo: filter by an id introduced by the PeerManager
       if (peerDatabase.isBlacklisted(peer.socketAddress)) {
         log.info(s"Got handshake from blacklisted ${peer.socketAddress}")
       } else {
@@ -107,32 +95,23 @@ class PeerManager extends Actor with ScorexLogging {
           context.system.eventStream.publish(HandshakedPeer(peer))
         }
       }
-  }
-
-  def disconnected: Receive = {
     case Disconnected(remote) =>
       connectedPeers -= remote
       connectingPeers -= remote
       context.system.eventStream.publish(DisconnectedPeer(remote))
-  }
-
-  override def receive: Receive = ({
     case CheckPeers =>
       if (connectedPeers.size + connectingPeers.size < settings.network.maxConnections) {
         randomPeer().foreach { address =>
-          //todo: avoid picking too many peers from the same bucket, see Bitcoin ref. impl.
           if (!connectedPeers.exists(_._1 == address) &&
             !connectingPeers.exists(_.getHostName == address.getHostName)) {
             sender() ! ConnectTo(address)
           }
         }
       }
-
     case AddToBlacklist(peer) =>
       log.info(s"Blacklist peer $peer")
       peerDatabase.blacklistPeer(peer, timeProvider.time())
-    // todo: shouldn't peer be removed from `connectedPeers` when it is blacklisted?
-  }: Receive) orElse peerListOperations orElse apiInterface orElse peerCycle
+  }
 }
 
 object PeerManager {
