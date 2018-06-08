bc19f8a6effbe93d437bbc18da2a62b0967a38b8
aleksandr <alexbromel@gmail.com>
2018-06-08 15:27:31 +0300

    Added support of InfluxDB

--- src/main/scala/encry/stats/StatsSender.scala
+++ src/main/scala/encry/stats/StatsSender.scala
@@ -1,13 +1,10 @@
 package encry.stats
 
-import java.util.concurrent.TimeUnit
-
 import akka.actor.Actor
 import com.typesafe.config.{Config, ConfigFactory}
+import encry.consensus.DifficultySerializer
 import encry.modifiers.history.block.EncryBlock
 import encry.network.NodeViewSynchronizer.ReceivableMessages.SemanticallySuccessfulModifier
-import encry.settings.Algos
-import org.influxdb.dto.Point
 import org.influxdb.{InfluxDB, InfluxDBFactory}
 import scorex.core.utils.ScorexLogging
 
@@ -16,7 +13,7 @@ class StatsSender extends Actor with ScorexLogging {
   val influxDBConfig: Config = ConfigFactory.load("influxDB")
 
   val influxDB: InfluxDB =
-    InfluxDBFactory.connect("http://172.16.10.55:8086", influxDBConfig.getString("loginDB"), influxDBConfig.getString("passwordDB") )
+    InfluxDBFactory.connect(influxDBConfig.getString("urlDB"), influxDBConfig.getString("loginDB"), influxDBConfig.getString("passwordDB") )
 
   influxDB.setRetentionPolicy("autogen")
 
@@ -28,10 +25,6 @@ class StatsSender extends Actor with ScorexLogging {
 
     case SemanticallySuccessfulModifier(fb: EncryBlock) =>
 
-      influxDB.write(8189, Point.measurement("blockApply")
-        .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-        .addField("ModifierId", Algos.encode(fb.id))
-        .addField("value", fb.payload.transactions.length)
-        .build().lineProtocol())
+      influxDB.write(8189, s"difficulty value=${DifficultySerializer.decodeCompactBits(fb.header.nBits)} ${System.currentTimeMillis}")
   }
 }
