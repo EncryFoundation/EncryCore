70f010336dbc95b1b79f53569ff0d7a6b77313f4
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2018-06-09 16:48:34 +0300

    InfluxDB support#EC-173 (#43)
    * Support of InfluxDB added
    
    * Added support of InfluxDB
    
    * Added support of InfluxDB
    
    * New query added
    
    * Added support of InfluxDB
    
    * Remove influxDB.conf

---
+++ src/main/scala/encry/stats/StatsSender.scala
@@ -0,0 +1,36 @@
+package encry.stats
+
+
+import java.util
+
+import akka.actor.Actor
+import encry.EncryApp.settings
+import encry.consensus.DifficultySerializer
+import encry.modifiers.history.block.EncryBlock
+import encry.network.NodeViewSynchronizer.ReceivableMessages.SemanticallySuccessfulModifier
+import org.influxdb.{InfluxDB, InfluxDBFactory}
+import scorex.core.utils.ScorexLogging
+
+class StatsSender extends Actor with ScorexLogging {
+
+  val influxDB: InfluxDB =
+    InfluxDBFactory.connect(settings.influxDB.url, settings.influxDB.login, settings.influxDB.password )
+
+  influxDB.setRetentionPolicy("autogen")
+
+  override def preStart(): Unit = {
+    context.system.eventStream.subscribe(self, classOf[SemanticallySuccessfulModifier[_]])
+  }
+
+  override def receive: Receive = {
+
+    case SemanticallySuccessfulModifier(fb: EncryBlock) =>
+
+      influxDB.write(8189, util.Arrays.asList(
+        s"difficulty,nodeName=${settings.network.nodeName} value=${DifficultySerializer.decodeCompactBits(fb.header.nBits)}",
+        s"height,nodeName=${settings.network.nodeName} value=${fb.header.height}",
+        s"height,nodeName=${settings.network.nodeName} value=${fb.payload.transactions.length}" )
+      )
+
+  }
+}
