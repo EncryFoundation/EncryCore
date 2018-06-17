a94de06f3b20a094dde937e3a98a66669e68ec8c
Ilya Oskin <ilya.arcadich@gmail.com>
2018-06-17 19:39:00 +0300

    Fix directives and Block validation.

--- src/main/scala/encry/modifiers/mempool/directive/AssetIssuingDirective.scala
+++ src/main/scala/encry/modifiers/mempool/directive/AssetIssuingDirective.scala
@@ -3,13 +3,14 @@ package encry.modifiers.mempool.directive
 import com.google.common.primitives.{Bytes, Ints, Longs, Shorts}
 import encry.account.Account
 import encry.modifiers.mempool.directive.Directive.DTypeId
-import encry.modifiers.state.box.proposition.{AccountProposition, ContractProposition}
+import encry.modifiers.state.box.proposition.EncryProposition
 import encry.modifiers.state.box.{AssetCreationBox, AssetIssuingBox, EncryBaseBox}
 import encry.settings.{Algos, Constants}
 import encry.utils.Utils
 import encrywm.common.{EncryContract, ScriptMeta}
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder, HCursor}
+import org.encryfoundation.prismlang.compiler.{CompiledContract, CompiledContractSerializer}
 import scorex.core.serialization.Serializer
 import scorex.core.transaction.box.Box.Amount
 import scorex.crypto.encode.Base58
@@ -19,7 +20,7 @@ import scorex.utils.Random
 
 import scala.util.Try
 
-case class AssetIssuingDirective(script: EncryContract,
+case class AssetIssuingDirective(contract: CompiledContract,
                                  amount: Amount,
                                  symbol: String) extends Directive {
 
@@ -28,19 +29,18 @@ case class AssetIssuingDirective(script: EncryContract,
   override val typeId: DTypeId = AssetIssuingDirective.TypeId
 
   override def boxes(digest: Digest32, idx: Int): Seq[EncryBaseBox] = {
-    val assetCreationBox = AssetCreationBox(AccountProposition(Account(PublicKey @@ Random.randomBytes(32))),
+    val assetCreationBox = AssetCreationBox(EncryProposition.accountLock(Account(PublicKey @@ Random.randomBytes(32))),
       Utils.nonceFromDigest(digest ++ Ints.toByteArray(idx) ++ Ints.toByteArray(1)), amount, symbol)
     Seq(
       assetCreationBox,
-      AssetIssuingBox(ContractProposition(script),
+      AssetIssuingBox(EncryProposition(contract),
         Utils.nonceFromDigest(digest ++ Ints.toByteArray(idx) ++ Ints.toByteArray(2)), amount, assetCreationBox.id)
     )
   }
 
-  override val cost: Amount = 20 * script.meta.complexityScore
+  override val cost: Amount = 20
 
-  override lazy val isValid: Boolean =
-    amount > 0 && symbol.length <= Constants.Chain.TokenSymbolMaxLength && script.validMeta
+  override lazy val isValid: Boolean = amount > 0 && symbol.length <= Constants.Chain.TokenSymbolMaxLength
 
   override def serializer: Serializer[M] = AssetIssuingDirectiveSerializer
 }
@@ -51,9 +51,9 @@ object AssetIssuingDirective {
 
   implicit val jsonEncoder: Encoder[AssetIssuingDirective] = (d: AssetIssuingDirective) => Map(
     "typeId" -> d.typeId.asJson,
-    "script" -> Base58.encode(d.script.serializedScript).asJson,
-    "complexityScore" -> d.script.meta.complexityScore.asJson,
-    "scriptFingerprint" -> Base58.encode(d.script.meta.scriptFingerprint).asJson,
+    "script" -> Base58.encode(d.contract.serializedScript).asJson,
+    "complexityScore" -> d.contract.meta.complexityScore.asJson,
+    "scriptFingerprint" -> Base58.encode(d.contract.meta.scriptFingerprint).asJson,
     "amount" -> d.amount.asJson
   ).asJson
 
@@ -83,21 +83,18 @@ object AssetIssuingDirectiveSerializer extends Serializer[AssetIssuingDirective]
 
   override def toBytes(obj: AssetIssuingDirective): Array[Byte] =
     Bytes.concat(
-      Shorts.toByteArray(obj.script.serializedScript.length.toShort),
-      obj.script.serializedScript,
-      Ints.toByteArray(obj.script.meta.complexityScore),
-      obj.script.meta.scriptFingerprint,
+      Shorts.toByteArray(obj.contract.bytes.length.toShort),
+      obj.contract.bytes,
       Longs.toByteArray(obj.amount),
       obj.symbol.getBytes(Algos.charset)
     )
 
-  override def parseBytes(bytes: Array[Byte]): Try[AssetIssuingDirective] = Try {
+  override def parseBytes(bytes: Array[Byte]): Try[AssetIssuingDirective] = {
     val scriptLen = Shorts.fromByteArray(bytes.take(2))
-    val complexity = Ints.fromByteArray(bytes.slice(scriptLen + 2, scriptLen + 2 + 4))
-    val fingerprint = bytes.slice(scriptLen + 2 + 4, scriptLen + 2 + 4 + 8)
-    val contract = EncryContract(bytes.slice(2, scriptLen), ScriptMeta(complexity, fingerprint))
-    val amount = Longs.fromByteArray(bytes.slice(scriptLen + 2 + 4 + 8, scriptLen + 2 + 4 + 8 + 8))
-    val symbol = new String(bytes.slice(scriptLen + 2 + 4 + 8 + 8, bytes.length), Algos.charset)
-    AssetIssuingDirective(contract, amount, symbol)
+    CompiledContractSerializer.parseBytes(bytes.slice(2, scriptLen + 2)).map { contract =>
+      val amount = Longs.fromByteArray(bytes.slice(scriptLen + 2, scriptLen + 2 + 8))
+      val symbol = new String(bytes.slice(scriptLen + 2 + 8, bytes.length), Algos.charset)
+      AssetIssuingDirective(contract, amount, symbol)
+    }
   }
 }
