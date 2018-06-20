dc9a5d822579f236bdd6dcec991c10981df64961
Ilya Oskin <ilya.arcadich@gmail.com>
2018-06-20 15:42:08 +0300

    isNewHeader() return.

--- src/main/scala/encry/view/history/processors/DownloadProcessor.scala
+++ src/main/scala/encry/view/history/processors/DownloadProcessor.scala
@@ -74,7 +74,7 @@ trait DownloadProcessor extends ScorexLogging {
     } else if (header.height >= blockDownloadProcessor.minimalBlockHeight) {
       // Already synced and header is not too far back. Download required modifiers
       requiredModifiersForHeader(header)
-    } else if (!isHeadersChainSynced) {
+    } else if (!isHeadersChainSynced && isNewHeader(header)) {
       // Headers chain is synced after this header. Start downloading full blocks
       log.info(s"Headers chain is synced after header ${header.encodedId} at height ${header.height}")
       isHeadersChainSyncedVar = true
