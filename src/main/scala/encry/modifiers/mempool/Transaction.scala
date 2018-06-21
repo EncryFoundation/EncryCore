b3ea42f22d45bab5bfcd7962e7093cb37dbbc0cb
kapinuss <kapinuss@rambler.ru>
2018-06-21 13:06:44 +0300

    -mempool

---
+++ src/main/scala/encry/modifiers/mempool/Transaction.scala
@@ -0,0 +1,17 @@
+package encry.modifiers.mempool
+
+import scorex.core.transaction.box.proposition.Proposition
+import scorex.core.{EphemerealNodeViewModifier, ModifierId, ModifierTypeId}
+import scorex.crypto.hash.Blake2b256
+
+abstract class Transaction[P <: Proposition] extends EphemerealNodeViewModifier {
+  override val modifierTypeId: ModifierTypeId = Transaction.ModifierTypeId
+
+  val messageToSign: Array[Byte]
+
+  override lazy val id: ModifierId = ModifierId @@ Blake2b256(messageToSign)
+}
+
+object Transaction {
+  val ModifierTypeId: scorex.core.ModifierTypeId = scorex.core.ModifierTypeId @@ 2.toByte
+}
