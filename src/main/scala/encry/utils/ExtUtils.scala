a029289302efc561b0d4c1316f84217e5f333637
Yury Ignatev <yury.ignatev@gmail.com>
2018-06-28 11:39:28 +0300

    log info, warn, debug and error

--- src/main/scala/encry/utils/ExtUtils.scala
+++ src/main/scala/encry/utils/ExtUtils.scala
@@ -1,15 +1,29 @@
 package encry.utils
 
-object ExtUtils {
+import com.typesafe.scalalogging.StrictLogging
+
+object ExtUtils extends StrictLogging {
 
   implicit class Appliable[T](val obj: T) extends AnyVal {
     def rapply[S](f: T => S ): S = { f(obj)}
     def iapply[S](f: T => S ): T = { f(obj); obj}
   }
 
-  implicit class Traceable[T](val obj: T) extends AnyVal {
-    def trace: T = { println(obj); obj}
-    def trace[U]( u: U): T = { println(u); obj}
-    def traceWith[S](reader: T => S ): T = { println(reader(obj)); obj}
+  implicit class Loggable[T](val obj: T) extends AnyVal {
+    def logInfo: T = { logger.info(obj.toString); obj}
+    def logInfo[U]( u: U): T = { logger.info(u.toString); obj}
+    def logInfoWith[S](extractor: T => S ): T = { logger.info(extractor(obj).toString); obj}
+
+    def logDebug: T = { logger.debug(obj.toString); obj}
+    def logDebug[U]( u: U): T = { logger.debug(u.toString); obj}
+    def logDebugWith[S](extractor: T => S ): T = { logger.debug(extractor(obj).toString); obj}
+
+    def logWarn: T = { logger.warn(obj.toString); obj}
+    def logWarn[U]( u: U): T = { logger.warn(u.toString); obj}
+    def logWarnWith[S](extractor: T => S ): T = { logger.warn(extractor(obj).toString); obj}
+
+    def logErr: T = { logger.error(obj.toString); obj}
+    def logErr[U]( u: U): T = { logger.error(u.toString); obj}
+    def logErrWith[S](extractor: T => S ): T = { logger.error(extractor(obj).toString); obj}
   }
 }
