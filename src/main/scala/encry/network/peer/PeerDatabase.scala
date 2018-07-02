d1bcc1555f8ca1d5a733704a90369a64be381c31
Stanislav Kapinus <kapinuss@rambler.ru>
2018-07-02 17:54:42 +0300

    Ec-238 PeerDatabase and logic about peer refactoring (#76)
    * -blacklist
    
    * -unused massegaes
    
    * -mutable
    
    * -mutable
    
    * tiny
    
    * tinies

--- src/main/scala/encry/network/peer/PeerDatabase.scala
+++ src/main/scala/encry/network/peer/PeerDatabase.scala
@@ -1,44 +1,27 @@
 package encry.network.peer
 
 import java.net.InetSocketAddress
-
 import encry.network.PeerConnectionHandler._
-import encry.utils.NetworkTime
 
-import scala.collection.mutable
+case object PeerDatabase {
 
-//todo: persistence
-case class PeerDatabase(filename: Option[String])  {
-
-  private val whitelistPersistence = mutable.Map[InetSocketAddress, PeerInfo]()
-
-  private val blacklist = mutable.Map[String, NetworkTime.Time]()
+  private var whitelistPersistence: Map[InetSocketAddress, PeerInfo] = Map.empty
 
   def addOrUpdateKnownPeer(address: InetSocketAddress, peerInfo: PeerInfo): Unit = {
     val updatedPeerInfo: PeerInfo = whitelistPersistence.get(address).fold(peerInfo) { dbPeerInfo =>
       val nodeNameOpt: Option[String] = peerInfo.nodeName orElse dbPeerInfo.nodeName
-      val connTypeOpt: Option[ConnectionType] = peerInfo.connectionType orElse  dbPeerInfo.connectionType
+      val connTypeOpt: Option[ConnectionType] = peerInfo.connectionType orElse dbPeerInfo.connectionType
       PeerInfo(peerInfo.lastSeen, nodeNameOpt, connTypeOpt)
     }
-    whitelistPersistence.put(address, updatedPeerInfo)
+    whitelistPersistence += (address -> updatedPeerInfo)
   }
-
-  def blacklistPeer(address: InetSocketAddress, time: NetworkTime.Time): Unit = {
-    whitelistPersistence.remove(address)
-    if (!isBlacklisted(address)) blacklist += address.getHostName -> time
-  }
-
-  def isBlacklisted(address: InetSocketAddress): Boolean =
-    blacklist.synchronized(blacklist.contains(address.getHostName))
 
   def knownPeers(): Map[InetSocketAddress, PeerInfo] =
     whitelistPersistence.keys.flatMap(k => whitelistPersistence.get(k).map(v => k -> v)).toMap
 
-  def blacklistedPeers(): Seq[String] = blacklist.keys.toSeq
-
   def isEmpty: Boolean = whitelistPersistence.isEmpty
 
-  def remove(address: InetSocketAddress): Boolean = whitelistPersistence.remove(address).nonEmpty
+  def remove(address: InetSocketAddress): Unit = whitelistPersistence -= address
 }
 
 case class PeerInfo(lastSeen: Long, nodeName: Option[String] = None, connectionType: Option[ConnectionType] = None)
