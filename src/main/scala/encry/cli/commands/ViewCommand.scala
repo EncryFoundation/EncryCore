a8908648c79b5b5827cfcaa2cff8b3539367d400
Yury Ignatev <yury.ignatev@gmail.com>
2018-07-03 19:12:30 +0300

    =>local command

--- src/main/scala/encry/cli/commands/ViewCommand.scala
+++ src/main/scala/encry/cli/commands/ViewCommand.scala
@@ -9,7 +9,7 @@ import scala.concurrent.Future
 import akka.pattern._
 
 trait ViewCommand extends Command {
-  override def executeRequest(args: Command.Args, settings: EncryAppSettings): Any = this
+  override def executeRequest(args: Command.Args, settings: EncryAppSettings): Any = LocalCommand
   override def execute(args: Command.Args, settings: EncryAppSettings): Future[Option[Response]] = {
     implicit val timeout: Timeout = Timeout(settings.restApi.timeout)
     (nodeViewHolder ? executeRequest(args, settings)).mapTo[Option[Response]]
