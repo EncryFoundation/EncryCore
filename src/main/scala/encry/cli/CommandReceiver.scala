ef99df2eae727132eb441070510925fe4cafcbbd
Yury Ignatev <yury.ignatev@gmail.com>
2018-07-04 01:10:50 +0300

    refactor

--- src/main/scala/encry/cli/CommandReceiver.scala
+++ src/main/scala/encry/cli/CommandReceiver.scala
@@ -23,37 +23,29 @@ trait CommandReceiver[StateType <: EncryState[StateType]] {
     CurrentView(nodeView.history, nodeView.state, nodeView.wallet, nodeView.mempool)
   def keys: Seq[PrivateKey25519] = currentView.vault.keyManager.keys
   def response(s: String): Option[Response] = Some(Response(s))
+  def send(s: String): Unit = sender() ! Some(Response(s))
   def commandReceive: Receive = {
-    case AddKey =>
-      if(Try(currentView.vault.keyManager.createNewKey()).isSuccess) Some(Response("OK"))
-      else Some(Response("Operation failed"))
+    case AddKey => send(if (Try(currentView.vault.keyManager.createNewKey()).isSuccess) "OK" else "Operation failed")
     case GetBalance =>
-      val balances = currentView.vault.getBalances
-      val res = if(balances.isEmpty) "<empty>"
-      else balances.map(tokenInfo => s"TokenID(${Algos.encode(tokenInfo._1)}) : ${tokenInfo._2}\n").mkString("\n")
-      sender() ! response(res)
-    case PrintMyAddrs =>
-      sender() ! response(keys.map(_.publicImage.address).mkString("\n"))
-    case PrintPrivKeys =>
-      sender() ! response(keys.map(k => Algos.encode(k.privKeyBytes)).mkString("\n"))
-    case PrintPubKeys =>
-      val res: String = Try(keys.map(_.publicKeyBytes).map(Algos.encode).mkString("\n")).getOrElse("ERROR!")
-      sender() ! response(res)
+      val balances = currentView.vault.getBalances.map(tokenInfo => s"TokenID(${Algos.encode(tokenInfo._1)}) : ${tokenInfo._2}\n")
+      send(if(balances.isEmpty) "<empty>" else balances.mkString("\n"))
+    case PrintMyAddrs => send(keys.map(_.publicImage.address).mkString("\n"))
+    case PrintPrivKeys => send(keys.map(k => Algos.encode(k.privKeyBytes)).mkString("\n"))
+    case PrintPubKeys => send(Try(keys.map(_.publicKeyBytes).map(Algos.encode).mkString("\n")).getOrElse("ERROR!"))
     case InitKeyStorage.Request(mnemonicCode) =>
-      sender() ! Try(currentView.vault.keyManager.initStorage(Mnemonic.mnemonicCodeToBytes(mnemonicCode)))
-        .map(_ => mnemonicCode)
-        .map(code => response(s"Your mnemonic code is: $code"))
-        .getOrElse(response("Operation failed. Couldn't init key storage."))
+      Try(currentView.vault.keyManager.initStorage(Mnemonic.mnemonicCodeToBytes(mnemonicCode)))
+        .map(_ => s"Your mnemonic code is: $mnemonicCode")
+        .getOrElse("Operation failed. Couldn't init key storage.")
+        .iapply(send)
     case Transfer.Request(recipient, fee, amount, timestamp) =>
-      val view = currentView
-      sender() ! Try{
-        val boxes: IndexedSeq[AssetBox] = view.vault.walletStorage.allBoxes.collect{case x: AssetBox => x}
+      Try {
+        currentView.vault.walletStorage.allBoxes.collect { case x: AssetBox => x }
           .foldLeft(0L -> Seq.empty[AssetBox]) { case ((sum, seq), box) =>
-            if (sum < (amount + fee)) (sum + box.amount, seq :+ box) else (sum, seq)
+            if (sum < (amount + fee)) (sum + box.amount, seq :+ box) else sum -> seq
           }._2.toIndexedSeq
-        TransactionFactory.defaultPaymentTransactionScratch(keys.head, fee, timestamp, boxes, recipient, amount).iapply(
-          self ! LocallyGeneratedTransaction[EncryProposition, EncryTransaction](_)
-        )
-      }.toOption.map(tx => response(tx.toString)).getOrElse(response("Operation failed. Malformed data."))
+      } .map(TransactionFactory.defaultPaymentTransactionScratch(keys.head, fee, timestamp, _, recipient, amount))
+        .map(_.iapply(self ! LocallyGeneratedTransaction[EncryProposition, EncryTransaction](_)))
+        .map(_.toString).toOption.getOrElse("Operation failed. Malformed data.")
+        .iapply(send)
   }
 }
--- src/main/scala/encry/cli/commands/InitKeyStorage.scala
+++ src/main/scala/encry/cli/commands/InitKeyStorage.scala
@@ -9,6 +9,8 @@ import encry.utils.Mnemonic
 object InitKeyStorage extends ViewCommand {
 
   case class Request(mnemonicCode: String)
+
   override def executeRequest(args: Command.Args, settings: EncryAppSettings): Any =
     Request(args.requireArgOrElse("seed", Ast.Str(Mnemonic.entropyToMnemonicCode(SecureRandom.getSeed(16)))).s)
+
 }
