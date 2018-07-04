a1bab616c45b4c89a26cbdd95dc95cbb9005c6bf
aleksandr <alexbromel@gmail.com>
2018-07-04 13:15:30 +0300

    Add payload, blocks seq

--- src/main/scala/encry/network/ModifiersHolder.scala
+++ src/main/scala/encry/network/ModifiersHolder.scala
@@ -2,6 +2,9 @@ package encry.network
 
 import akka.persistence.{PersistentActor, SnapshotOffer}
 import encry.ModifierTypeId
+import encry.modifiers.history.block.EncryBlock
+import encry.modifiers.history.block.header.EncryBlockHeader
+import encry.modifiers.history.block.payload.EncryBlockPayload
 import encry.modifiers.{EncryPersistentModifier, NodeViewModifier}
 import encry.network.ModifiersHolder.{Mods, RequestedModifiers}
 import encry.network.PeerConnectionHandler.ConnectedPeer
@@ -9,6 +12,10 @@ import encry.utils.ScorexLogging
 import encry.view.EncryNodeViewHolder.ReceivableMessages.{LocallyGeneratedModifier, ModifiersFromRemote}
 
 class ModifiersHolder extends PersistentActor with ScorexLogging {
+
+  var headers: Seq[EncryBlockHeader] = Seq.empty
+  var payloads: Seq[EncryBlockPayload] = Seq.empty
+  var blocks: Seq[EncryBlock] = Seq.empty
 
   var modsFromRemote: Mods = Mods(Map.empty, 0)
 
@@ -20,8 +27,8 @@ class ModifiersHolder extends PersistentActor with ScorexLogging {
   }
 
   override def receiveCommand: Receive = {
-    case RequestedModifiers(modifierTypeId, modifiers) => _
-    case lm: LocallyGeneratedModifier[EncryPersistentModifier] => _
+    case RequestedModifiers(modifierTypeId, modifiers) => updateModifiers(modifierTypeId, modifiers)
+    case lm: LocallyGeneratedModifier[EncryPersistentModifier] => updateModifiers(lm.pmod.modifierTypeId, Seq(lm.pmod))
     case x: Any => logger.info(s"Strange input: $x")
   }
 
@@ -33,6 +40,14 @@ class ModifiersHolder extends PersistentActor with ScorexLogging {
 
   def updateModsFromRemote(newMods: ModifiersFromRemote): Unit = {
     modsFromRemote = Mods(modsFromRemote.numberOfModsByPeerAndModType + ((newMods.source, newMods.modifierTypeId) -> newMods.remoteObjects.size), modsFromRemote.numberOfPacksFromRemotes + 1)
+  }
+
+  def updateModifiers(modsTypeId: ModifierTypeId, modifiers: Seq[NodeViewModifier]): Unit = modifiers.foreach {
+
+    case header: EncryBlockHeader => headers = headers :+ header
+    case payload: EncryBlockPayload => payloads = payloads :+ payload
+    case block: EncryBlock => blocks = blocks :+ block
+    case _ =>
   }
 }
 
