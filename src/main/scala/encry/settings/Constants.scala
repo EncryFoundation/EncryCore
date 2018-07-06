a6e38a02ce3d44af03aa09a1ad982e5b9989fb79
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2018-07-06 11:49:02 +0300

    Change maxRollBack (#98)
    * Change maxRollBack
    
    * Fix

--- src/main/scala/encry/settings/Constants.scala
+++ src/main/scala/encry/settings/Constants.scala
@@ -67,7 +67,7 @@ object Constants {
     val PreGenesisHeight: Height = Height @@ (GenesisHeight - 1)
 
     // Maximum number of epochs blockchain state can be rolled back
-    val MaxRollbackDepth: Int = (10.days.toMillis / DesiredBlockInterval.toMillis).toInt
+    val MaxRollbackDepth: Int = 100
 
     val MaxTimeDrift: Long = 10 * DesiredBlockInterval.toMillis
 
