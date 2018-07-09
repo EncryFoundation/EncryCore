350750b9f90ee4c8cdb8ba286273965ef18ac272
aleksandr <alexbromel@gmail.com>
2018-07-09 16:38:32 +0300

    Some test added

--- src/main/scala/encry/network/ModifiersHolder.scala
+++ src/main/scala/encry/network/ModifiersHolder.scala
@@ -9,6 +9,7 @@ import encry.modifiers.{EncryPersistentModifier, NodeViewModifier}
 import encry.network.ModifiersHolder._
 import encry.network.PeerConnectionHandler.ConnectedPeer
 import encry.settings.Algos
+import encry.utils.Chain._
 import encry.utils.Logging
 import encry.view.EncryNodeViewHolder.ReceivableMessages.LocallyGeneratedModifier
 import encry.{ModifierId, ModifierTypeId}
@@ -19,15 +20,15 @@ import scala.concurrent.duration._
 class ModifiersHolder extends PersistentActor with Logging {
 
   var modsFromRemote: Mods = Mods(Map.empty, 0)
-  var amount: Amount = Amount(0, 0, 0, 0, 0, Seq.empty)
+  var amount: Amount = Amount(0, 0, 0, 0, 0, Seq.empty, Seq.empty)
 
   /**
     * Map, which contains not completed blocks
     * Key can be payloadId or also header id. So value depends on key, and can contains: headerId, payloadId
     */
   var notCompletedBlocks: Map[String, String] = Map.empty
-  var headerCache: Map[String, EncryBlockHeader] = Map.empty
-  var payloadCache: Map[String, EncryBlockPayload] = Map.empty
+  var headerCache: Map[String, (EncryBlockHeader, Int)] = Map.empty
+  var payloadCache: Map[String, (EncryBlockPayload, Int)] = Map.empty
   var completedBlocks: SortedMap[Int, EncryBlock] = SortedMap.empty
 
   context.system.scheduler.schedule(10.second, 10.second) {
@@ -35,8 +36,10 @@ class ModifiersHolder extends PersistentActor with Logging {
       payloadCache.size,
       notCompletedBlocks.size,
       completedBlocks.size,
-      completedBlocks.keys.max,
-      gaps
+      if (completedBlocks.nonEmpty) completedBlocks.keys.max else 0,
+      countGaps(completedBlocks.keys.toSeq),
+      headerCache.values.filter(_._2 > 1).map( headerWithDuplicatesQty => headerWithDuplicatesQty._1.id -> headerWithDuplicatesQty._2).toSeq ++
+        payloadCache.values.filter(_._2 > 1).map( payloadWithDuplicatesQty => payloadWithDuplicatesQty._1.id -> payloadWithDuplicatesQty._2).toSeq
     )
     saveSnapshot(amount)
     logger.info(s"${amount.receivedHeaders} headers received - " +
@@ -44,7 +47,8 @@ class ModifiersHolder extends PersistentActor with Logging {
       s"${amount.notCompletedBlocks} not full blocks - " +
       s"${amount.completedBlocks} full blocks - " +
       s"max height: ${amount.maxHeight} " +
-      s"Gaps: ${amount.gaps.foldLeft(""){ case (str, gap) => str + s"(${gap._1}, ${gap._2})"}}")
+      s"Gaps: ${amount.gaps.foldLeft("") { case (str, gap) => str + s"(${gap._1}, ${gap._2})" }} " +
+      s"Duplicates: ${amount.duplicates.foldLeft(""){ case (str, duplicate) => str + s"(${Algos.encode(duplicate._1)}, ${duplicate._2})"  }}")
   }
 
   override def preStart(): Unit = logger.info(s"ModifiersHolder actor is started.")
@@ -71,7 +75,7 @@ class ModifiersHolder extends PersistentActor with Logging {
   def createBlockIfPossible(payloadId: ModifierId): Unit = {
     notCompletedBlocks.get(Algos.encode(payloadId)).foreach(headerId => headerCache.get(headerId).foreach { header =>
       payloadCache.get(Algos.encode(payloadId)).foreach { payload =>
-        completedBlocks += header.height -> EncryBlock(header, payload, None)
+        completedBlocks += header._1.height -> EncryBlock(header._1, payload._1, None)
         notCompletedBlocks -= Algos.encode(payloadId)
       }
     })
@@ -80,7 +84,8 @@ class ModifiersHolder extends PersistentActor with Logging {
   def updateModifiers(modsTypeId: ModifierTypeId, modifiers: Seq[NodeViewModifier]): Unit = modifiers.foreach {
 
     case header: EncryBlockHeader =>
-      headerCache += Algos.encode(header.id) -> header
+      val prevValue: (EncryBlockHeader, Int) = headerCache.getOrElse(Algos.encode(header.id), (header, -1))
+      headerCache += Algos.encode(header.id) -> (prevValue._1, prevValue._2 + 1)
       if (!notCompletedBlocks.contains(Algos.encode(header.payloadId)))
         notCompletedBlocks += Algos.encode(header.payloadId) -> Algos.encode(header.id)
       else {
@@ -88,18 +93,13 @@ class ModifiersHolder extends PersistentActor with Logging {
         createBlockIfPossible(header.payloadId)
       }
     case payload: EncryBlockPayload =>
-      payloadCache += Algos.encode(payload.id) -> payload
+      val prevValue: (EncryBlockPayload, Int) = payloadCache.getOrElse(Algos.encode(payload.id), (payload, -1))
+      payloadCache += Algos.encode(payload.id) -> (prevValue._1, prevValue._2 + 1)
       if (!notCompletedBlocks.contains(Algos.encode(payload.id))) notCompletedBlocks += Algos.encode(payload.id) -> ""
       else createBlockIfPossible(payload.id)
     case block: EncryBlock => completedBlocks += block.header.height -> block
     case _ =>
   }
-
-  def gaps: Seq[(Int, Int)] = completedBlocks.keys.foldLeft(Seq[(Int, Int)](), 0) {
-      case ((gaps, prevHeight), blockHeight) =>
-        if (prevHeight + 1 != blockHeight) (gaps :+ (prevHeight, blockHeight - 1), blockHeight)
-        else (gaps, blockHeight)
-    }._1
 }
 
 object ModifiersHolder {
@@ -109,9 +109,11 @@ object ModifiersHolder {
                     notCompletedBlocks: Int,
                     completedBlocks: Int,
                     maxHeight: Int,
-                    gaps: Seq[(Int, Int)])
+                    gaps: Seq[(Int, Int)],
+                    duplicates: Seq[(ModifierId, Int)])
 
   case class RequestedModifiers(modifierTypeId: ModifierTypeId, modifiers: Seq[NodeViewModifier])
 
   case class Mods(numberOfModsByPeerAndModType: Map[(ConnectedPeer, ModifierTypeId), Int], numberOfPacksFromRemotes: Int)
+
 }
---
+++ src/test/scala/encry/utils/ChainTest.scala
@@ -0,0 +1,27 @@
+package encry.utils
+
+import org.scalatest.{Matchers, PropSpec}
+
+import scala.util.Random
+
+class ChainTest extends PropSpec with Matchers {
+
+  property("Correct counting of gaps") {
+
+    val firstMinRange: Int = Random.nextInt(1000) + 1
+
+    val firstMaxRange: Int = firstMinRange + Random.nextInt(1000)
+
+    val secondMinRange: Int = firstMaxRange + Random.nextInt(1000)
+
+    val secondMaxRange: Int = secondMinRange + Random.nextInt(10000)
+
+    val thirdMinRange: Int = secondMaxRange + Random.nextInt(1000)
+
+    val thirdMaxRange: Int = thirdMinRange + Random.nextInt(1000)
+
+    val fakeHeightChain: Seq[Int] = (firstMinRange to firstMaxRange) ++ (secondMinRange to secondMaxRange) ++ (thirdMinRange to thirdMaxRange)
+
+    Chain.countGaps(fakeHeightChain) shouldEqual Seq((1, firstMinRange - 1), (firstMaxRange + 1, secondMinRange - 1), (secondMaxRange + 1, thirdMinRange - 1))
+  }
+}
