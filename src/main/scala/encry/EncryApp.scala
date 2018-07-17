3cfb7c96df7e0ff278145dbf22a5099023137d76
kapinuss <kapinuss@rambler.ru>
2018-07-17 17:20:52 +0300

    mailbox

--- src/main/scala/encry/EncryApp.scala
+++ src/main/scala/encry/EncryApp.scala
@@ -48,7 +48,8 @@ object EncryApp extends App with Logging {
       ModifiersSpec
     )
   }
-  lazy val nodeViewHolder: ActorRef = system.actorOf(EncryNodeViewHolder.props().withDispatcher("nvh-dispatcher"), "nodeViewHolder")
+  lazy val nodeViewHolder: ActorRef = system.actorOf(EncryNodeViewHolder.props()
+    .withDispatcher("nvh-dispatcher").withMailbox("nvh-mailbox"), "nodeViewHolder")
   val readersHolder: ActorRef = system.actorOf(Props[EncryViewReadersHolder], "readersHolder")
   lazy val networkController: ActorRef = system.actorOf(Props[NetworkController]
     .withDispatcher("network-dispatcher"), "networkController")
