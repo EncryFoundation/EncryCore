455d1cd63d243215e300db983e41119594582474
aleksandr <alexbromel@gmail.com>
2018-07-23 16:51:16 +0300

    New logging

--- src/main/scala/encry/local/TransactionGenerator.scala
+++ src/main/scala/encry/local/TransactionGenerator.scala
@@ -44,9 +44,10 @@ class TransactionGenerator extends Actor with Logging {
       if (limit > 0) {
         (0 until limit).foldLeft(Seq[EncryTransaction](), walletData) {
           case ((txs, wd), i) =>
-            if (wd.boxes.map(_.amount).sum >= (limit - i) * (amountD + minimalFeeD) ) {
+            if (wd.boxes.map(_.amount).sum > (limit - i) * (amountD + minimalFeeD) ) {
               val tx: EncryTransaction = createTransaction(wd)
               val leftBoxes: Seq[AssetBox] = wd.boxes.filterNot(bx => tx.inputs.map(_.boxId).contains(bx.id))
+              log.info(s"Inputs: ${tx.inputs.map(input => walletData.boxes.find(_.id == input.boxId).map(box => Algos.encode(box.id) + " : " + box.amount))}")
               (txs :+ tx) -> wd.copy(boxes = leftBoxes)
             } else txs -> wd
         }._1.foreach(tx => {
@@ -63,7 +64,7 @@ class TransactionGenerator extends Actor with Logging {
   def createTransaction(wd: WalletData): EncryTransaction = {
     val timestamp: Time = timeProvider.time()
     val boxes: IndexedSeq[AssetBox] = wd.boxes.foldLeft(Seq.empty[AssetBox]) { case (boxesAcc, box) =>
-      if (boxesAcc.map(_.amount).sum < (amountD + minimalFeeD)) boxesAcc :+ box else boxesAcc
+      if (boxesAcc.map(_.amount).sum <= (amountD + minimalFeeD)) boxesAcc :+ box else boxesAcc
     }.toIndexedSeq
     TransactionFactory.defaultPaymentTransactionScratch(wd.secret, minimalFeeD, timestamp, boxes,
       Address @@ settings.testing.defaultRecipientAddress, amountD)
