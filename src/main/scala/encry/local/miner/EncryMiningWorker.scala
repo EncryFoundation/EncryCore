53da97858f0b12bcf69482cbd37ed10013dfd649
Ilya Oskin <ilya.arcadich@gmail.com>
2018-07-30 11:41:28 +0300

    `Box` added to contract context, Prism version update.

--- src/main/scala/encry/local/miner/EncryMiningWorker.scala
+++ src/main/scala/encry/local/miner/EncryMiningWorker.scala
@@ -18,11 +18,11 @@ class EncryMiningWorker(myIdx: Int, numberOfWorkers: Int) extends Actor with Log
 
   def miningInProgress: Receive = {
     case MineBlock(candidate: CandidateBlock, nonce: Long) =>
-      log.info(s"Iter with nonce: $nonce. Start nonce is: ${Long.MaxValue / numberOfWorkers * myIdx}. " +
+      log.info(s"Trying nonce: $nonce. Start nonce is: ${Long.MaxValue / numberOfWorkers * myIdx}. " +
         s"Iter qty: ${nonce - (Long.MaxValue / numberOfWorkers * myIdx) + 1} on worker: $myIdx with diff: ${candidate.difficulty}")
       ConsensusSchemeReaders.consensusScheme.verifyCandidate(candidate, nonce)
         .fold(self ! MineBlock(candidate, nonce + 1)) { block =>
-          log.info(s"New block is found: $block on worker $self in ${sdf.format(new Date(System.currentTimeMillis()))}. Iter qty: ${nonce - (Long.MaxValue / numberOfWorkers * myIdx) + 1}")
+          log.info(s"New block is found: $block on worker $self at ${sdf.format(new Date(System.currentTimeMillis()))}. Iter qty: ${nonce - (Long.MaxValue / numberOfWorkers * myIdx) + 1}")
           miner ! MinedBlock(block, myIdx)
         }
     case DropChallenge => context.become(miningPaused)
@@ -32,7 +32,7 @@ class EncryMiningWorker(myIdx: Int, numberOfWorkers: Int) extends Actor with Log
     case NextChallenge(candidate: CandidateBlock) =>
       challengeStartTime = new Date(System.currentTimeMillis())
       context.become(miningInProgress)
-      log.info(s"Start challenge on worker: $myIdx on height ${candidate.parentOpt.map(_.height + 1).getOrElse("No parent")} in ${sdf.format(challengeStartTime)}")
+      log.info(s"Start challenge on worker: $myIdx at height ${candidate.parentOpt.map(_.height + 1).getOrElse("No parent")} at ${sdf.format(challengeStartTime)}")
       self ! MineBlock(candidate, Long.MaxValue / numberOfWorkers * myIdx)
     case _ =>
   }
