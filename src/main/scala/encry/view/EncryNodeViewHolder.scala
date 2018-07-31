f7aa989b894038efc09399310b043a06b7089eda
aleksandr <alexbromel@gmail.com>
2018-07-31 09:37:13 +0300

    Fix

--- src/main/scala/encry/view/EncryNodeViewHolder.scala
+++ src/main/scala/encry/view/EncryNodeViewHolder.scala
@@ -12,7 +12,7 @@ import encry.modifiers.history.block.payload.{EncryBlockPayload, EncryBlockPaylo
 import encry.modifiers.history.{ADProofSerializer, ADProofs}
 import encry.modifiers.mempool.{BaseTransaction, EncryTransactionSerializer}
 import encry.modifiers.serialization.Serializer
-import encry.modifiers.state.box.EncryProposition
+import encry.modifiers.state.box.{AssetBox, EncryProposition}
 import encry.network.EncryDeliveryManager.{ContinueSync, FullBlockChainSynced, StopSync}
 import encry.network.EncryNodeViewSynchronizer.ReceivableMessages._
 import encry.network.ModifiersHolder.{ApplyState, RequestedModifiers}
