5a423b16b425bd16bb280af853bfe6030780cc16
ugulavaGeorge <ugulava.george@gmail.com>
2018-08-16 17:04:34 +0300

    empty lines, return types

--- src/main/scala/encry/stats/KafkaAgent.scala
+++ src/main/scala/encry/stats/KafkaAgent.scala
@@ -5,20 +5,20 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
 
 object KafkaAgent {
-  val kafkaBrokers = "172.16.10.55:9092"
-  val topicName = "Logs"
+  val kafkaBrokers: String= "172.16.10.55:9092"
+  val topicName: String = "Logs"
 
-  val kafkaParams = new Properties()
+  val kafkaParams: Properties= new Properties
 
   kafkaParams.put("bootstrap.servers", kafkaBrokers)
   kafkaParams.put("key.serializer", classOf[StringSerializer])
   kafkaParams.put("value.serializer", classOf[StringSerializer])
   kafkaParams.put("group.id", "encry")
 
-  val producer = new KafkaProducer[String,String](kafkaParams)
+  val producer: KafkaProducer[String,String] = new KafkaProducer[String,String](kafkaParams)
 
   def sendEvent(message: String) : Unit = {
-    val key = java.util.UUID.randomUUID().toString
+    val key = java.util.UUID.randomUUID.toString
     producer.send(new ProducerRecord[String, String](topicName, key, message))
   }
 }
