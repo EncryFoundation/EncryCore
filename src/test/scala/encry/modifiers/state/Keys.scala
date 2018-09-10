4488819d5fdfd4d0bcd9ec64b9ab7901b491b922
Ilya Oskin <ilya.arcadich@gmail.com>
2018-09-10 11:01:02 +0300

    `test-data` directory removed.

--- src/test/scala/encry/modifiers/state/Keys.scala
+++ src/test/scala/encry/modifiers/state/Keys.scala
@@ -5,7 +5,7 @@ import org.encryfoundation.common.crypto.{PrivateKey25519, PublicKey25519}
 
 trait Keys {
 
-  val secrets: Seq[PrivateKey25519] = TestHelper.getOrGenerateKeys(TestHelper.Props.keysFilePath)
+  val secrets: Seq[PrivateKey25519] = TestHelper.genKeys(TestHelper.Props.keysQty)
 
   val secret: PrivateKey25519 = secrets.head
   val publicKey: PublicKey25519 = secret.publicImage
--- src/test/scala/encry/utils/TestHelper.scala
+++ src/test/scala/encry/utils/TestHelper.scala
@@ -1,59 +1,42 @@
 package encry.utils
 
-import java.io.{File, FileWriter}
 import encry.modifiers.state.box.Box.Amount
 import encry.modifiers.state.box.{AssetBox, EncryBaseBox, EncryProposition}
 import org.encryfoundation.common.crypto.PrivateKey25519
 import org.encryfoundation.common.transaction.EncryAddress
 import org.encryfoundation.common.utils.TaggedTypes.ADKey
-import scorex.crypto.signatures.Curve25519
-import scala.io.Source
+import scorex.crypto.signatures.{Curve25519, PrivateKey, PublicKey}
 import scala.util.Random
 
 object TestHelper {
 
-  lazy val genesisSeed: Long = Long.MaxValue
-  lazy val rndGen = new scala.util.Random(genesisSeed)
+  val genesisSeed: Long = Long.MaxValue
 
   object Props {
-    lazy val keysQty = 1000
-    lazy val boxValue: Amount = 1000000
-    lazy val txAmount: Amount = 199000
-    lazy val txFee: Amount = 4300
-    lazy val testDir = "test-data/"
-    lazy val keysFilePath = s"${testDir}seeds"
+    val keysQty: Int = 1000
+    val boxValue: Amount = 1000000
+    val txAmount: Amount = 199000
+    val txFee: Amount = 4300
   }
 
-  def genKeysFile(qty: Int, filePath: String): Unit = {
-    val fileWriter = new FileWriter(new File(filePath))
-    (0 until qty).foreach { _ =>
-      fileWriter.write(Random.alphanumeric.take(32).mkString + "\n")
-    }
-    fileWriter.close()
+  def genKeys(qty: Int): Seq[PrivateKey25519] = {
+    val rnd: Random = new scala.util.Random(genesisSeed)
+    (0 to qty)
+      .foldLeft(Seq[PrivateKey25519]()) { case (acc, _) =>
+        val keys: (PrivateKey, PublicKey) = Curve25519.createKeyPair(
+          rnd.alphanumeric.take(32).mkString.getBytes)
+        acc :+ PrivateKey25519(keys._1, keys._2)
+      }
   }
 
-  def getOrGenerateKeys(filePath: String): Seq[PrivateKey25519] = {
-    def getKeysFromFile: Seq[PrivateKey25519] =
-      Source.fromFile(new File(filePath)).getLines.toList
-        .foldLeft(Seq[PrivateKey25519]()) { case (pks, keyString) =>
-          val keys = Curve25519.createKeyPair(keyString.getBytes())
-          pks :+ PrivateKey25519(keys._1, keys._2)
-        }
-
-    val file = new File(filePath)
-    if (file.exists) getKeysFromFile
-    else {
-      genKeysFile(Props.keysQty, Props.keysFilePath)
-      getKeysFromFile
-    }
-  }
-
-  def genAssetBoxes: IndexedSeq[AssetBox] =
-    getOrGenerateKeys(Props.keysFilePath).foldLeft(IndexedSeq[AssetBox]()) { case (bxs, pk) =>
+  def genAssetBoxes: IndexedSeq[AssetBox] = {
+    val rnd: Random = new scala.util.Random(genesisSeed)
+    genKeys(Props.keysQty).foldLeft(IndexedSeq[AssetBox]()) { case (bxs, pk) =>
       bxs :+ AssetBox(
         EncryProposition.pubKeyLocked(pk.publicKeyBytes),
-        rndGen.nextLong(), Props.boxValue)
+        rnd.nextLong(), Props.boxValue)
     }
+  }
 
   def genAssetBox(address: EncryAddress.Address, amount: Amount = 9L): AssetBox =
     AssetBox(EncryProposition.addressLocked(address), amount, Props.boxValue)
