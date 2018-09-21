4db4cbcf7d75fc0fb588858b65e47d08e8b232ba
kapinuss <kapinuss@rambler.ru>
2018-09-21 16:54:48 +0300

    refactoring

--- src/main/scala/encry/api/http/ApiRoute.scala
+++ src/main/scala/encry/api/http/ApiRoute.scala
@@ -6,19 +6,17 @@ import akka.http.scaladsl.server.{Directive0, Directives, Route}
 import akka.http.scaladsl.unmarshalling.PredefinedFromEntityUnmarshallers
 import akka.util.Timeout
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import encry.settings.EncryAppSettings
+import encry.settings.RESTApiSettings
 
 trait ApiRoute extends Directives with FailFastCirceSupport with PredefinedFromEntityUnmarshallers {
 
-  val settings: EncryAppSettings
+  val settings: RESTApiSettings
 
-  lazy val withCors: Directive0 = settings.restApi.corsAllowedOrigin.fold(pass) { origin =>
+  lazy val withCors: Directive0 = settings.corsAllowedOrigin.fold(pass) { origin =>
     respondWithHeaders(RawHeader("Access-Control-Allow-Origin", origin))
   }
 
-  val apiKeyHeaderName: String = "api_key"
-
-  implicit lazy val timeout: Timeout = Timeout(settings.restApi.timeout)
+  implicit lazy val timeout: Timeout = Timeout(settings.timeout)
 
   def context: ActorRefFactory
 
