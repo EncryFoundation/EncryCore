bd7f7f4602fc633739e41cb4d93b3f3980a5e6ae
kapinuss <kapinuss@rambler.ru>
2018-09-21 16:27:35 +0300

    refactoring

--- src/main/scala/encry/api/http/ApiRoute.scala
+++ src/main/scala/encry/api/http/ApiRoute.scala
@@ -1,22 +1,28 @@
 package encry.api.http
 
 import akka.actor.ActorRefFactory
-import akka.http.scaladsl.server.{Directive0, Route}
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.server.{Directive0, Directives, Route}
 import akka.http.scaladsl.unmarshalling.PredefinedFromEntityUnmarshallers
 import akka.util.Timeout
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import encry.utils.ActorHelper
+import encry.settings.EncryAppSettings
 
-trait ApiRoute extends ApiDirectives with ActorHelper with FailFastCirceSupport with PredefinedFromEntityUnmarshallers {
+trait ApiRoute extends Directives with FailFastCirceSupport with PredefinedFromEntityUnmarshallers {
+
+  val settings: EncryAppSettings
+
+  lazy val withCors: Directive0 = settings.restApi.corsAllowedOrigin.fold(pass) { origin =>
+    respondWithHeaders(RawHeader("Access-Control-Allow-Origin", origin))
+  }
+
+  val apiKeyHeaderName: String = "api_key"
+
+  implicit lazy val timeout: Timeout = Timeout(settings.restApi.timeout)
 
   def context: ActorRefFactory
 
   def route: Route
-
-  override val apiKeyHeaderName: String = "api_key"
-
-  //implicit val printer: Printer = Printer.spaces2.copy(dropNullValues = true)
-  implicit lazy val timeout: Timeout = Timeout(settings.timeout)
 
   protected def jsonRoute(fn: ScorexApiResponse, method: Directive0): Route = method {
     withCors(complete(fn))
