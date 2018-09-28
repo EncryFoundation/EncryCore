ec6c9268320f0c3741494827ac72bd30c4e7809a
aleksandr <alexbromel@gmail.com>
2018-09-28 13:08:56 +0300

    New logging

--- src/main/scala/encry/view/history/processors/BlockProcessor.scala
+++ src/main/scala/encry/view/history/processors/BlockProcessor.scala
@@ -74,7 +74,8 @@ trait BlockProcessor extends BlockHeaderProcessor with Logging {
       logInfo(s"toApply.lengthCompare(newChain.length - 1) != 0: ${toApply.lengthCompare(newChain.length - 1) != 0}")
       logInfo(s"toApply.length: ${toApply.length}")
       logInfo(s"toRemove.length: ${toRemove.length}")
-      logInfo(s"Diff: ${toApply.map(_.header.height).diff(newChain.headers.map(_.height)).mkString(",")}")
+      logInfo(s"toApply Heights: ${toApply.map(_.header.height).mkString(",")}")
+      logInfo(s"newChain Heights: ${newChain.headers.map(_.height).mkString(",")}")
       if (toApply.lengthCompare(newChain.length - 1) != 0) nonBestBlock(toProcess)
       else {
         logStatus(toRemove, toApply, fullBlock, Some(prevBest))
