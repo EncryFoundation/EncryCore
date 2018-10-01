622c7ff84bc950a8899ed8b0b8f24ffb2f3e7865
kapinuss <kapinuss@rambler.ru>
2018-10-01 18:49:31 +0300

    refactoring

--- src/main/scala/encry/storage/EncryStorage.scala
+++ src/main/scala/encry/storage/EncryStorage.scala
@@ -10,12 +10,10 @@ trait EncryStorage extends AutoCloseable with Logging {
 
   val store: Store
 
-  def insert(version: ByteArrayWrapper,
-             toInsert: Seq[(ByteArrayWrapper, ByteArrayWrapper)]): Unit =
+  def insert(version: ByteArrayWrapper, toInsert: Seq[(ByteArrayWrapper, ByteArrayWrapper)]): Unit =
     store.update(version, Seq.empty, toInsert)
 
-  def remove(version: ByteArrayWrapper,
-             toRemove: Seq[ByteArrayWrapper]): Unit =
+  def remove(version: ByteArrayWrapper, toRemove: Seq[ByteArrayWrapper]): Unit =
     store.update(version, toRemove, Seq.empty)
 
   def update(version: ByteArrayWrapper,
@@ -27,12 +25,9 @@ trait EncryStorage extends AutoCloseable with Logging {
 
   def get(key: ByteArrayWrapper): Option[Array[Byte]] = store.get(key).map(_.data)
 
-  def rollbackTo(version: VersionTag): Try[Unit] =
-    store.get(ByteArrayWrapper(version)) match {
-      case Some(_) =>
-        Success(store.rollback(ByteArrayWrapper(version)))
-      case None =>
-        Failure(new Exception(s"Unable to get root hash at version ${Algos.encoder.encode(version)}"))
+  def rollbackTo(version: VersionTag): Try[Unit] = store.get(ByteArrayWrapper(version)) match {
+      case Some(_) => Success(store.rollback(ByteArrayWrapper(version)))
+      case None => Failure(new Exception(s"Unable to get root hash at version ${Algos.encoder.encode(version)}"))
     }
 
   override def close(): Unit = {
