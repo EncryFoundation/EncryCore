c531331a17a98600c1a875d5434f3eaa25da606f
aleksandr <alexbromel@gmail.com>
2018-10-19 17:13:31 +0300

    Change emissi

--- src/main/scala/encry/consensus/EncrySupplyController.scala
+++ src/main/scala/encry/consensus/EncrySupplyController.scala
@@ -6,11 +6,11 @@ import encry.view.history.History.Height
 
 object EncrySupplyController {
 
-  def supplyAt(height: Height): Amount = {
+  def supplyAt(height: Height): Amount = if (height > Constants.Chain.EmissionEpochLength ){
     val multiptlyIterQty: Int =
       if (height % Constants.Chain.EmissionEpochLength == 0)
-        Math.round(height / Constants.Chain.EmissionEpochLength) + 1
+        Math.round(height / Constants.Chain.EmissionEpochLength) +  1
       else Math.round(height / Constants.Chain.EmissionEpochLength)
     (Constants.Chain.InitialEmissionAmount * Math.pow(1 - Constants.Chain.EmissionDecay, multiptlyIterQty)).toLong
-  }
+  } else Constants.Chain.InitialEmissionAmount
 }
