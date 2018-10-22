791ea46ff451d4d43f2859302845810d619003c8
aleksandr <alexbromel@gmail.com>
2018-10-22 10:36:11 +0300

    Code cleanup

--- src/main/scala/encry/local/miner/Miner.scala
+++ src/main/scala/encry/local/miner/Miner.scala
@@ -79,7 +79,7 @@ class Miner extends Actor with Logging {
       context.become(miningDisabled)
     case MinedBlock(block, workerIdx) if candidateOpt.exists(_.stateRoot sameElements block.header.stateRoot) =>
       logInfo(s"Going to propagate new block $block from worker $workerIdx" +
-        s" with nonce ${block.header.nonce}")
+        s" with nonce: ${block.header.nonce}")
       killAllWorkers()
         nodeViewHolder ! LocallyGeneratedModifier(block.header)
         nodeViewHolder ! LocallyGeneratedModifier(block.payload)
