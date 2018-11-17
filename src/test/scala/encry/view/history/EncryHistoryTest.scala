01a3a072050396f5e5471d0316d7132a29d6a30f
aleksandr <alexbromel@gmail.com>
2018-11-17 20:14:00 +0300

    Add one more test

--- src/test/scala/encry/view/history/EncryHistoryTest.scala
+++ src/test/scala/encry/view/history/EncryHistoryTest.scala
@@ -69,4 +69,42 @@ class EncryHistoryTest extends PropSpec with Matchers with InstanceFactory with 
       .get
       .parentId shouldEqual fork.last.header.id
   }
+
+  property("Fork on non-best-height, but with bigger commulative sum"){
+
+    val historyWith35Blocks: EncryHistory = (0 until 35).foldLeft(generateDummyHistory) {
+      case (prevHistory, _) =>
+        val block: Block = generateNextBlock(prevHistory)
+        prevHistory.append(block.header).get._1.append(block.payload).get._1.reportModifierIsValid(block)
+    }
+
+    val (historyWith40Blocks: EncryHistory, fork: Seq[Block]) = (0 until 5).foldLeft(historyWith35Blocks, Seq[Block]()) {
+      case ((prevHistory, forkBlocks), _) =>
+        val mainBlock: Block = generateNextBlock(prevHistory)
+        val forkBlock: Block = generateNextBlock(prevHistory, 100, prevId = forkBlocks.lastOption.map(_.id))
+        prevHistory.append(mainBlock.header).get._1.append(mainBlock.payload).get._1.reportModifierIsValid(mainBlock) ->
+          (forkBlocks :+ forkBlock)
+    }
+
+    val maxHistoryHeightWithoutForks: Int = historyWith40Blocks.bestBlockHeight
+
+    val historyWithFork: EncryHistory = fork.take(3).foldLeft(historyWith40Blocks) {
+      case (prevHistory, blockToApply) =>
+        prevHistory.append(blockToApply.header).get._1.append(blockToApply.payload).get._1.reportModifierIsValid(blockToApply)
+    }
+
+    maxHistoryHeightWithoutForks shouldEqual historyWithFork.bestBlockHeight
+
+    val historyAfterSimpleFork: EncryHistory = (0 until 5).foldLeft(historyWithFork) {
+      case (prevHistory, _) =>
+        val block: Block = generateNextBlock(prevHistory)
+        prevHistory.append(block.header).get._1.append(block.payload).get._1.reportModifierIsValid(block)
+    }
+
+//    historyAfterSimpleFork.headerIdsAtHeight(34).length shouldEqual 1
+//    historyAfterSimpleFork.headerIdsAtHeight(35).length shouldEqual 2 //two headers at height
+//    historyAfterSimpleFork.headerIdsAtHeight(36).length shouldEqual 2
+//    historyAfterSimpleFork.headerIdsAtHeight(37).length shouldEqual 2
+//    historyAfterSimpleFork.headerIdsAtHeight(38).length shouldEqual 1
+  }
 }
