7a4ad63a47984560cd521e5c229b1f0c2c05554c
aleksandr <alexbromel@gmail.com>
2018-11-27 10:51:21 +0300

    Some additions

--- src/main/scala/encry/api/http/routes/WalletInfoApiRoute.scala
+++ src/main/scala/encry/api/http/routes/WalletInfoApiRoute.scala
@@ -39,7 +39,7 @@ case class WalletInfoApiRoute(nodeViewActorRef: ActorRef,
 
   def getUtxosR: Route = (path("utxos") & get) {
     getWallet
-      .map { wallet => scala.util.Random.shuffle(wallet.walletStorage.allBoxes).take(3000).asJson }
+      .map { wallet => scala.util.Random.shuffle(wallet.walletStorage.allBoxes).take(300).asJson }
       .okJson()
   }
 }
