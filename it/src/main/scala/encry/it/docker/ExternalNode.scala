b7656f773b19ae1551e88976239cec37cfe203f8
timofey <gusev.t.s1@gmail.com>
2018-12-10 16:35:11 +0300

    added new code

--- it/src/main/scala/encry/it/docker/ExternalNode.scala
+++ it/src/main/scala/encry/it/docker/ExternalNode.scala
@@ -1,15 +1,13 @@
 package encry.it.docker
 
 import java.net.{InetSocketAddress, URL}
-
 import com.typesafe.config.Config
 
 class ExternalNode(config: Config) extends Node(config) {
-  override def nodeApiEndpoint = new URL(config.getString("node-api-endpoint"))
+  override def nodeApiEndpoint: URL = new URL(config.getString("node-api-endpoint"))
+  override def apiKey: String       = config.getString("api-key")
 
-  override def apiKey = config.getString("api-key")
-
-  override def networkAddress = {
+  override def networkAddress: InetSocketAddress = {
     val hostAndPort             = "([^:]+)\\:([\\d+])+".r
     val hostAndPort(host, port) = config.getString("network-address")
     new InetSocketAddress(host, port.toInt)
