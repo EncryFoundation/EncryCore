2e5ebd0f7d207e830116c3c8c84b188b93def4ae
aleksandr <alexbromel@gmail.com>
2018-12-10 18:28:54 +0300

    Some additions

---
+++ it/src/main/scala/encry/it/docker/NodesFromDocker.scala
@@ -0,0 +1,12 @@
+package encry.it.docker
+
+import monix.eval.Coeval
+import org.scalatest.Suite
+
+trait NodesFromDocker extends Nodes with DockerBased { _: Suite =>
+  protected val dockerNodes: Coeval[Seq[Docker.DockerNode]] = dockerSingleton
+    .map(_.startNodes(nodeConfigs))
+    .memoize
+
+  override protected def nodes: Seq[Node] = dockerNodes()
+}
