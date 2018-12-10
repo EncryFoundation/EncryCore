43f56c66496365985a217f0a5e63ea4381ebe480
aleksandr <alexbromel@gmail.com>
2018-12-10 12:14:19 +0300

    Some additions

---
+++ src/it/scala/encry/it/api/package.scala
@@ -0,0 +1,13 @@
+package encry.it
+
+import io.circe.{Decoder, Json}
+import org.asynchttpclient.Response
+
+import scala.concurrent.{ExecutionContext, Future}
+
+package object api {
+  implicit class ResponseFutureExt(val f: Future[Response]) extends AnyVal {
+    def as[A: Decoder](implicit ec: ExecutionContext): Future[A] = f.map(r => Json.fromString(r.getResponseBody).as[A].right.get)(ec)
+  }
+}
+
