e5a9fe248113523152d104e9c2660bf2c4d4c8af
aleksandr <alexbromel@gmail.com>
2018-12-10 12:05:34 +0300

    Some additions

--- src/it/scala/container/NodeInfo.java
+++
@@ -1,9 +0,0 @@
-package container;
-
-case class NodeInfo(hostRestApiPort: Int,
-                    hostNetworkPort: Int,
-                    containerNetworkPort: Int,
-                    containerApiPort: Int,
-                    apiIpAddress: String,
-                    networkIpAddress: String,
-                    containerId: String)
