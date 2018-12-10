6e994d7434699db907fb255c008b816941fd5df1
timofey <gusev.t.s1@gmail.com>
2018-12-10 12:20:39 +0300

    added additional packages

---
+++ src/it/scala/encry/it/api/NetworkNodeApi.scala
@@ -0,0 +1,21 @@
+package encry.it.api
+
+import java.net.InetSocketAddress
+import encry.it.network.NetworkSender
+import scala.concurrent.{ExecutionContext, Future}
+
+trait NetworkNodeApi {
+
+  def networkAddress: String
+  def networkPort: Int
+  def networkNodeName: String
+  def chainId: Char
+  def nonce: Long = System.currentTimeMillis()
+
+  def sendByNetwork(message: Array[Byte]*)(implicit ec: ExecutionContext): Future[Unit] = {
+    val sender = new NetworkSender(chainId, networkNodeName, nonce)
+    sender.connect(new InetSocketAddress(networkAddress, networkPort)).map { ch =>
+      if (ch.isActive) sender.send(ch, message: _*).map(_ => sender.close()) else sender.close()
+    }
+  }
+}
