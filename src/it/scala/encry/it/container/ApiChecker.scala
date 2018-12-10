acd7ded09ab5f783c2ee641c6784b6c683bf18ac
aleksandr <alexbromel@gmail.com>
2018-12-10 12:14:19 +0300

    Some additions

---
+++ src/it/scala/encry/it/container/ApiChecker.scala
@@ -0,0 +1,5 @@
+package encry.it.container
+
+case class ApiCheckerConfig(apiAddressToCheck: String, specFilePath: String, paramsFilePath: String)
+
+case class ApiChecker(containerId: String, config: ApiCheckerConfig)
