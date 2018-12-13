56405710682883b97a6723b24f5ffb74f1335e3b
aleksandr <alexbromel@gmail.com>
2018-12-13 19:00:56 +0300

    Renaming

--- it/src/test/scala/encry/it/MinerWalletBalanceTest.scala
+++ it/src/test/scala/encry/it/MinerWalletBalanceTest.scala
@@ -8,12 +8,13 @@ import encry.settings.Constants._
 import encry.view.history.History.Height
 import org.encryfoundation.common.Algos
 import org.scalatest.{AsyncFunSuite, Matchers}
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class MinerWalletBalanceTest extends AsyncFunSuite with Matchers {
 
-  test("Miner balance should increase ") {
+  test("Miner balance should increase") {
 
     val heightToCheck = 5
     val supplyAtHeight = (0 to heightToCheck).foldLeft(0: Long) {
@@ -21,16 +22,16 @@ class MinerWalletBalanceTest extends AsyncFunSuite with Matchers {
     }
 
     val docker = Docker()
-    val config = Configs.mining(true)
-      .withFallback(Configs.knownPeers(Seq.empty))
+    val config = ConfigFactory.load
+      .withFallback(Configs.mining(true))
+      .withFallback(Configs.knownPeers(List.empty))
       .withFallback(Configs.offlineGeneration(true))
-      .withFallback(Configs.nodeName("node1234"))
-
-    val nodes = docker.startNodes(Seq(config))
-    val height = nodes.head.waitForHeadersHeight(heightToCheck)
+      .withFallback(Configs.nodeName("node1"))
+    val node = docker.startNodeInternal(config)
+    val height = node.waitForHeadersHeight(heightToCheck)
     Await.result(height, 4.minutes)
     height map { _ =>
-      val res = Await.result(nodes.head.balances, 4.minutes)
+      val res = Await.result(node.balances, 4.minutes)
         .find(_._1 == Algos.encode(IntrinsicTokenId))
         .map(_._2 == supplyAtHeight)
         .get
