6306e8a45bea775e20558fed1a4b4e187655f821
aleksandr <alexbromel@gmail.com>
2018-12-14 11:37:19 +0300

    Renaming

--- it/src/main/scala/encry/it/api/Api.scala
+++ it/src/main/scala/encry/it/api/Api.scala
@@ -1,10 +1,10 @@
-package encry.it
+package encry.it.api
 
 import io.circe.{Decoder, Json}
 import org.asynchttpclient.Response
 import scala.concurrent.{ExecutionContext, Future}
 
-package object api {
+object Api {
   implicit class ResponseFutureExt(val f: Future[Response]) extends AnyVal {
     def as[A: Decoder](implicit ec: ExecutionContext): Future[A] = f.map(r => Json.fromString(r.getResponseBody).as[A].right.get)(ec)
   }
