a9307bcfab6e37502dfd4237413e7dcd2d183dee
aleksandr <alexbromel@gmail.com>
2018-12-24 18:11:48 +0300

    Add revert op to wallet diff

--- src/main/scala/encry/storage/levelDb/forksTree/Diff.scala
+++ src/main/scala/encry/storage/levelDb/forksTree/Diff.scala
@@ -1,15 +1,21 @@
 package encry.storage.levelDb.forksTree
 
+import encry.modifiers.state.StateModifierSerializer
 import encry.modifiers.state.box.EncryBaseBox
 import org.encryfoundation.common.utils.TaggedTypes.ADKey
+import org.iq80.leveldb.DB
 
 trait Diff {
-  def revert: Diff
+  def revert(db: DB): Diff
 }
 
 case class WalletDiff(boxesToRemove: Seq[ADKey],
                       boxesToAdd: Seq[EncryBaseBox],
                       balanceChanges: Map[Array[Byte], Long]) extends Diff {
-  override def revert: Diff =
-    this.copy(balanceChanges = this.balanceChanges.map(assetBalance => assetBalance.copy(_2 = assetBalance._2 * -1)))
+  override def revert(db: DB): Diff =
+    this.copy(
+      boxesToAdd = this.boxesToRemove.map(boxId => StateModifierSerializer.parseBytes(db.get(boxId), boxId.head).get),
+      boxesToRemove = this.boxesToAdd.map(_.id),
+      balanceChanges = this.balanceChanges.map(assetBalance => assetBalance.copy(_2 = assetBalance._2 * -1))
+    )
 }
