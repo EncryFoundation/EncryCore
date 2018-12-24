37a867a1bf83447860cdf8e645f1c2260a80a2f2
aleksandr <alexbromel@gmail.com>
2018-12-24 13:47:58 +0300

    Some additions

--- src/main/scala/encry/storage/levelDb/forksTree/Diff.scala
+++ src/main/scala/encry/storage/levelDb/forksTree/Diff.scala
@@ -1,13 +1,14 @@
 package encry.storage.levelDb.forksTree
 
-import encry.modifiers.state.box.{EncryBox, EncryProposition}
+import encry.modifiers.state.box.EncryBaseBox
+import org.encryfoundation.common.utils.TaggedTypes.ADKey
 
 trait Diff {
   def revert: Diff
 }
 
-case class WalletDiff(boxesToRemove: Seq[EncryBox[EncryProposition]],
-                      boxesToAdd: Seq[EncryBox[EncryProposition]],
+case class WalletDiff(boxesToRemove: Seq[ADKey],
+                      boxesToAdd: Seq[EncryBaseBox],
                       balanceChanges: Map[Array[Byte], Long]) extends Diff {
   override def revert: Diff =
     this.copy(balanceChanges = this.balanceChanges.map(assetBalance => assetBalance.copy(_2 = assetBalance._2 * -1)))
--- src/main/scala/encry/storage/levelDb/forksTree/WalletForksTree.scala
+++ src/main/scala/encry/storage/levelDb/forksTree/WalletForksTree.scala
@@ -2,6 +2,7 @@ package encry.storage.levelDb.forksTree
 import encry.modifiers.NodeViewModifier
 import encry.modifiers.history.Block
 import encry.modifiers.mempool.Transaction
+import encry.utils.BalanceCalculator
 
 class WalletForksTree extends ForksTree {
 
@@ -10,11 +11,13 @@ class WalletForksTree extends ForksTree {
 
 object WalletForksTree {
 
-  def getDiffs(modifier: NodeViewModifier, ): Seq[Diff] = modifier match {
-    case block: Block =>
-      Seq.empty
+  def getDiffs(modifier: NodeViewModifier): Seq[Diff] = modifier match {
+    case block: Block => block.payload.transactions.flatMap(getDiffs)
     case tx: Transaction =>
-    case
+      val toDelete = tx.inputs.map(_.boxId)
+      val toAdd = tx.newBoxes.toList
+      val balances = BalanceCalculator.balanceSheet(toAdd)
+      Seq(WalletDiff(toDelete, toAdd, balances))
     case _ => Seq.empty
   }
 }
