b9a252dd9e94f1b90e25e25896afec8c859925eb
aleksandr <alexbromel@gmail.com>
2018-12-25 13:57:17 +0300

    Add test of applying 35 blocks

--- src/main/scala/encry/storage/levelDb/forksTree/ForksTree.scala
+++ src/main/scala/encry/storage/levelDb/forksTree/ForksTree.scala
@@ -9,7 +9,6 @@ import org.encryfoundation.common.Algos
 import org.encryfoundation.common.Algos.HF
 import org.iq80.leveldb.DB
 import scorex.crypto.hash.Digest32
-
 import scala.util.Try
 
 trait ForksTree[D <: RevertabaleDiff[D]] extends StrictLogging {
@@ -41,14 +40,26 @@ trait ForksTree[D <: RevertabaleDiff[D]] extends StrictLogging {
       .getOrElse(diffs)
   }
 
-  def rollbackTo(rollbackTarger: ModifierId): Try[Unit] = Try {
-    if (modifiersTree.modifierId == rollbackTarger)
-      logger.info(s"Successful rollback to: ${Algos.encode(rollbackTarger)}")
-    else modifiersTree.parent.map{ parent =>
-      modifiersTree = parent.copy(parent = None)
-      rollbackTo(rollbackTarger)
-    }.getOrElse(throw new Exception(s"Impossible to rollback to: ${Algos.encode(rollbackTarger)}"))
+  def rollbackTo(rollbackPoint: ModifierId,
+                 prover: encry.avltree.PersistentBatchAVLProver[Digest32, HF]): Try[Unit] = Try {
+    if (checkRollbackPoint(rollbackPoint)) {
+      val diffs = getDiffsPath(rollbackPoint, persistantProver = prover)
+      if (modifiersTree.modifierId == rollbackPoint) {
+        applyDiff(diffs.tail.foldLeft(diffs.head)(_ ++ _))
+        logger.info(s"Successful rollback to: ${Algos.encode(rollbackPoint)}")
+      }
+      else modifiersTree.parent.map { parent =>
+        modifiersTree = parent.copy(parent = None)
+        rollbackTo(rollbackPoint, prover)
+      }
+    }
+    else throw new Exception(s"Impossible to rollback to: ${Algos.encode(rollbackPoint)}")
   }
+
+  private def checkRollbackPoint(rollbackPoint: ModifierId, node: ForksTreeNode[D] = modifiersTree): Boolean =
+    if (node.modifierId sameElements rollbackPoint) true
+    else if (node.childrens.isEmpty) false
+    else node.childrens.forall(child => checkRollbackPoint(rollbackPoint, child))
 }
 
 object ForksTree {
