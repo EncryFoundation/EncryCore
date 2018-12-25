88986f48753fe836727e2d0da538a02793673ad9
aleksandr <alexbromel@gmail.com>
2018-12-25 12:02:57 +0300

    Add trait revertableDiff

--- src/main/scala/encry/storage/levelDb/forksTree/Diff.scala
+++ src/main/scala/encry/storage/levelDb/forksTree/Diff.scala
@@ -2,21 +2,36 @@ package encry.storage.levelDb.forksTree
 
 import encry.modifiers.state.StateModifierSerializer
 import encry.modifiers.state.box.EncryBaseBox
+import org.encryfoundation.common.Algos.HF
 import org.encryfoundation.common.utils.TaggedTypes.ADKey
-import org.iq80.leveldb.DB
+import scorex.crypto.hash.Digest32
 
-trait Diff {
-  def revert(db: DB): Diff
+trait Diff
+
+trait RevertabaleDiff[D <: Diff] extends Diff {
+
+  def ++(diff: D): D
+
+  def revert(persistantProver: encry.avltree.PersistentBatchAVLProver[Digest32, HF]): D
 }
 
 case class WalletDiff(boxesToRemove: Seq[ADKey],
                       boxesToAdd: Seq[EncryBaseBox],
-                      balanceChanges: Map[Array[Byte], Long]) extends Diff {
-  override def revert(db: DB): Diff =
+                      balanceChanges: Map[Array[Byte], Long]) extends RevertabaleDiff[WalletDiff] {
+  override def revert(persistantProver: encry.avltree.PersistentBatchAVLProver[Digest32, HF]): WalletDiff =
     this.copy(
-      boxesToAdd = this.boxesToRemove.map(boxId => StateModifierSerializer.parseBytes(db.get("cache".getBytes ++ boxId),
-        boxId.head).get),
+      boxesToAdd =
+        this.boxesToRemove
+          .map(boxId => persistantProver.unauthenticatedLookup(boxId)
+            .map(bytes => StateModifierSerializer.parseBytes(bytes, boxId.head)).get.get),
       boxesToRemove = this.boxesToAdd.map(_.id),
       balanceChanges = this.balanceChanges.map(assetBalance => assetBalance.copy(_2 = assetBalance._2 * -1))
+    )
+
+  override def ++(diff: WalletDiff): WalletDiff =
+    WalletDiff(
+      this.boxesToRemove ++ diff.boxesToRemove,
+      this.boxesToAdd ++ diff.boxesToAdd,
+      this.balanceChanges ++ diff.balanceChanges
     )
 }
--- src/test/scala/encry/storage/levelDb/forksTree/WalletForksTreeTest.scala
+++ src/test/scala/encry/storage/levelDb/forksTree/WalletForksTreeTest.scala
@@ -1,11 +1,7 @@
 package encry.storage.levelDb.forksTree
 
-import java.io.File
-
 import encry.modifiers.InstanceFactory
-import encry.modifiers.history.Block
-import encry.modifiers.state.StateModifierSerializer
-import encry.modifiers.state.box.{AssetBox, EncryBaseBox}
+import encry.modifiers.state.box.AssetBox
 import encry.settings.{Constants, EncryAppSettings}
 import encry.utils.{EncryGenerator, FileHelper}
 import org.encryfoundation.common.Algos
@@ -13,27 +9,6 @@ import org.iq80.leveldb.{DB, Options}
 import org.scalatest.{Matchers, PropSpec}
 
 class WalletForksTreeTest extends PropSpec with Matchers with EncryGenerator with InstanceFactory {
-
-  property("Adding boxes to cache") {
-
-    val settings = EncryAppSettings.read
-
-    val db: DB = {
-      val dir = FileHelper.getRandomTempDir
-      if (!dir.exists()) dir.mkdirs()
-      if (!dir.isDirectory) throw new IllegalArgumentException(s"Can't find directory at '${settings.directory}/walletLevelDB'")
-      LevelDbFactory.factory.open(dir, new Options)
-    }
-
-    val walletTree = WalletForksTree(db)
-
-    val boxes = (0 to 100).foldLeft(Seq.empty[EncryBaseBox])( (boxes, _) => boxes :+ genAssetBox(randomAddress))
-
-    walletTree.addBoxesToCache(boxes)
-
-    StateModifierSerializer
-      .parseBytes(db.get("cache".getBytes ++ boxes.head.id), boxes.head.id.head).get.id shouldEqual boxes.head.id
-  }
 
   property("Dummy block with 1 coinbase tx applying. Wallet balance should increase") {
 
