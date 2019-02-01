619d36e3ebb809711854172d48ac338546ccd800
timofey <gusev.t.s1@gmail.com>
2019-02-01 18:40:15 +0300

    code cleanup

--- it/src/main/scala/encry/it/docker/NodesFromDocker.scala
+++ it/src/main/scala/encry/it/docker/NodesFromDocker.scala
@@ -1,12 +1,17 @@
 package encry.it.docker
 
+import com.typesafe.config.Config
 import monix.eval.Coeval
 import org.scalatest.Suite
 
-trait NodesFromDocker extends Nodes with DockerAfterAll { _: Suite =>
+trait NodesFromDocker extends DockerAfterAll {
+  _: Suite =>
+
+  protected def nodeConfigs: Seq[Config]
+
   protected val dockerNodes: Coeval[Seq[Node]] = dockerSingleton
     .map(_.startNodes(nodeConfigs))
     .memoize
 
-  override protected def nodes: Seq[Node] = dockerNodes()
+  protected def nodes: Seq[Node] = dockerNodes()
 }
