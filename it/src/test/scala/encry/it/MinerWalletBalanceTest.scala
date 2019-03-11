cc46d9ec6aa7024ef62b2e0d9f955b0ba2b0f2d7
timofey <gusev.t.s1@gmail.com>
2019-03-11 10:14:31 +0300

    code cleanup

--- it/src/test/scala/encry/it/MinerWalletBalanceTest.scala
+++ it/src/test/scala/encry/it/MinerWalletBalanceTest.scala
@@ -4,12 +4,10 @@ import com.typesafe.config.Config
 import encry.consensus.EncrySupplyController
 import encry.it.configs.Configs
 import encry.it.docker.NodesFromDocker
-import encry.modifiers.state.box.{AssetBox, EncryBaseBox}
 import encry.settings.Constants._
 import encry.view.history.History.Height
 import org.encryfoundation.common.Algos
 import org.scalatest.{AsyncFunSuite, Matchers}
-
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
@@ -28,8 +26,6 @@ class MinerWalletBalanceTest extends AsyncFunSuite with Matchers with NodesFromD
 
     val height = dockerNodes().head.waitForHeadersHeight(heightToCheck)
     Await.result(height, 30.minutes)
-    val boxes: Seq[EncryBaseBox] = Await.result(dockerNodes().head.outputs, 30.minutes)
-    val oneBox: AssetBox = boxes.collect { case ab: AssetBox => ab }.head
     height map { _ =>
       val res = Await.result(dockerNodes().head.balances, 30.minutes)
         .find(_._1 == Algos.encode(IntrinsicTokenId))
