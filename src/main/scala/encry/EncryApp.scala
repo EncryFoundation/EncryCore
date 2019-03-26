bb2b88183d9b64fb1af89a3f17810c2b6132ca21
aleksandr <alexbromel@gmail.com>
2019-03-26 22:42:29 +0300

    add mailbox configuration to nvsh

--- src/main/scala/encry/EncryApp.scala
+++ src/main/scala/encry/EncryApp.scala
@@ -56,7 +56,8 @@ object EncryApp extends App with StrictLogging {
     .withDispatcher("network-dispatcher"), "networkController")
   lazy val peerManager: ActorRef = system.actorOf(Props(classOf[PeerManager]), "peerManager")
   lazy val nodeViewSynchronizer: ActorRef = system.actorOf(
-    Props(classOf[NodeViewSynchronizer], influxRef, nodeViewHolder, networkController, system, settings),
+    Props(classOf[NodeViewSynchronizer], influxRef, nodeViewHolder, networkController, system, settings)
+    .withMailbox(""),
     "nodeViewSynchronizer")
   lazy val miner: ActorRef = system.actorOf(Props[Miner], "miner")
   if (settings.monitoringSettings.exists(_.kamonEnabled)) {
