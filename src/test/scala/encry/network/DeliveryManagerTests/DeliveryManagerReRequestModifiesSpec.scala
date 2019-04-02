0fcc9d2d1a4719c67db7aaf4fb34310773c2b2fd
timofey <gusev.t.s1@gmail.com>
2019-04-02 20:50:18 +0300

    fixed tests

--- src/test/scala/encry/network/DeliveryManagerTests/DeliveryManagerReRequestModifiesSpec.scala
+++ src/test/scala/encry/network/DeliveryManagerTests/DeliveryManagerReRequestModifiesSpec.scala
@@ -1,14 +1,14 @@
 package encry.network.DeliveryManagerTests
 
 import java.net.InetSocketAddress
-
 import akka.actor.ActorSystem
-import akka.testkit.TestProbe
+import akka.testkit.{TestActorRef, TestProbe}
 import encry.consensus.History.Older
 import encry.modifiers.InstanceFactory
 import encry.modifiers.history.{Block, Header}
 import encry.modifiers.mempool.Transaction
 import encry.network.BasicMessagesRepo.{Handshake, RequestModifiersNetworkMessage}
+import encry.network.DeliveryManager
 import encry.network.DeliveryManagerTests.DMUtils.{generateBlocks, initialiseDeliveryManager, toKey}
 import encry.network.NodeViewSynchronizer.ReceivableMessages.{CheckDelivery, HandshakedPeer, OtherNodeSyncingStatus, RequestFromLocal}
 import encry.network.PeerConnectionHandler.{ConnectedPeer, Incoming}
@@ -16,23 +16,24 @@ import encry.settings.EncryAppSettings
 import encry.utils.CoreTaggedTypes.ModifierId
 import encry.view.history.EncryHistory
 import org.scalatest.{BeforeAndAfterAll, Matchers, OneInstancePerTest, WordSpecLike}
-
+import scala.concurrent.duration._
 import scala.collection.mutable.WrappedArray
 
-class DeliveryManagerReRequestModifiesSpec extends WordSpecLike with BeforeAndAfterAll
+class DeliveryManagerReRequestModifiesSpec extends WordSpecLike
+  with BeforeAndAfterAll
   with Matchers
   with InstanceFactory
   with OneInstancePerTest {
 
   implicit val system: ActorSystem = ActorSystem("SynchronousTestingSpec")
-  val settings: EncryAppSettings = EncryAppSettings.read
+  val settings: EncryAppSettings = DummyEncryAppSettingsReader.read
 
   override def afterAll(): Unit = system.terminate()
 
   "ReRequestModifies" should {
     val testProbeActor: TestProbe = TestProbe()
     val initialState = initialiseDeliveryManager(isBlockChainSynced = true, isMining = true, settings)
-    val deliveryManager = initialState._1
+    val deliveryManager: TestActorRef[DeliveryManager] = initialState._1
     val newPeer = new InetSocketAddress("172.16.13.10", 9001)
     val peer: ConnectedPeer = ConnectedPeer(newPeer, testProbeActor.ref, Incoming,
       Handshake(protocolToBytes(settings.network.appVersion), "peer", Some(newPeer), System.currentTimeMillis()))
@@ -41,49 +42,48 @@ class DeliveryManagerReRequestModifiesSpec extends WordSpecLike with BeforeAndAf
     deliveryManager ! HandshakedPeer(peer)
     deliveryManager ! OtherNodeSyncingStatus(peer, Older, None)
 
-    "re-ask necessary modifiers correctly" in {
-      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, headerIds)
-      headerIds.foreach { id =>
-        deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, id)
-        assert(deliveryManager.underlyingActor.expectedModifiers.getOrElse(peer.socketAddress.getAddress, Map.empty)
-          .find(mId => mId._1 == toKey(id)).forall(elem => elem._2._2 == 2))
-      }
+    "re-ask necessary modifier several times (number of attempts from settings) and remove modifier from " +
+      "expectedModifiers collection after all the attempts will expire" in {
+      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(headerIds.head))
+      //settings.network.maxDeliveryChecks + (1) is using for expecting CheckDelivery which will trigger modifier removal
+      testProbeActor.receiveN(settings.network.maxDeliveryChecks + 1,
+        ((settings.network.maxDeliveryChecks + 1) * settings.network.deliveryTimeout._1).seconds)
+      //this thread sleep is using for expecting modifier removal
+      Thread.sleep((settings.network.deliveryTimeout._1 + 1) * 1000)
+      assert(deliveryManager.underlyingActor.expectedModifiers.getOrElse(newPeer.getAddress, Map.empty).isEmpty)
     }
     "don't re-ask unnecessary modifiers" in {
-      generateBlocks(10, generateDummyHistory(settings))._2.map(_.header.id)
-        .foreach { id =>
-          deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, id)
-          assert(deliveryManager.underlyingActor.expectedModifiers
-            .getOrElse(peer.socketAddress.getAddress, Map.empty).isEmpty)
-        }
+      (0 to headerIds.size).foreach { _ =>
+        deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, headerIds.head)
+        testProbeActor.expectNoMsg(0.5.seconds)
+        assert(deliveryManager.underlyingActor.expectedModifiers
+          .getOrElse(peer.socketAddress.getAddress, Map.empty).isEmpty)
+      }
     }
-    "remove expired modifier form awaiting collection" in {
-      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, headerIds)
+    "remove expired modifier from expectedModifiers collection" in {
+      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(headerIds.head))
       (0 to settings.network.maxDeliveryChecks).foreach(_ =>
         deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, headerIds.head))
       assert(deliveryManager.underlyingActor.expectedModifiers
-        .getOrElse(peer.socketAddress.getAddress, Map.empty).size == 9)
+        .getOrElse(peer.socketAddress.getAddress, Map.empty).isEmpty)
     }
     "correctly handle reRequest for modifier which number of tries has expired" in {
-      val block: Block = generateBlocks(1, generateDummyHistory(settings))._2.head
-      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(block.header.id))
+      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(headerIds.head))
       (0 until settings.network.maxDeliveryChecks).foreach(_ =>
-        deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, block.header.id))
-      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(block.header.id))
-      testProbeActor.expectMsg(RequestModifiersNetworkMessage(Header.modifierTypeId -> Seq(block.header.id)))
+        deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, headerIds.head))
+      deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(headerIds.head))
+      testProbeActor.expectMsg(RequestModifiersNetworkMessage(Header.modifierTypeId -> Seq(headerIds.head)))
       assert(deliveryManager.underlyingActor.expectedModifiers.size == 1)
     }
-
-    "remove peer if expected modifiers collection from this peer is empty" in {
+    "remove peer from expectedModifiers if expected modifiers collection from this peer is empty" in {
       deliveryManager ! RequestFromLocal(peer, Header.modifierTypeId, Seq(headerIds.head))
       (0 to settings.network.maxDeliveryChecks).foreach(_ =>
         deliveryManager ! CheckDelivery(peer, Header.modifierTypeId, headerIds.head))
       assert(deliveryManager.underlyingActor.expectedModifiers
         .getOrElse(peer.socketAddress.getAddress, Map.empty) == Map.empty)
     }
-
-    "remove reRequested transactions from expected modifiers" in {
-      val transactions: Seq[ModifierId] = genValidPaymentTxs(10).map(_.id)
+    "remove reRequested transactions from expected modifiers if CheckDelivery " in {
+      val transactions: Seq[ModifierId] = genValidPaymentTxs(2).map(_.id)
       val txsToKey: Seq[WrappedArray.ofByte] = transactions.map(toKey)
       deliveryManager ! RequestFromLocal(peer, Transaction.ModifierTypeId, transactions)
       deliveryManager ! CheckDelivery(peer, Transaction.ModifierTypeId, transactions.head)
