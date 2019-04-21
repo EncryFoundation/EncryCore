93a902dcceba0aa31d5d16724f70e7f73a724c20
timofey <gusev.t.s1@gmail.com>
2019-04-21 19:49:24 +0300

    some additions

--- src/main/scala/encry/local/miner/Miner.scala
+++ src/main/scala/encry/local/miner/Miner.scala
@@ -2,7 +2,6 @@ package encry.local.miner
 
 import java.text.SimpleDateFormat
 import java.util.Date
-
 import akka.actor.{Actor, Props}
 import com.typesafe.scalalogging.StrictLogging
 import encry.EncryApp._
@@ -30,11 +29,9 @@ import org.encryfoundation.common.crypto.PrivateKey25519
 import org.encryfoundation.common.utils.TaggedTypes.{ADDigest, SerializedAdProof}
 import akka.util.Timeout
 import encry.utils.CoreTaggedTypes.ModifierId
-import akka.pattern.ask
 import encry.view.mempool.Mempool._
 import scala.concurrent.duration._
 import scala.collection._
-import scala.concurrent.{Await, Future}
 
 class Miner extends Actor with StrictLogging {
 
@@ -141,7 +138,7 @@ class Miner extends Actor with StrictLogging {
   }
 
   def unknownMessage: Receive = {
-    case m => logger.warn(s"Unexpected message $m")
+    case m => logger.debug(s"Unexpected message $m")
   }
 
   def chainEvents: Receive = {
