406ca24d5236ceb9c1f31ebecafe292fa9963e5e
Timofey <gusev.t.s1@gmail.com>
2019-05-16 14:47:42 +0300

    ec-669 (#324)
    * added stat
    
    * jenkins
    
    * rebased

--- src/main/scala/encry/local/miner/Miner.scala
+++ src/main/scala/encry/local/miner/Miner.scala
@@ -15,7 +15,7 @@ import encry.modifiers.state.box.Box.Amount
 import encry.network.DeliveryManager.FullBlockChainIsSynced
 import encry.network.NodeViewSynchronizer.ReceivableMessages.SemanticallySuccessfulModifier
 import encry.settings.Constants
-import encry.stats.StatsSender.{CandidateProducingTime, MiningEnd, MiningTime, SleepTime}
+import encry.stats.StatsSender._
 import encry.utils.CoreTaggedTypes.ModifierId
 import encry.utils.NetworkTime.Time
 import encry.view.EncryNodeViewHolder.CurrentView
@@ -52,8 +52,12 @@ class Miner extends Actor with StrictLogging {
   val powScheme: EquihashPowScheme = EquihashPowScheme(Constants.Equihash.n, Constants.Equihash.k)
   var transactionsPool: IndexedSeq[Transaction] = IndexedSeq.empty[Transaction]
 
-  override def preStart(): Unit =
+  override def preStart(): Unit = {
     context.system.eventStream.subscribe(self, classOf[SemanticallySuccessfulModifier[_]])
+    context.system.scheduler.schedule(5.seconds, 1.seconds)(
+      influxRef.foreach(_ ! InfoAboutTxsFromMiner(transactionsPool.size))
+    )
+  }
 
   override def postStop(): Unit = killAllWorkers()
 
@@ -224,6 +228,7 @@ object Miner {
 
   case object DisableMining
 
+
   case object EnableMining
 
   case object StartMining
@@ -241,13 +246,13 @@ object Miner {
 
   case class MinerStatus(isMining: Boolean, candidateBlock: Option[CandidateBlock]) {
     lazy val json: Json = Map(
-      "isMining" -> isMining.asJson,
+      "isMining"       -> isMining.asJson,
       "candidateBlock" -> candidateBlock.map(_.asJson).getOrElse("None".asJson)
     ).asJson
   }
 
   implicit val jsonEncoder: Encoder[MinerStatus] = (r: MinerStatus) => Map(
-    "isMining" -> r.isMining.asJson,
+    "isMining"       -> r.isMining.asJson,
     "candidateBlock" -> r.candidateBlock.map(_.asJson).getOrElse("None".asJson)
   ).asJson
 }
