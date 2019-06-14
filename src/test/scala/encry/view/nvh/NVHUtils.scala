e713282317da455fddc7cf977a61ae2c7ab55245
aleksandr <alexbromel@gmail.com>
2019-06-14 17:00:18 +0300

    add test

---
+++ src/test/scala/encry/view/nvh/NVHUtils.scala
@@ -0,0 +1,25 @@
+package encry.view.nvh
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestActor, TestActorRef, TestProbe}
+import encry.EncryApp.settings
+import encry.settings.EncryAppSettings
+import encry.utils.NetworkTimeProvider
+import encry.view.EncryNodeViewHolder
+import encry.view.state.UtxoState
+
+object NVHUtils {
+
+  def initNvh(settings: EncryAppSettings)(implicit actorSystem: ActorSystem): TestActorRef[EncryNodeViewHolder[UtxoState]] = {
+    val fakeActorRef = TestProbe()
+    val ntp: NetworkTimeProvider = new NetworkTimeProvider(settings.ntp)
+    TestActorRef[EncryNodeViewHolder[UtxoState]](EncryNodeViewHolder.props(
+      settings,
+      ntp,
+      fakeActorRef.ref,
+      None,
+      fakeActorRef.ref,
+      fakeActorRef.ref
+    ))
+  }
+}
---
+++ src/test/scala/encry/view/nvh/NodeViewHolderTest.scala
@@ -0,0 +1,109 @@
+package encry.view.nvh
+
+import java.io.File
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import encry.modifiers.InstanceFactory
+import encry.network.DeliveryManagerTests.DummyEncryAppSettingsReader
+import encry.settings.EncryAppSettings
+import encry.storage.VersionalStorage
+import encry.utils.CoreTaggedTypes.VersionTag
+import encry.view.EncryNodeViewHolder.ReceivableMessages.ModifiersFromRemote
+import encry.view.state.{BoxHolder, EncryState, UtxoState}
+import org.encryfoundation.common.modifiers.history._
+import org.encryfoundation.common.modifiers.state.box.{AssetBox, EncryBaseBox}
+import org.encryfoundation.common.utils.Algos
+import org.encryfoundation.common.utils.TaggedTypes.Difficulty
+import org.scalatest.{BeforeAndAfterAll, Matchers, OneInstancePerTest, WordSpecLike}
+
+import scala.concurrent.Await
+
+//class NodeViewHolderTest extends WordSpecLike
+//  with BeforeAndAfterAll
+//  with Matchers
+//  with InstanceFactory
+//  with OneInstancePerTest {
+//
+//  implicit val system: ActorSystem = ActorSystem("NodeViewHolderSpec")
+//  val settings: EncryAppSettings = DummyEncryAppSettingsReader.read
+//
+//  override def afterAll: Unit = TestKit.shutdownActorSystem(system)
+//
+//  "nvh" should {
+//    "not stock at rollback" in {
+//
+//      val tmpDirForNvh: File = getRandomTempDir
+//
+//      val nvh = NVHUtils.initNvh(settings.copy(directory = tmpDirForNvh.getAbsolutePath))
+//      val tmpDir: File = getRandomTempDir
+//
+//      val initialBoxes: IndexedSeq[AssetBox] = EncryState.initialStateBoxes.toIndexedSeq
+//      val boxesHolder: BoxHolder = BoxHolder(initialBoxes)
+//      var state: UtxoState = utxoFromBoxHolder(boxesHolder, tmpDir, None, settings, VersionalStorage.IODB)
+//      val genesisBlock: Block = generateGenesisBlockValidForState(state)
+//
+//      state = state.applyModifier(genesisBlock).get
+//
+//      //logger.info(s"digest root: ${Algos.encode(state.persistentProver.digest)}")
+//
+//      val stateGenerationResults: (List[(Block, Block)], Block, UtxoState, IndexedSeq[AssetBox], List[VersionTag]) =
+//        (0 to 400).foldLeft(List.empty[(Block, Block)], genesisBlock, state, initialBoxes, List.empty[VersionTag]) {
+//          case ((blocks, block, stateL, boxes, versions), i) =>
+//            val nextBlockMainChain: Block = generateNextBlockForStateWithSpendingAllPreviousBoxes(
+//              block,
+//              stateL,
+//              block.payload.txs.flatMap(_.newBoxes.map(_.asInstanceOf[AssetBox])).toIndexedSeq,
+//              isLast = i == 400
+//            )
+//            val nextBlockFork: Block = generateNextBlockForStateWithSpendingAllPreviousBoxes(
+//              block,
+//              stateL,
+//              block.payload.txs.flatMap(_.newBoxes.map(_.asInstanceOf[AssetBox])).toIndexedSeq,
+//              addDiff = Difficulty @@ BigInt(100),
+//              isLast = i == 400
+//            )
+//            val stateN: UtxoState = stateL.applyModifier(nextBlockMainChain).get
+//            (blocks :+ (nextBlockMainChain, nextBlockFork),
+//              nextBlockMainChain,
+//              stateN,
+//              boxes.drop(2),
+//              versions :+ stateN.version
+//            )
+//        }
+//      val chain: List[Block] = genesisBlock +: stateGenerationResults._1.map(_._1)
+//
+//      val stateAfterRollback = stateGenerationResults._3.rollbackTo(stateGenerationResults._5.dropRight(1).last).get
+//      val newState = stateAfterRollback.applyModifier(stateGenerationResults._1.last._2).get
+//      val newBlock: Block = generateNextBlockForStateWithSpendingAllPreviousBoxes(
+//        stateGenerationResults._1.last._2,
+//        newState,
+//        stateGenerationResults._1.last._2.payload.txs.flatMap(_.newBoxes.map(_.asInstanceOf[AssetBox])).toIndexedSeq
+//      )
+//
+//      println(s"genesisBlockHeader: ${genesisBlock.header}. Payload id: ${genesisBlock.payload.encodedId}")
+//      println(s"nextBlock: ${chain(1).header}")
+//
+//      chain.foreach{block =>
+//        nvh ! ModifiersFromRemote(Header.modifierTypeId, Seq(HeaderProtoSerializer.toProto(block.header).toByteArray))
+//        //nvh ! ModifiersFromRemote(Payload.modifierTypeId, Seq(PayloadProtoSerializer.toProto(block.payload).toByteArray))
+//      }
+//
+//      chain.foreach{block =>
+//        nvh ! ModifiersFromRemote(Payload.modifierTypeId, Seq(PayloadProtoSerializer.toProto(block.payload).toByteArray))
+//      }
+//
+//      val forkBlock = stateGenerationResults._1.last._2
+//
+//      nvh ! ModifiersFromRemote(Header.modifierTypeId, Seq(HeaderProtoSerializer.toProto(forkBlock.header).toByteArray))
+//      nvh ! ModifiersFromRemote(Payload.modifierTypeId, Seq(PayloadProtoSerializer.toProto(forkBlock.payload).toByteArray))
+//
+//      nvh.underlyingActor.nodeView.state.persistentProver.digest
+//
+//      nvh ! ModifiersFromRemote(Header.modifierTypeId, Seq(HeaderProtoSerializer.toProto(newBlock.header).toByteArray))
+//      nvh ! ModifiersFromRemote(Payload.modifierTypeId, Seq(PayloadProtoSerializer.toProto(newBlock.payload).toByteArray))
+//
+//      Thread.sleep(5000)
+//    }
+//  }
+//}
