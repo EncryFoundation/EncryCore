80dd93113f4279aca7f63bfa9e7b8c9d63a936dd
aleksandr <alexbromel@gmail.com>
2019-06-27 13:09:26 +0300

    set adProofsRoot, stateRoot to Array.emptyByteArray after deser from proto msg

--- src/main/scala/encry/network/ModifiersToNetworkUtils.scala
+++ src/main/scala/encry/network/ModifiersToNetworkUtils.scala
@@ -3,13 +3,16 @@ package encry.network
 import BlockProto.BlockProtoMessage.AdProofsProtoMessage
 import HeaderProto.HeaderProtoMessage
 import PayloadProto.PayloadProtoMessage
+import com.typesafe.scalalogging.StrictLogging
 import encry.modifiers.history.{HeaderUtils, PayloadUtils}
 import org.encryfoundation.common.modifiers.PersistentModifier
 import org.encryfoundation.common.modifiers.history.{ADProofs, ADProofsProtoSerializer, Header, HeaderProtoSerializer, Payload, PayloadProtoSerializer}
-import org.encryfoundation.common.utils.TaggedTypes.ModifierTypeId
+import org.encryfoundation.common.utils.TaggedTypes.{ADDigest, ModifierTypeId}
+import scorex.crypto.hash.Digest32
+
 import scala.util.{Failure, Try}
 
-object ModifiersToNetworkUtils {
+object ModifiersToNetworkUtils extends StrictLogging {
 
   def toProto(modifier: PersistentModifier): Array[Byte] = modifier match {
     case m: Header   => HeaderProtoSerializer.toProto(m).toByteArray
@@ -20,6 +23,7 @@ object ModifiersToNetworkUtils {
 
   def fromProto(modType: ModifierTypeId, bytes: Array[Byte]): Try[PersistentModifier] = Try(modType match {
     case Header.modifierTypeId   => HeaderProtoSerializer.fromProto(HeaderProtoMessage.parseFrom(bytes))
+      .map(_.copy(adProofsRoot = Digest32 @@ Array.emptyByteArray, stateRoot = ADDigest @@ Array.emptyByteArray))
     case ADProofs.modifierTypeId => Try(ADProofsProtoSerializer.fromProto(AdProofsProtoMessage.parseFrom(bytes)))
     case Payload.modifierTypeId  => PayloadProtoSerializer.fromProto(PayloadProtoMessage.parseFrom(bytes))
     case m                       => Failure(new RuntimeException(s"Try to deserialize unknown modifier: $m from proto."))
