cdff216edabb053c439b937d6790c797174805fa
aleksandr <alexbromel@gmail.com>
2019-07-10 17:29:38 +0300

    fix benches

--- benchmarks/src/test/scala/benches/StateBenches.scala
+++ benchmarks/src/test/scala/benches/StateBenches.scala
@@ -1,110 +1,109 @@
 package benches
 
-//import java.io.File
-//import java.util.concurrent.TimeUnit
-//
-//import benches.StateBenches.StateBenchState
-//import org.openjdk.jmh.annotations._
-//import benches.Utils._
-//import encry.settings.EncryAppSettings
-//import encry.storage.VersionalStorage
-//import encry.storage.VersionalStorage.IODB
-//import encry.view.state.{BoxHolder, UtxoState}
-//import encryBenchmark.Settings
-//import org.encryfoundation.common.modifiers.history.Block
-//import org.encryfoundation.common.modifiers.state.box.AssetBox
-//import org.openjdk.jmh.infra.Blackhole
-//import org.openjdk.jmh.profile.GCProfiler
-//import org.openjdk.jmh.runner.{Runner, RunnerException}
-//import org.openjdk.jmh.runner.options.{OptionsBuilder, TimeValue, VerboseMode}
+import java.io.File
+import java.util.concurrent.TimeUnit
+import benches.StateBenches.StateBenchState
+import org.openjdk.jmh.annotations._
+import benches.Utils._
+import encry.settings.EncryAppSettings
+import encry.storage.VersionalStorage
+import encry.storage.VersionalStorage.IODB
+import encry.view.state.{BoxHolder, UtxoState}
+import encryBenchmark.Settings
+import org.encryfoundation.common.modifiers.history.Block
+import org.encryfoundation.common.modifiers.state.box.AssetBox
+import org.openjdk.jmh.infra.Blackhole
+import org.openjdk.jmh.profile.GCProfiler
+import org.openjdk.jmh.runner.{Runner, RunnerException}
+import org.openjdk.jmh.runner.options.{OptionsBuilder, TimeValue, VerboseMode}
 
-//class StateBenches {
-//
-//  @Benchmark
-//  def applyBlocksToTheState(stateBench: StateBenchState, bh: Blackhole): Unit = {
-//    bh.consume {
-//      val innerState: UtxoState =
-//        utxoFromBoxHolder(stateBench.boxesHolder, getRandomTempDir, None, stateBench.settings, VersionalStorage.LevelDB)
-//      stateBench.chain.foldLeft(innerState) { case (state, block) =>
-//        state.applyModifier(block).get
-//      }
-//      innerState.closeStorage()
-//    }
-//  }
-//
-//  @Benchmark
-//  def readStateFileBench(stateBench: StateBenchState, bh: Blackhole): Unit = {
-//    bh.consume {
-//      val localState: UtxoState =
-//        utxoFromBoxHolder(stateBench.boxesHolder, stateBench.tmpDir, None, stateBench.settings, IODB)
-//      localState.closeStorage()
-//    }
-//  }
-//}
-//
-//object StateBenches {
-//
-//  val benchSettings: Settings = Settings.read
-//
-//  @throws[RunnerException]
-//  def main(args: Array[String]): Unit = {
-//    val opt = new OptionsBuilder()
-//      .include(".*" + classOf[StateBenches].getSimpleName + ".*")
-//      .forks(1)
-//      .threads(1)
-//      .warmupIterations(benchSettings.benchesSettings.warmUpIterations)
-//      .measurementIterations(benchSettings.benchesSettings.measurementIterations)
-//      .mode(Mode.AverageTime)
-//      .timeUnit(TimeUnit.SECONDS)
-//      .verbosity(VerboseMode.EXTRA)
-//      .addProfiler(classOf[GCProfiler])
-//      .warmupTime(TimeValue.milliseconds(benchSettings.benchesSettings.warmUpTime))
-//      .measurementTime(TimeValue.milliseconds(benchSettings.benchesSettings.measurementTime))
-//      .build
-//    new Runner(opt).run
-//  }
-//
-//  @State(Scope.Benchmark)
-//  class StateBenchState {
-//
-//    val settings: EncryAppSettings = EncryAppSettings.read
-//    val tmpDir: File = getRandomTempDir
-//
-//    val initialBoxes: IndexedSeq[AssetBox] = (0 until benchSettings.stateBenchSettings.totalBoxesNumber).map(nonce =>
-//      genHardcodedBox(privKey.publicImage.address.address, nonce)
-//    )
-//    val boxesHolder: BoxHolder = BoxHolder(initialBoxes)
-//    var state: UtxoState = utxoFromBoxHolder(boxesHolder, tmpDir, None, settings, VersionalStorage.LevelDB)
-//    val genesisBlock: Block = generateGenesisBlockValidForState(state)
-//
-//    state = state.applyModifier(genesisBlock).get
-//
-//    val stateGenerationResults: (Vector[Block], Block, UtxoState, IndexedSeq[AssetBox]) =
-//      (0 until benchSettings.stateBenchSettings.blocksNumber).foldLeft(Vector[Block](), genesisBlock, state, initialBoxes) {
-//        case ((blocks, block, stateL, boxes), _) =>
-//          val nextBlock: Block = generateNextBlockValidForState(
-//            block,
-//            stateL,
-//            boxes.take(
-//              benchSettings.stateBenchSettings.transactionsNumberInEachBlock *
-//                benchSettings.stateBenchSettings.numberOfInputsInOneTransaction
-//            ),
-//            benchSettings.stateBenchSettings.transactionsNumberInEachBlock,
-//            benchSettings.stateBenchSettings.numberOfInputsInOneTransaction,
-//            benchSettings.stateBenchSettings.numberOfOutputsInOneTransaction
-//          )
-//          val stateN: UtxoState = stateL.applyModifier(nextBlock).get
-//          (blocks :+ nextBlock,
-//            nextBlock,
-//            stateN,
-//            boxes.drop(
-//              benchSettings.stateBenchSettings.transactionsNumberInEachBlock *
-//                benchSettings.stateBenchSettings.numberOfInputsInOneTransaction)
-//          )
-//      }
-//
-//    val chain: Vector[Block] = genesisBlock +: stateGenerationResults._1
-//    state = stateGenerationResults._3
-//    state.closeStorage()
-//  }
-//}
+class StateBenches {
+
+  @Benchmark
+  def applyBlocksToTheState(stateBench: StateBenchState, bh: Blackhole): Unit = {
+    bh.consume {
+      val innerState: UtxoState =
+        utxoFromBoxHolder(stateBench.boxesHolder, getRandomTempDir, None, stateBench.settings, VersionalStorage.LevelDB)
+      stateBench.chain.foldLeft(innerState) { case (state, block) =>
+        state.applyModifier(block).right.get
+      }
+      innerState.close()
+    }
+  }
+
+  @Benchmark
+  def readStateFileBench(stateBench: StateBenchState, bh: Blackhole): Unit = {
+    bh.consume {
+      val localState: UtxoState =
+        utxoFromBoxHolder(stateBench.boxesHolder, stateBench.tmpDir, None, stateBench.settings, IODB)
+      localState.close()
+    }
+  }
+}
+
+object StateBenches {
+
+  val benchSettings: Settings = Settings.read
+
+  @throws[RunnerException]
+  def main(args: Array[String]): Unit = {
+    val opt = new OptionsBuilder()
+      .include(".*" + classOf[StateBenches].getSimpleName + ".*")
+      .forks(1)
+      .threads(1)
+      .warmupIterations(benchSettings.benchesSettings.warmUpIterations)
+      .measurementIterations(benchSettings.benchesSettings.measurementIterations)
+      .mode(Mode.AverageTime)
+      .timeUnit(TimeUnit.SECONDS)
+      .verbosity(VerboseMode.EXTRA)
+      .addProfiler(classOf[GCProfiler])
+      .warmupTime(TimeValue.milliseconds(benchSettings.benchesSettings.warmUpTime))
+      .measurementTime(TimeValue.milliseconds(benchSettings.benchesSettings.measurementTime))
+      .build
+    new Runner(opt).run
+  }
+
+  @State(Scope.Benchmark)
+  class StateBenchState {
+
+    val settings: EncryAppSettings = EncryAppSettings.read
+    val tmpDir: File = getRandomTempDir
+
+    val initialBoxes: IndexedSeq[AssetBox] = (0 until benchSettings.stateBenchSettings.totalBoxesNumber).map(nonce =>
+      genHardcodedBox(privKey.publicImage.address.address, nonce)
+    )
+    val boxesHolder: BoxHolder = BoxHolder(initialBoxes)
+    var state: UtxoState = utxoFromBoxHolder(boxesHolder, tmpDir, None, settings, VersionalStorage.LevelDB)
+    val genesisBlock: Block = generateGenesisBlockValidForState(state)
+
+    state = state.applyModifier(genesisBlock).right.get
+
+    val stateGenerationResults: (Vector[Block], Block, UtxoState, IndexedSeq[AssetBox]) =
+      (0 until benchSettings.stateBenchSettings.blocksNumber).foldLeft(Vector[Block](), genesisBlock, state, initialBoxes) {
+        case ((blocks, block, stateL, boxes), _) =>
+          val nextBlock: Block = generateNextBlockValidForState(
+            block,
+            stateL,
+            boxes.take(
+              benchSettings.stateBenchSettings.transactionsNumberInEachBlock *
+                benchSettings.stateBenchSettings.numberOfInputsInOneTransaction
+            ),
+            benchSettings.stateBenchSettings.transactionsNumberInEachBlock,
+            benchSettings.stateBenchSettings.numberOfInputsInOneTransaction,
+            benchSettings.stateBenchSettings.numberOfOutputsInOneTransaction
+          )
+          val stateN: UtxoState = stateL.applyModifier(nextBlock).right.get
+          (blocks :+ nextBlock,
+            nextBlock,
+            stateN,
+            boxes.drop(
+              benchSettings.stateBenchSettings.transactionsNumberInEachBlock *
+                benchSettings.stateBenchSettings.numberOfInputsInOneTransaction)
+          )
+      }
+
+    val chain: Vector[Block] = genesisBlock +: stateGenerationResults._1
+    state = stateGenerationResults._3
+    state.close()
+  }
+}
--- benchmarks/src/test/scala/benches/Utils.scala
+++ benchmarks/src/test/scala/benches/Utils.scala
@@ -1,15 +1,22 @@
 package benches
 
 import java.io.File
+
+import akka.actor.ActorRef
 import com.typesafe.scalalogging.StrictLogging
 import encry.modifiers.mempool.TransactionFactory
 import encry.settings.{EncryAppSettings, NodeSettings}
+import encry.storage.VersionalStorage
+import encry.storage.VersionalStorage.{StorageKey, StorageType, StorageValue, StorageVersion}
+import encry.storage.iodb.versionalIODB.IODBWrapper
 import encry.storage.levelDb.versionalLevelDB.VersionalLevelDBCompanion.{LevelDBVersion, VersionalLevelDbKey, VersionalLevelDbValue}
 import encry.storage.levelDb.versionalLevelDB._
 import encry.utils.{FileHelper, Mnemonic, NetworkTimeProvider}
 import encry.view.history.EncryHistory
 import encry.view.history.processors.payload.BlockPayloadProcessor
 import encry.view.history.storage.HistoryStorage
+import encry.view.state.{BoxHolder, UtxoState}
+import encry.view.state.UtxoState.{initialStateBoxes, logger}
 import io.iohk.iodb.LSMStore
 import org.encryfoundation.common.crypto.equihash.EquihashSolution
 import org.encryfoundation.common.crypto.{PrivateKey25519, PublicKey25519, Signature25519}
@@ -26,6 +33,7 @@ import org.iq80.leveldb.Options
 import scorex.crypto.hash.{Blake2b256, Digest32}
 import scorex.crypto.signatures.{Curve25519, PrivateKey, PublicKey}
 import scorex.utils.Random
+
 import scala.collection.immutable
 import scala.util.{Random => R}
 
@@ -56,98 +64,86 @@ object Utils extends StrictLogging {
         ) :: acc
     }
 
-//  def generateGenesisBlockValidForState(state: UtxoState): Block = {
-//    val txs = Seq(coinbaseTransaction(0))
-//    val (adProofN: SerializedAdProof, adDigest: ADDigest) = state.generateProofs(txs).get
-//    val adPN: Digest32 = ADProofs.proofDigest(adProofN)
-//    val header = genHeader.copy(
-//      parentId = Header.GenesisParentId,
-//      adProofsRoot = adPN,
-//      stateRoot = adDigest,
-//      height = TestNetConstants.GenesisHeight
-//    )
-//    Block(header, Payload(header.id, txs), None)
-//  }
-//
-//  def generateGenesisBlockValidForHistory: Block = {
-//    val header = genHeader.copy(parentId = Header.GenesisParentId, height = TestNetConstants.GenesisHeight)
-//    Block(header, Payload(header.id, Seq(coinbaseTransaction)), None)
-//  }
-//
-//  def generateNextBlockValidForState(prevBlock: Block,
-//                                     state: UtxoState,
-//                                     box: Seq[AssetBox],
-//                                     transactionsNumberInEachBlock: Int,
-//                                     numberOfInputsInOneTransaction: Int,
-//                                     numberOfOutputsInOneTransaction: Int): Block = {
-//
-//    val transactions: Seq[Transaction] = (0 until transactionsNumberInEachBlock).foldLeft(box, Seq.empty[Transaction]) {
-//      case ((boxes, transactionsL), _) =>
-//        val tx: Transaction = defaultPaymentTransactionScratch(
-//          privKey,
-//          fee = 111,
-//          timestamp = 11L,
-//          useBoxes = boxes.take(numberOfInputsInOneTransaction).toIndexedSeq,
-//          recipient = randomAddress,
-//          amount = 10000,
-//          numOfOutputs = numberOfOutputsInOneTransaction
-//        )
-//        (boxes.drop(numberOfInputsInOneTransaction), transactionsL :+ tx)
-//    }._2 ++ Seq(coinbaseTransaction(prevBlock.header.height + 1))
-//    logger.info(s"Number of generated transactions: ${transactions.size}.")
-//    val (adProofN: SerializedAdProof, adDigest: ADDigest) = state.generateProofs(transactions).get
-//    val adPN: Digest32 = ADProofs.proofDigest(adProofN)
-//    val header = Header(
-//      1.toByte,
-//      prevBlock.id,
-//      adPN,
-//      adDigest,
-//      Payload.rootHash(transactions.map(_.id)),
-//      System.currentTimeMillis(),
-//      prevBlock.header.height + 1,
-//      R.nextLong(),
-//      Difficulty @@ BigInt(1),
-//      EquihashSolution(Seq(1, 3))
-//    )
-//    Block(header, Payload(header.id, transactions), None)
-//  }
-//
-//  def generateNextBlockForStateWithSpendingAllPreviousBoxes(prevBlock: Block,
-//                                                            state: UtxoState,
-//                                                            box: Seq[AssetBox],
-//                                                            splitCoef: Int = 2,
-//                                                            addDiff: Difficulty = Difficulty @@ BigInt(0)): Block = {
-//
-//    val transactions: Seq[Transaction] = box.indices.foldLeft(box, Seq.empty[Transaction]) {
-//      case ((boxes, transactionsL), _) =>
-//        val tx: Transaction = defaultPaymentTransactionScratch(
-//          privKey,
-//          fee = 1,
-//          timestamp = 11L,
-//          useBoxes = IndexedSeq(boxes.head),
-//          recipient = privKey.publicImage.address.address,
-//          amount = boxes.head.amount - 1,
-//          numOfOutputs = splitCoef
-//        )
-//        (boxes.tail, transactionsL :+ tx)
-//    }._2.filter(tx => state.isValid(tx)) ++ Seq(coinbaseTransaction(prevBlock.header.height + 1))
-//    logger.info(s"Number of generated transactions: ${transactions.size}.")
-//    val (adProofN: SerializedAdProof, adDigest: ADDigest) = state.generateProofs(transactions).get
-//    val adPN: Digest32 = ADProofs.proofDigest(adProofN)
-//    val header = Header(
-//      1.toByte,
-//      prevBlock.id,
-//      adPN,
-//      adDigest,
-//      Payload.rootHash(transactions.map(_.id)),
-//      System.currentTimeMillis(),
-//      prevBlock.header.height + 1,
-//      R.nextLong(),
-//      Difficulty @@ (BigInt(1) + addDiff),
-//      EquihashSolution(Seq(1, 3))
-//    )
-//    Block(header, Payload(header.id, transactions), None)
-//  }
+  def generateGenesisBlockValidForState(state: UtxoState): Block = {
+    val txs = Seq(coinbaseTransaction(0))
+    val header = genHeader.copy(
+      parentId = Header.GenesisParentId,
+      height = TestNetConstants.GenesisHeight
+    )
+    Block(header, Payload(header.id, txs))
+  }
+
+  def generateGenesisBlockValidForHistory: Block = {
+    val header = genHeader.copy(parentId = Header.GenesisParentId, height = TestNetConstants.GenesisHeight)
+    Block(header, Payload(header.id, Seq(coinbaseTransaction)))
+  }
+
+  def generateNextBlockValidForState(prevBlock: Block,
+                                     state: UtxoState,
+                                     box: Seq[AssetBox],
+                                     transactionsNumberInEachBlock: Int,
+                                     numberOfInputsInOneTransaction: Int,
+                                     numberOfOutputsInOneTransaction: Int): Block = {
+
+    val transactions: Seq[Transaction] = (0 until transactionsNumberInEachBlock).foldLeft(box, Seq.empty[Transaction]) {
+      case ((boxes, transactionsL), _) =>
+        val tx: Transaction = defaultPaymentTransactionScratch(
+          privKey,
+          fee = 111,
+          timestamp = 11L,
+          useBoxes = boxes.take(numberOfInputsInOneTransaction).toIndexedSeq,
+          recipient = randomAddress,
+          amount = 10000,
+          numOfOutputs = numberOfOutputsInOneTransaction
+        )
+        (boxes.drop(numberOfInputsInOneTransaction), transactionsL :+ tx)
+    }._2 ++ Seq(coinbaseTransaction(prevBlock.header.height + 1))
+    logger.info(s"Number of generated transactions: ${transactions.size}.")
+    val header = Header(
+      1.toByte,
+      prevBlock.id,
+      Payload.rootHash(transactions.map(_.id)),
+      System.currentTimeMillis(),
+      prevBlock.header.height + 1,
+      R.nextLong(),
+      Difficulty @@ BigInt(1),
+      EquihashSolution(Seq(1, 3))
+    )
+    Block(header, Payload(header.id, transactions))
+  }
+
+  def generateNextBlockForStateWithSpendingAllPreviousBoxes(prevBlock: Block,
+                                                            state: UtxoState,
+                                                            box: Seq[AssetBox],
+                                                            splitCoef: Int = 2,
+                                                            addDiff: Difficulty = Difficulty @@ BigInt(0)): Block = {
+
+    val transactions: Seq[Transaction] = box.indices.foldLeft(box, Seq.empty[Transaction]) {
+      case ((boxes, transactionsL), _) =>
+        val tx: Transaction = defaultPaymentTransactionScratch(
+          privKey,
+          fee = 1,
+          timestamp = 11L,
+          useBoxes = IndexedSeq(boxes.head),
+          recipient = privKey.publicImage.address.address,
+          amount = boxes.head.amount - 1,
+          numOfOutputs = splitCoef
+        )
+        (boxes.tail, transactionsL :+ tx)
+    }._2.filter(tx => state.validate(tx).isRight) ++ Seq(coinbaseTransaction(prevBlock.header.height + 1))
+    logger.info(s"Number of generated transactions: ${transactions.size}.")
+    val header = Header(
+      1.toByte,
+      prevBlock.id,
+      Payload.rootHash(transactions.map(_.id)),
+      System.currentTimeMillis(),
+      prevBlock.header.height + 1,
+      R.nextLong(),
+      Difficulty @@ (BigInt(1) + addDiff),
+      EquihashSolution(Seq(1, 3))
+    )
+    Block(header, Payload(header.id, transactions))
+  }
 
   def generateNextBlockValidForHistory(history: EncryHistory,
                                        difficultyDiff: BigInt = 0,
@@ -177,36 +173,32 @@ object Utils extends StrictLogging {
   def genAssetBox(address: Address, amount: Amount = 100000L, tokenIdOpt: Option[ADKey] = None): AssetBox =
     AssetBox(EncryProposition.addressLocked(address), R.nextLong(), amount, tokenIdOpt)
 
-//  def utxoFromBoxHolder(bh: BoxHolder,
-//                        dir: File,
-//                        nodeViewHolderRef: Option[ActorRef],
-//                        settings: EncryAppSettings,
-//                        storageType: StorageType): UtxoState = {
-//    val p = new avltree.BatchAVLProver[Digest32, Algos.HF](keyLength = 32, valueLengthOpt = None)
-//    bh.sortedBoxes.foreach(b => p.performOneOperation(avltree.Insert(b.id, ADValue @@ b.bytes)).ensuring(_.isSuccess))
-//    val versionalStorage = storageType match {
-//      case VersionalStorage.IODB =>
-//        IODBWrapper(new LSMStore(dir, keySize = 32, keepVersions = 10))
-//      case VersionalStorage.LevelDB =>
-//        val reopenedLevelDb = LevelDbFactory.factory.open(dir, new Options)
-//        VLDBWrapper(VersionalLevelDBCompanion(reopenedLevelDb, LevelDBSettings(100, 33), keySize = 33))
-//    }
-//    val persistentProver: avltree.PersistentBatchAVLProver[Digest32, HF] = {
-//      val np: NodeParameters = NodeParameters(keySize = 32, valueSize = None, labelSize = 32)
-//      val storage: VersionedAVLStorage[Digest32] = new VersionedAVLStorage(versionalStorage, np, settings)(Algos.hash)
-//      PersistentBatchAVLProver.create(p, storage).get
-//    }
-//    new UtxoState(
-//      persistentProver,
-//      EncryState.genesisStateVersion,
-//      TestNetConstants.GenesisHeight,
-//      versionalStorage,
-//      0L,
-//      None,
-//      settings,
-//      None
-//    )
-//  }
+  def utxoFromBoxHolder(bh: BoxHolder,
+                        dir: File,
+                        nodeViewHolderRef: Option[ActorRef],
+                        settings: EncryAppSettings,
+                        storageType: StorageType): UtxoState = {
+    val storage = settings.storage.state match {
+      case VersionalStorage.IODB =>
+        logger.info("Init state with iodb storage")
+        IODBWrapper(new LSMStore(dir, keepVersions = TestNetConstants.DefaultKeepVersions))
+      case VersionalStorage.LevelDB =>
+        logger.info("Init state with levelDB storage")
+        val levelDBInit = LevelDbFactory.factory.open(dir, new Options)
+        VLDBWrapper(VersionalLevelDBCompanion(levelDBInit, settings.levelDB, keySize = 32))
+    }
+
+    storage.insert(
+      StorageVersion @@ Array.fill(32)(0: Byte),
+      bh.boxes.values.map(bx => (StorageKey !@@ bx.id, StorageValue @@ bx.bytes)).toList
+    )
+
+    new UtxoState(
+      storage,
+      TestNetConstants.PreGenesisHeight,
+      0L,
+    )
+  }
 
   def getRandomTempDir: File = {
     val dir = java.nio.file.Files.createTempDirectory("encry_test_" + R.alphanumeric.take(15).mkString).toFile
