4d84f38eb1e74eef595aaa3c10df52b5dc027cef
timofey <gusev.t.s1@gmail.com>
2019-07-11 13:50:50 +0300

    transactions threshold added

--- src/main/scala/encry/view/mempool/Mempool.scala
+++ src/main/scala/encry/view/mempool/Mempool.scala
@@ -6,7 +6,7 @@ import com.google.common.base.Charsets
 import com.google.common.hash.{BloomFilter, Funnels}
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.StrictLogging
-import encry.network.NodeViewSynchronizer.ReceivableMessages.SuccessfulTransaction
+import encry.network.NodeViewSynchronizer.ReceivableMessages.{SemanticallySuccessfulModifier, SuccessfulTransaction}
 import encry.network.PeerConnectionHandler.ConnectedPeer
 import encry.settings.EncryAppSettings
 import encry.stats.StatsSender.MempoolStat
@@ -14,12 +14,12 @@ import encry.utils.NetworkTimeProvider
 import encry.view.NodeViewHolder.ReceivableMessages.LocallyGeneratedTransaction
 import encry.view.mempool.Mempool._
 import encry.view.state.UtxoState
+import org.encryfoundation.common.modifiers.history.Block
 import org.encryfoundation.common.modifiers.mempool.transaction.Transaction
 import org.encryfoundation.common.utils.Algos
 import org.encryfoundation.common.utils.TaggedTypes.{ADKey, ModifierId, ModifierTypeId}
 import scala.collection.immutable.HashMap
 import scala.collection.{IndexedSeq, mutable}
-import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
 
 class Mempool(settings: EncryAppSettings,
@@ -27,37 +27,39 @@ class Mempool(settings: EncryAppSettings,
               minerRef: ActorRef,
               influx: Option[ActorRef]) extends Actor with StrictLogging {
 
-  type WrappedIdAsKey = scala.collection.mutable.WrappedArray.ofByte
+  import context.dispatcher
 
-  implicit val ec: ExecutionContextExecutor = context.dispatcher
+  type Key = scala.collection.mutable.WrappedArray.ofByte
 
   implicit val system: ActorSystem = context.system
 
-  var memoryPool: HashMap[WrappedIdAsKey, Transaction] = HashMap.empty[WrappedIdAsKey, Transaction]
+  var currentNumberOfTransactions: Int = 0
+
+  var canProcessNewTransactions: Boolean = true
+
+  var memoryPool: HashMap[Key, Transaction] = HashMap.empty[Key, Transaction]
 
   var bloomFilterForTransactionsIds: BloomFilter[String] = initBloomFilter
 
   var bloomFilterForBoxesIds: BloomFilter[String] = initBloomFilter
 
   override def receive: Receive = {
-    case _@UpdatedState(updatedState) =>
-      updatedState match {
-        case utxoState: UtxoState => logger.debug(s"Received state instance on MemoryPool actor. Starting mainLogic on this actor.")
-          context.system.scheduler.schedule(
-            settings.node.bloomFilterCleanupInterval,
-            settings.node.bloomFilterCleanupInterval, self, TickForCleanupBloomFilter)
-          context.system.scheduler.schedule(
-            settings.node.mempoolCleanupInterval,
-            settings.node.mempoolCleanupInterval, self, TickForRemoveExpired)
-          context.system.scheduler.schedule(
-            settings.node.mempoolTxSendingInterval,
-            settings.node.mempoolTxSendingInterval, self, TickForSendTransactionsToMiner)
-          context.system.scheduler.schedule(
-            5.seconds,
-            5.seconds)(influx.foreach(_ ! MempoolStat(memoryPool.size)))
-          context.become(messagesHandler(utxoState))
-        //case digestState: DigestState => logger.info(s"Got digest state on MemoryPool actor.")
-      }
+    case UpdatedState(updatedState) =>
+      logger.debug(s"Received state instance on MemoryPool actor. Starting mainLogic on this actor.")
+      context.system.scheduler.schedule(
+        settings.node.bloomFilterCleanupInterval,
+        settings.node.bloomFilterCleanupInterval, self, TickForCleanupBloomFilter)
+      context.system.scheduler.schedule(
+        settings.node.mempoolCleanupInterval,
+        settings.node.mempoolCleanupInterval, self, TickForRemoveExpired)
+      context.system.scheduler.schedule(
+        settings.node.mempoolTxSendingInterval,
+        settings.node.mempoolTxSendingInterval, self, TickForSendTransactionsToMiner)
+      context.system.scheduler.schedule(
+        5.seconds,
+        1.seconds)(influx.foreach(_ ! MempoolStat(memoryPool.size)))
+      context.system.eventStream.subscribe(self, classOf[SemanticallySuccessfulModifier])
+      context.become(messagesHandler(updatedState))
     case GetMempoolSize => sender() ! memoryPool.size
     case msg => logger.info(s"Got strange message on MemoryPool actor $msg.")
   }
@@ -72,53 +74,66 @@ class Mempool(settings: EncryAppSettings,
     case TickForCleanupBloomFilter =>
       bloomFilterForTransactionsIds = initBloomFilter
       bloomFilterForBoxesIds = initBloomFilter
-    case CompareTransactionsWithUnconfirmed(peer, transactions) =>
+    case CompareTransactionsWithUnconfirmed(peer, transactions) if canProcessNewTransactions =>
       val unrequestedModifiers: IndexedSeq[ModifierId] = notRequested(transactions)
       if (unrequestedModifiers.nonEmpty) sender ! RequestForTransactions(peer, Transaction.modifierTypeId, unrequestedModifiers)
-    case RolledBackTransactions(txs) => memoryPool = validateAndPutTransactions(txs, memoryPool, state, fromNetwork = false)
+    case CompareTransactionsWithUnconfirmed(peer, transactions) =>
+      logger.info(s"Mempool got CompareTransactionsWithUnconfirmed but canProcessNewTransactions is: $canProcessNewTransactions")
+    case RolledBackTransactions(txs) =>
+      memoryPool = validateAndPutTransactions(txs, memoryPool, state, fromNetwork = false)
+      logger.info(s"Mempool got RolledBackTransactions. Current mempool size is: ${memoryPool.size}")
     case TransactionsForRemove(txs) =>
       memoryPool = removeOldTransactions(txs, memoryPool)
     case LocallyGeneratedTransaction(tx) =>
       memoryPool = validateAndPutTransactions(IndexedSeq(tx), memoryPool, state, fromNetwork = true)
     case TickForSendTransactionsToMiner =>
-      val validatedTxs: (IndexedSeq[WrappedIdAsKey], IndexedSeq[Transaction]) =
+      val validatedTxs: (IndexedSeq[Key], IndexedSeq[Transaction]) =
         memoryPool.values.toIndexedSeq.sortBy(_.fee).reverse
-          .foldLeft(IndexedSeq.empty[WrappedIdAsKey], IndexedSeq.empty[Transaction]) { case ((boxes, txs), tx) =>
-            val txInputsIds: Set[WrappedIdAsKey] = tx.inputs.map(input => toKey(ModifierId @@ input.boxId.untag(ADKey))).toSet
+          .foldLeft(IndexedSeq.empty[Key], IndexedSeq.empty[Transaction]) { case ((boxes, txs), tx) =>
+            val txInputsIds: Set[Key] = tx.inputs.map(input => toKey(ModifierId @@ input.boxId.untag(ADKey))).toSet
             if (txInputsIds.forall(id => !boxes.contains(id)) && txInputsIds.size == tx.inputs.size)
               (boxes ++: txInputsIds.toIndexedSeq, txs :+ tx)
             else (boxes, txs)
           }
       memoryPool = memoryPool -- validatedTxs._2.map(tx => toKey(tx.id))
       minerRef ! TxsForMiner(validatedTxs._2)
+      currentNumberOfTransactions += validatedTxs._2.size
+      logger.info(s"Sending to miner new transactions from mempool. Current threshold is: $currentNumberOfTransactions")
+      if (currentNumberOfTransactions >= settings.node.mempoolTransactionsThreshold) {
+        canProcessNewTransactions = false
+        logger.info(s"Threshold has its limit. Stop processing new transactions.")
+      }
+
     case AskTransactionsFromNVS(remote, ids) =>
-      val idsToWrapped: Seq[WrappedIdAsKey] = ids.map(toKey)
+      val idsToWrapped: Seq[Key] = ids.map(toKey)
       val txsForNVS: Seq[Transaction] = idsToWrapped.flatMap(id => memoryPool.get(id))
       sender() ! TxsForNVSH(remote, txsForNVS)
+
+    case SemanticallySuccessfulModifier(modifier) if modifier.modifierTypeId == Block.modifierTypeId =>
+      logger.info(s"Mempool actor got SemanticallySuccessfulModifier with new block. Reset all transactions params")
+      currentNumberOfTransactions = 0
+      canProcessNewTransactions = true
+    case SemanticallySuccessfulModifier(modifier) =>
+      logger.info(s"Mempool actor got message SemanticallySuccessfulModifier with modifier ${modifier.modifierTypeId}")
   }
 
   def handleStates: Receive = {
-    case _@UpdatedState(updatedState) =>
-      updatedState match {
-        case utxoState: UtxoState => context.become(messagesHandler(utxoState))
-        //case digestState: DigestState => logger.info(s"Got digest state on MemoryPool actor.")
-      }
+    case UpdatedState(updatedState) => context.become(messagesHandler(updatedState))
     case msg => logger.info(s"Got strange message on MemoryPool actor $msg.")
   }
 
   def removeOldTransactions(txs: IndexedSeq[Transaction],
-                            pool: HashMap[WrappedIdAsKey, Transaction]): HashMap[WrappedIdAsKey, Transaction] = {
-    val transactionsIds: IndexedSeq[WrappedIdAsKey] = txs.map(tx => toKey(tx.id))
+                            pool: HashMap[Key, Transaction]): HashMap[Key, Transaction] = {
+    val transactionsIds: IndexedSeq[Key] = txs.map(tx => toKey(tx.id))
     pool -- transactionsIds
   }
 
   def validateAndPutTransactions(inputTransactions: IndexedSeq[Transaction],
-                                 currentMemoryPool: HashMap[WrappedIdAsKey, Transaction],
+                                 currentMemoryPool: HashMap[Key, Transaction],
                                  currentState: UtxoState,
-                                 fromNetwork: Boolean): HashMap[WrappedIdAsKey, Transaction] = {
+                                 fromNetwork: Boolean): HashMap[Key, Transaction] = {
     val validatedTransactions: IndexedSeq[Transaction] = inputTransactions.filter(tx =>
       tx.semanticValidity.isSuccess && !currentMemoryPool.contains(toKey(tx.id))
-      //&& currentState.validate(tx).isSuccess
     )
     if (memoryPool.size + validatedTransactions.size <= settings.node.mempoolMaxCapacity)
       validatedTransactions.foldLeft(memoryPool) { case (pool, tx) =>
@@ -126,7 +141,7 @@ class Mempool(settings: EncryAppSettings,
         pool.updated(toKey(tx.id), tx)
       }
     else {
-      val filteredMemoryPool: HashMap[WrappedIdAsKey, Transaction] = cleanMemoryPoolFromExpired(memoryPool)
+      val filteredMemoryPool: HashMap[Key, Transaction] = cleanMemoryPoolFromExpired(memoryPool)
       val availableNumberOfTransactions: Int = settings.node.mempoolMaxCapacity - filteredMemoryPool.size
       val transactionsForAdding: IndexedSeq[Transaction] = validatedTransactions.take(availableNumberOfTransactions)
       transactionsForAdding.foldLeft(memoryPool) { case (pool, tx) =>
@@ -136,7 +151,7 @@ class Mempool(settings: EncryAppSettings,
     }
   }
 
-  def cleanMemoryPoolFromExpired(pool: HashMap[WrappedIdAsKey, Transaction]): HashMap[WrappedIdAsKey, Transaction] =
+  def cleanMemoryPoolFromExpired(pool: HashMap[Key, Transaction]): HashMap[Key, Transaction] =
     pool.filter { case (_, tx) => (ntp.estimatedTime - tx.timestamp) < settings.node.utxMaxAge.toMillis }
 
   def initBloomFilter: BloomFilter[String] = BloomFilter.create(
@@ -155,23 +170,25 @@ class Mempool(settings: EncryAppSettings,
       true
     } else false
 
-  def toKey(id: ModifierId): WrappedIdAsKey = new mutable.WrappedArray.ofByte(id)
+  def toKey(id: ModifierId): Key = new mutable.WrappedArray.ofByte(id)
 
 }
 
 object Mempool {
 
-  final case class TransactionsFromRemote(tx: Seq[Transaction])
+  case object StopReceivingTransactions
+
+  final case class TransactionsFromRemote(tx: Seq[Transaction]) extends AnyVal
 
   case class CompareTransactionsWithUnconfirmed(peer: ConnectedPeer, transactions: IndexedSeq[ModifierId])
 
-  case class TransactionsFromMemoryPool(txs: IndexedSeq[Transaction])
+  final case class TransactionsFromMemoryPool(txs: IndexedSeq[Transaction]) extends AnyVal
 
-  case class TransactionsForRemove(txs: IndexedSeq[Transaction])
+  final case class TransactionsForRemove(txs: IndexedSeq[Transaction]) extends AnyVal
 
-  case class RolledBackTransactions(txs: IndexedSeq[Transaction])
+  final case class RolledBackTransactions(txs: IndexedSeq[Transaction]) extends AnyVal
 
-  case class UpdatedState(state: UtxoState)
+  final case class UpdatedState(state: UtxoState) extends AnyVal
 
   case object TickForSendTransactionsToMiner
 
@@ -183,11 +200,11 @@ object Mempool {
 
   case object AskTransactionsFromMemoryPoolFromMiner
 
-  case class AskTransactionsFromNVS(peer: ConnectedPeer, txsIds: Seq[ModifierId])
+  final case class AskTransactionsFromNVS(peer: ConnectedPeer, txsIds: Seq[ModifierId])
 
-  case class TxsForMiner(txs: IndexedSeq[Transaction])
+  final case class TxsForMiner(txs: IndexedSeq[Transaction]) extends AnyVal
 
-  case class TxsForNVSH(peer: ConnectedPeer, txs: Seq[Transaction])
+  final case class TxsForNVSH(peer: ConnectedPeer, txs: Seq[Transaction])
 
   final case class RequestForTransactions(source: ConnectedPeer,
                                           modifierTypeId: ModifierTypeId,
@@ -198,14 +215,9 @@ object Mempool {
 
   class MempoolPriorityQueue(settings: ActorSystem.Settings, config: Config)
     extends UnboundedStablePriorityMailbox(
-      // Create a new PriorityGenerator, lower priority means more important
       PriorityGenerator {
-        // 'highpriority messages should be treated first if possible
-        case UpdatedState(_) => 0
-
+        case UpdatedState(_)                                                => 0
         case AskTransactionsFromMemoryPoolFromMiner | TransactionsForRemove => 1
-        // We default to 1, which is in between high and low
-        case otherwise => 2
+        case _                                                              => 2
       })
-
 }
