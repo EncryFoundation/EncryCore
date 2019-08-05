7ffe71b7e8f8b921e93859e61aa2e9184c781ffe
Lior <lior.liviev@mail.ru>
2019-08-05 17:42:35 +0300

    some addition

--- src/main/scala/encry/view/ModifiersCache.scala
+++ src/main/scala/encry/view/ModifiersCache.scala
@@ -21,6 +21,10 @@ final case class ModifiersCache(modifiersCache: Map[Key, PersistentModifier],
 
   val size: Int = modifiersCache.size
 
+  private var isChainSynced = false
+
+  def setChainSynced(): Unit = isChainSynced = true
+
   def contains(key: Key): Boolean = modifiersCache.contains(key)
 
   def put(modifier: PersistentModifier, history: EncryHistory): ModifiersCache =
@@ -46,6 +50,7 @@ final case class ModifiersCache(modifiersCache: Map[Key, PersistentModifier],
   private def getHeadersIdsAtHeight(height: Int, h: EncryHistory): (Map[Key, PersistentModifier], List[Key]) =
     headersCache.get(height) match {
       case Some(headersIds) => headersIds
+        .view
         .map(key)
         .foldLeft(modifiersCache, List.empty[Key]) { case ((updatedCache, applicable), modifierId) => modifiersCache
           .get(modifierId)
@@ -54,23 +59,24 @@ final case class ModifiersCache(modifiersCache: Map[Key, PersistentModifier],
             case Left(_: FatalValidationError) => (updatedCache - modifierId, applicable)
             case Left(_) => (updatedCache, applicable)
             case Right(_) => (updatedCache, applicable :+ modifierId)
-          }.getOrElse((modifiersCache, List.empty[Key]))
+          }
+          .getOrElse((modifiersCache, List.empty[Key]))
         }
       case _ =>
         logger.debug(s"Can't find headers at height $height in cache")
         (modifiersCache, List.empty[Key])
     }
 
-  def remove(key: Key): (ModifiersCache, Option[PersistentModifier]) = {
+  def remove(key: Key): (Map[Key, PersistentModifier], Option[PersistentModifier]) = {
     logger.debug(s"Going to delete ${Algos.encode(key.toArray)}. Cache contains: ${modifiersCache.get(key).isDefined}.")
-    val modCache = ModifiersCache(modifiersCache - key, headersCache, settings)
+    val modCache = modifiersCache - key
     (modCache, modifiersCache.get(key))
   }
 
   def popCandidate(history: EncryHistory): (List[PersistentModifier], ModifiersCache) = {
-    val a: List[Key] = findCandidateKey(history)
+    val a: (List[Key], ModifiersCache) = findCandidateKey(history)
     val b: (Map[Key, PersistentModifier], List[PersistentModifier]) =
-      a.foldLeft(modifiersCache, List.empty[PersistentModifier]) { case ((cache, modifiers), modsKey) =>
+      a._1.foldLeft(modifiersCache, List.empty[PersistentModifier]) { case ((cache, modifiers), modsKey) =>
         cache.get(modsKey) match {
           case Some(value) => (cache - modsKey) -> (modifiers :+ value)
           case None => (cache - modsKey) -> modifiers
@@ -79,10 +85,10 @@ final case class ModifiersCache(modifiersCache: Map[Key, PersistentModifier],
     b._2 -> ModifiersCache(b._1, headersCache, settings)
   }
 
-  def findCandidateKey(history: EncryHistory): List[Key] = {
+  def findCandidateKey(history: EncryHistory): (List[Key], ModifiersCache) = {
 
     def isApplicable(modifierId: Key): Boolean = modifiersCache.get(modifierId).exists(mod => history.testApplicable(mod) match {
-      case Left(_: FatalValidationError) => remove(modifierId); false
+      case Left(_: FatalValidationError) => false
       case Right(_) => true
       case Left(_) => false
     })
@@ -107,19 +113,25 @@ final case class ModifiersCache(modifiersCache: Map[Key, PersistentModifier],
           headersCache - (history.bestHeaderHeight + 1)
         logger.debug(s"HeadersCollection size is: ${headersCache.size}")
         logger.debug(s"Drop height ${history.bestHeaderHeight + 1} in HeadersCollection")
-        val resultedIds: List[Key] = elems
+        val resultedIds: (Map[Key, PersistentModifier], List[Key]) = elems
           .map(k => modifiersCache.get(key(k)))
-          .collect { case Some(v: Header) if
-          ((v.parentId sameElements history.bestHeaderOpt.map(_.id).getOrElse(Array.emptyByteArray)) ||
-            (history.bestHeaderHeight == TestNetConstants.PreGenesisHeight &&
-              (v.parentId sameElements Header.GenesisParentId)) ||
-            history.modifierById(v.parentId).nonEmpty) && isApplicable(key(v.id)) =>
-            logger.debug(s"Find new bestHeader in cache: ${Algos.encode(v.id)}")
-            key(v.id)
+           .foldLeft(modifiersCache, List.empty[Key]) { case ((mCache, keys), modifier) =>
+             modifier match {
+               case Some(value) if
+               ((value.parentId sameElements history.bestHeaderOpt.map(_.id).getOrElse(Array.emptyByteArray)) ||
+                 (history.bestHeaderHeight == TestNetConstants.PreGenesisHeight &&
+                   (value.parentId sameElements Header.GenesisParentId)) ||
+                 history.modifierById(value.parentId).nonEmpty) && isApplicable(key(modifier.get.id)) =>
+                 val newModCache = if (!isApplicable(key(value.id))) remove(key(value.id))._1
+                 else modifiersCache
+             newModCache -> (keys :+ key(value.id))
+               case _ => mCache -> keys
           }
+    }
+
         val updatedCache: Map[Key, PersistentModifier] =
-          modifiersCache -- elems.map(id => key(id)).filterNot(resultedIds.contains)
-        (resultedIds, ModifiersCache(updatedCache, updatedHeadersCollection, settings))
+          resultedIds._1 -- elems.map(id => key(id)).filterNot(resultedIds._2.contains)
+        (resultedIds._2, ModifiersCache(updatedCache, updatedHeadersCollection, settings))
       case None =>
         logger.debug(s"No header in cache at height ${history.bestHeaderHeight + 1}. " +
           s"Trying to find in range [${history.bestHeaderHeight - TestNetConstants.MaxRollbackDepth}," +
@@ -131,20 +143,20 @@ final case class ModifiersCache(modifiersCache: Map[Key, PersistentModifier],
         (modifiers, ModifiersCache(modifiersCache, headersCache, settings))
     }
 
-    if (bestHeadersIds._1.nonEmpty) bestHeadersIds._1
+    if (bestHeadersIds._1.nonEmpty) (bestHeadersIds._1, bestHeadersIds._2)
     else history
       .headerIdsAtHeight(history.bestBlockHeight + 1)
       .headOption match {
       case Some(id) => history.modifierById(id) match {
         case Some(header: Header) if isApplicable(new mutable.WrappedArray.ofByte(header.payloadId)) =>
-          List(new mutable.WrappedArray.ofByte(header.payloadId))
-        case _ => exhaustiveSearch
+          (List(new mutable.WrappedArray.ofByte(header.payloadId)), bestHeadersIds._2)
+        case _ => (exhaustiveSearch, bestHeadersIds._2)
       }
       case None if isChainSynced =>
         logger.debug(s"No payloads for current history")
-        exhaustiveSearch
+        (exhaustiveSearch, bestHeadersIds._2)
       case None => logger.debug(s"No payloads for current history")
-        List.empty[Key]
+        (List.empty[Key], this)
     }
   }
 
@@ -162,13 +174,7 @@ object ModifiersCache {
 
   type Key = mutable.WrappedArray[Byte]
 
-  var isChainSynced = false
-
-  def setChainSynced(): Unit = isChainSynced = true
-
   def key(id: ModifierId): mutable.WrappedArray.ofByte = new mutable.WrappedArray.ofByte(id)
-
-  sealed trait ModifiersCacheErrors
 
   def apply(settings: EncryAppSettings): ModifiersCache =
     ModifiersCache(Map.empty[Key, PersistentModifier], SortedMap.empty[Int, List[ModifierId]], settings)
