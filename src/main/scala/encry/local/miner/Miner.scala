0127a26dbdfebd2c3115f23ee5f124989322365a
Lior <lior.liviev@mail.ru>
2019-08-29 11:46:31 +0300

    some addition

--- src/main/scala/encry/local/miner/Miner.scala
+++ src/main/scala/encry/local/miner/Miner.scala
@@ -125,6 +125,17 @@ class Miner(dataHolder: ActorRef, influx: Option[ActorRef]) extends Actor with S
 
   def receiveSemanticallySuccessfulModifier: Receive = {
     case SemanticallySuccessfulModifier(mod: Block) if needNewCandidate(mod) =>
+      logger.info(s"Txs = $transactionsPool")
+      logger.info(s"SSM height = ${mod.header.height} / Candidate height = ${candidateOpt.get.parentOpt.get.height + 1}")
+      logger.info(s"SSM size = ${mod.payload.txs.size} / Candidate size = ${candidateOpt.get.transactions.size}")
+      logger.info(s"First = ${mod.payload.txs.diff(candidateOpt.get.transactions)} / Second = ${candidateOpt.get.transactions.diff(mod.payload.txs)}")
+      if (mod.header.height == candidateOpt.get.parentOpt.get.height + 1
+        && mod.payload.txs.size != candidateOpt.get.transactions.size) {
+        logger.info(s"if loop")
+        //if (mod.payload.txs.size != candidateOpt.get.transactions.size)
+          transactionsPool = transactionsPool ++ mod.payload.txs.diff(candidateOpt.get.transactions)
+        logger.info(s"IF txs = $transactionsPool")
+      }
       logger.info(s"Got new block. Starting to produce candidate at height: ${mod.header.height + 1} " +
         s"at ${dateFormat.format(new Date(System.currentTimeMillis()))}")
       produceCandidate()
