60ce8e9c040669cc3ed5fef5a80938aee0555174
aleksandr <alexbromel@gmail.com>
2019-09-18 18:37:17 +0300

    add single left, right rotations

--- src/main/scala/encry/view/state/avlTree/LeafNode.scala
+++ src/main/scala/encry/view/state/avlTree/LeafNode.scala
@@ -3,5 +3,9 @@ package encry.view.state.avlTree
 final case class LeafNode[K, V](key: K,
                                 value: V) extends Node[K, V] {
 
-  override def toString: String = s"($key, $value)"
+  override val balance: Int = 0
+
+  override val height: Int = 0
+
+  override def toString: String = s"($key, $value, height: 0, balance: 0)"
 }
--- src/test/scala/encry/view/state/avlTree/AvlTreeTest.scala
+++ src/test/scala/encry/view/state/avlTree/AvlTreeTest.scala
@@ -12,7 +12,10 @@ class AvlTreeTest extends PropSpec with Matchers with EncryGenerator {
 
     val avl = AvlTree[Int, String]()
 
-    val newAvl = avl.insert(1, "test")
+    val newAvl = avl
+      .insert(3, "test")
+      .insert(4, "test123")
+      .insert(5, "sss")
 
     println(newAvl)
   }
