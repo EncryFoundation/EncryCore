dfad6dfad081d83b2cfdd6ec9709c148a91cd99e
aleksandr <alexbromel@gmail.com>
2019-09-19 14:56:02 +0300

    add get(), find(), contains()

--- src/main/scala/encry/view/state/avlTree/InternalNode.scala
+++ src/main/scala/encry/view/state/avlTree/InternalNode.scala
@@ -1,17 +1,25 @@
 package encry.view.state.avlTree
 
+import encry.view.state.avlTree.AvlTree.InsertDirection
+
 final case class InternalNode[K, V](key: K,
                                     value: V,
                                     height: Int,
                                     balance: Int,
                                     leftChild: Option[Node[K, V]],
-                                    rightChild: Option[Node[K, V]]) extends Node[K, V] {
-  override def toString: String = s"[($key, $value, $height, $balance) -> LeftChild:${leftChild.map(_.toString)}, RightChild: ${rightChild.map(_.toString)}]"
+                                    rightChild: Option[Node[K, V]],
+                                    insertDirection: InsertDirection) extends Node[K, V] {
+
+  override def selfInspection: Node[K, V] = if (leftChild.isEmpty & rightChild.isEmpty) LeafNode(key, value)
+                                            else this
+
+  override def toString: String = s"[($key, $value, height: $height, balance $balance, $insertDirection) \n-> LeftChildOf($key):${leftChild.map(_.toString)}, \n-> RightChildOf($key): ${rightChild.map(_.toString)}]"
 }
 
 object InternalNode {
   def apply[K, V](key: K,
                   value: V,
                   height: Int,
-                  balance: Int): InternalNode[K, V] = new InternalNode(key, value, height, balance, None, None)
+                  balance: Int,
+                  insertDirection: InsertDirection): InternalNode[K, V] = new InternalNode(key, value, height, balance, None, None, insertDirection)
 }
--- src/main/scala/encry/view/state/avlTree/LeafNode.scala
+++ src/main/scala/encry/view/state/avlTree/LeafNode.scala
@@ -1,11 +1,18 @@
 package encry.view.state.avlTree
 
+import encry.view.state.avlTree.AvlTree.InsertDirection
+import encry.view.state.avlTree.AvlTree.InsertDirections.EMPTY
+
 final case class LeafNode[K, V](key: K,
                                 value: V) extends Node[K, V] {
 
   override val balance: Int = 0
 
   override val height: Int = 0
+
+  override val insertDirection: InsertDirection = EMPTY
+
+  override def selfInspection: Node[K, V] = this
 
   override def toString: String = s"($key, $value, height: 0, balance: 0)"
 }
