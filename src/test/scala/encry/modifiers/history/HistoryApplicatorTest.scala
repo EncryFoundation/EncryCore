3f81a7df2ab2b2a7a78de8d1ed2a94993efb6d00
capdev <sergey.moskalenko@scruge.world>
2019-09-25 17:40:46 +0500

    adapted test for new chain some refactoring

--- src/test/scala/encry/modifiers/history/HistoryApplicatorTest.scala
+++ src/test/scala/encry/modifiers/history/HistoryApplicatorTest.scala
@@ -46,52 +46,43 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
     TestKit.shutdownActorSystem(system)
   }
 
-  //  def genHistoryBlocks(initialHistory: History, count: Int): (History, Seq[Block]) = {
-  //    (0 until count).foldLeft(initialHistory, Seq.empty[Block]) {
-  //      case ((prevHistory, blocks), _) =>
-  //        val block: Block = generateNextBlock(prevHistory)
-  //        prevHistory.append(block.header)
-  //        prevHistory.append(block.payload)
-  //        (prevHistory.reportModifierIsValid(block), blocks :+ block)
-  //    }
-  //  }
-
   def blockToModifiers(block: Block): Seq[PersistentModifier] = Seq(block.header, block.payload)
 
   def rndAddress: Address = Pay2PubKeyAddress(PublicKey @@ ScorexRandom.randomBytes()).address
 
-  def genNextBlockForState(prevBlock: Block, state: UtxoState, boxes: Seq[AssetBox], invalid: Boolean = false,
-                           addDiff: Difficulty = Difficulty @@ BigInt(0)): (Block, Seq[AssetBox]) = {
-    val timestamp = System.currentTimeMillis()
-
-    val amount = if(invalid) boxes.map(_.amount).sum + 1L else 1L
-
-    val transactions: Seq[Transaction] =
-      boxes.map(b => TransactionFactory.defaultPaymentTransactionScratch(privKey, 1L, timestamp, IndexedSeq(b), rndAddress, amount)) :+
-        ChainUtils.coinbaseTransaction(prevBlock.header.height + 1)
-
-    val header = Header(1.toByte, prevBlock.id, Payload.rootHash(transactions.map(_.id)), timestamp,
-      prevBlock.header.height + 1, Random.nextLong(), Difficulty @@ (BigInt(1) + addDiff), EquihashSolution(Seq(1, 3)))
-
-    val newBoxes = transactions.filter(_.newBoxes.size > 1).map(_.newBoxes.toSeq(1).asInstanceOf[AssetBox])
-    (Block(header, Payload(header.id, transactions)), newBoxes)
-  }
-
-  def genForkBlock(boxQty: Int, prevBlock: Block, box: AssetBox, addDiff: Difficulty = Difficulty @@ BigInt(0)): (Block, Seq[AssetBox]) = {
-    val timestamp = System.currentTimeMillis()
-
-    val transactions: Seq[Transaction] = Seq(
-      ChainUtils.defaultPaymentTransactionScratch(privKey, 1L, timestamp, IndexedSeq(box), rndAddress, 1000L, None, boxQty),
-      ChainUtils.coinbaseTransaction(prevBlock.header.height + 1)
-    )
-
-    val header = Header(1.toByte, prevBlock.id, Payload.rootHash(transactions.map(_.id)), timestamp,
-      prevBlock.header.height + 1, Random.nextLong(), Difficulty @@ (BigInt(1) + addDiff), EquihashSolution(Seq(1, 3)))
-
-    (Block(header, Payload(header.id, transactions)), transactions.head.newBoxes.tail.map(_.asInstanceOf[AssetBox]).toSeq)
-  }
-
   def genChain(blockQty: Int, transPerBlock: Int = 1, genInvalidBlockFrom: Option[Int] = None): (UtxoState, UtxoState, List[Block]) = {
+
+    def genForkBlock(boxQty: Int, prevBlock: Block, box: AssetBox, addDiff: Difficulty = Difficulty @@ BigInt(0)): (Block, Seq[AssetBox]) = {
+      val timestamp = System.currentTimeMillis()
+
+      val transactions: Seq[Transaction] = Seq(
+        ChainUtils.defaultPaymentTransactionScratch(privKey, 1L, timestamp, IndexedSeq(box), rndAddress, 1000L, None, boxQty),
+        ChainUtils.coinbaseTransaction(prevBlock.header.height + 1)
+      )
+
+      val header = Header(1.toByte, prevBlock.id, Payload.rootHash(transactions.map(_.id)), timestamp,
+        prevBlock.header.height + 1, Random.nextLong(), Difficulty @@ (BigInt(1) + addDiff), EquihashSolution(Seq(1, 3)))
+
+      (Block(header, Payload(header.id, transactions)), transactions.head.newBoxes.tail.map(_.asInstanceOf[AssetBox]).toSeq)
+    }
+
+    def genNextBlockForState(prevBlock: Block, state: UtxoState, boxes: Seq[AssetBox], invalid: Boolean = false,
+                             addDiff: Difficulty = Difficulty @@ BigInt(0)): (Block, Seq[AssetBox]) = {
+      val timestamp = System.currentTimeMillis()
+
+      val amount = if(invalid) boxes.map(_.amount).sum + 1L else 1L
+
+      val transactions: Seq[Transaction] =
+        boxes.map(b => TransactionFactory.defaultPaymentTransactionScratch(privKey, 1L, timestamp, IndexedSeq(b), rndAddress, amount)) :+
+          ChainUtils.coinbaseTransaction(prevBlock.header.height + 1)
+
+      val header = Header(1.toByte, prevBlock.id, Payload.rootHash(transactions.map(_.id)), timestamp,
+        prevBlock.header.height + 1, Random.nextLong(), Difficulty @@ (BigInt(1) + addDiff), EquihashSolution(Seq(1, 3)))
+
+      val newBoxes = transactions.filter(_.newBoxes.size > 1).map(_.newBoxes.toSeq(1).asInstanceOf[AssetBox])
+      (Block(header, Payload(header.id, transactions)), newBoxes)
+    }
+
     assert(blockQty >= 2, "chain at least 2 blocks")
 
     val genesisBlock: Block = ChainUtils.generateGenesisBlock(Height @@ 0)
@@ -113,25 +104,48 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
     (state, newState, chain)
   }
 
-  def applyBlocksToHistory(history: History, blocks: Seq[Block]): History =
-    blocks.foldLeft(history) {
-      case (prevHistory, block) =>
-        prevHistory.append(block.header)
-        prevHistory.append(block.payload)
-        prevHistory.reportModifierIsValid(block)
-    }
+  def rollbackTest(transQty: Int) {
 
-  def printIds(blocks: List[Block]) = {
-    blocks.foreach { b =>
-      println(s"header.timestamp: ${b.header.timestamp}")
-      println(s"header.id: ${Algos.encode(b.header.id)}")
-      println(s"header.payloadId: ${Algos.encode(b.header.payloadId)}")
-      println(s"payload.id: ${Algos.encode(b.payload.id)}")
-      println(s"payload.headerId: ${Algos.encode(b.payload.headerId)}")
-    }
+    val history: History = generateDummyHistory(settings)
+    //generate invalid blocks begining from 6 blocks
+    val (genesisState, state, chain) = genChain(10, transQty, Some(6))
+
+    val historyApplicator: TestActorRef[HistoryApplicator] =
+      TestActorRef[HistoryApplicator](
+        HistoryApplicator.props(history, settings, genesisState, wallet, nodeViewHolder.ref, Some(influx.ref))
+      )
+    system.eventStream.subscribe(self, classOf[SemanticallySuccessfulModifier])
+
+    chain
+      .flatMap(blockToModifiers)
+      .foreach(historyApplicator ! ModifierFromRemote(_))
+
+    receiveN(6 * 2, 30 seconds)
+
+    history.getBestBlockHeight shouldBe 4
+    history.getBestBlock.map(b => Algos.encode(b.id)) shouldBe Some(Algos.encode(chain(4).id))
+    history.getBestBlockHeightDB shouldBe 4
   }
 
-  def checkFullBlockChainIsSynced(qty: Int): Unit = (0 until qty).foreach(_ => expectMsg(timeout, FullBlockChainIsSynced()))
+//  def applyBlocksToHistory(history: History, blocks: Seq[Block]): History =
+//    blocks.foldLeft(history) {
+//      case (prevHistory, block) =>
+//        prevHistory.append(block.header)
+//        prevHistory.append(block.payload)
+//        prevHistory.reportModifierIsValid(block)
+//    }
+
+//  def printIds(blocks: List[Block]) = {
+//    blocks.foreach { b =>
+//      println(s"header.timestamp: ${b.header.timestamp}")
+//      println(s"header.id: ${Algos.encode(b.header.id)}")
+//      println(s"header.payloadId: ${Algos.encode(b.header.payloadId)}")
+//      println(s"payload.id: ${Algos.encode(b.payload.id)}")
+//      println(s"payload.headerId: ${Algos.encode(b.payload.headerId)}")
+//    }
+//  }
+
+  //def checkFullBlockChainIsSynced(qty: Int): Unit = (0 until qty).foreach(_ => expectMsg(timeout, FullBlockChainIsSynced()))
 
   val dir: File = FileHelper.getRandomTempDir
   val wallet: EncryWallet = EncryWallet.readOrGenerate(settings.copy(directory = dir.getAbsolutePath))
@@ -146,7 +160,6 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
     "apply locall blocks and chain sync" in {
 
       val blockQty = 10
-
       val history: History = generateDummyHistory(settings)
       val (genesisState, state, chain) = genChain(blockQty)
 
@@ -155,21 +168,21 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
           HistoryApplicator.props(history, settings, genesisState, wallet, nodeViewHolder.ref, Some(influx.ref))
         )
 
-      system.eventStream.subscribe(self, classOf[FullBlockChainIsSynced])
+      system.eventStream.subscribe(self, classOf[SemanticallySuccessfulModifier])
 
       chain.foreach(historyApplicator ! LocallyGeneratedBlock(_))
 
-      checkFullBlockChainIsSynced((blockQty + 1) * 2)
-      history.getBestBlockHeight shouldBe blockQty
+      receiveN(blockQty * 2, 30 seconds)
+      history.getBestBlockHeight shouldBe blockQty - 1
     }
 
-    "check queues" in {
+    "check SemanticallySuccessfulModifier per block" in {
 
+      val blockQty = 2
       val initialHistory: History = generateDummyHistory(settings)
-      val (genesisState, state, chain) = genChain(1)
+      val (genesisState, state, chain) = genChain(blockQty)
 
       val modifiers: Seq[PersistentModifier] = chain.flatMap(blockToModifiers)
-      //val modifierIds = modifiers.map(_.id)
 
       val historyApplicator: TestActorRef[HistoryApplicator] =
         TestActorRef[HistoryApplicator](
@@ -180,33 +193,10 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
 
       modifiers.foreach(historyApplicator ! ModifierFromRemote(_))
 
-      println(s"test.modifiersQueue: ${historyApplicator.underlyingActor.modifiersQueue.size}")
-      println(s"test.currentNumberOfAppliedModifiers: ${historyApplicator.underlyingActor.currentNumberOfAppliedModifiers}")
+      //println(s"test.modifiersQueue: ${historyApplicator.underlyingActor.modifiersQueue.size}")
+      //println(s"test.currentNumberOfAppliedModifiers: ${historyApplicator.underlyingActor.currentNumberOfAppliedModifiers}")
 
-      receiveN(4, timeout).forall { case _: SemanticallySuccessfulModifier => true }
-
-      //      val modifiersQueue = historyApplicator.underlyingActor.modifiersQueue
-      //      modifiersQueue.size shouldBe 2
-      //      historyApplicator.underlyingActor.currentNumberOfAppliedModifiers shouldBe 2
-
-      //Thread.sleep(15000)
-      //historyApplicator ! StateApplicator.RequestNextModifier
-      //
-      //      val msg = receiveWhile(timeout) {
-      //        case msg: StartModifiersApplicationOnStateApplicator => msg.progressInfo.toApply.size
-      //        case msg: Any => println(msg)
-      //      }
-      //      println(s"msg: ${msg}")
-
-      //      historyApplicator.underlyingActor.modifiersQueue shouldBe 0
-      //      historyApplicator.underlyingActor.currentNumberOfAppliedModifiers shouldBe 2
-      //
-      //      val history = historyApplicator.underlyingActor.history
-      //
-      //      expectMsgType[StartModifiersApplicationOnStateApplicator](timeout)
-      //
-      //      assert(modifierIds.forall(history.isModifierDefined))
-      //      assert(modifierIds.map(Algos.encode) == modifiersQueue.map(_._1))
+      receiveN(blockQty * 2, timeout).forall { case _: SemanticallySuccessfulModifier => true }
     }
 
     "check queue for rollback height" in {
@@ -214,7 +204,7 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
       val overQty = 30
 
       val history: History = generateDummyHistory(settings)
-      val (genesisState, state, chain) = genChain(settings.levelDB.maxVersions + overQty)
+      val (genesisState, state, chain) = genChain(settings.levelDB.maxVersions + overQty, 10)
 
       val historyApplicator: TestActorRef[HistoryApplicator] =
         TestActorRef[HistoryApplicator](
@@ -226,62 +216,20 @@ class HistoryApplicatorTest extends TestKit(ActorSystem())
         .flatMap(blockToModifiers)
         .foreach(historyApplicator ! ModifierFromRemote(_))
 
-      println(s"modifiersQueue.size: ${historyApplicator.underlyingActor.currentNumberOfAppliedModifiers}")
+      //println(s"modifiersQueue.size: ${historyApplicator.underlyingActor.currentNumberOfAppliedModifiers}")
       historyApplicator.underlyingActor.modifiersQueue.size should be <= settings.levelDB.maxVersions
 
       receiveN((settings.levelDB.maxVersions + overQty) * 2, 30 seconds)
 
-      history.getBestBlockHeight shouldBe settings.levelDB.maxVersions + overQty
+      history.getBestBlockHeight shouldBe settings.levelDB.maxVersions + overQty - 1
     }
 
-    "check rollback for invalid state" in {
-
-      val history: History = generateDummyHistory(settings)
-
-      //generate invalid blocks begining from 2 blocks
-      val (genesisState, state, chain) = genChain(3, 1, Some(2))
-
-      val historyApplicator: TestActorRef[HistoryApplicator] =
-        TestActorRef[HistoryApplicator](
-          HistoryApplicator.props(history, settings, genesisState, wallet, nodeViewHolder.ref, Some(influx.ref))
-        )
-      system.eventStream.subscribe(self, classOf[FullBlockChainIsSynced])
-
-      chain
-        .flatMap(blockToModifiers)
-        .foreach(historyApplicator ! ModifierFromRemote(_))
-
-      checkFullBlockChainIsSynced(3 * 2)
-
-      history.getBestBlockHeight shouldBe 1
-      history.getBestBlock.map(b => Algos.encode(b.id)) shouldBe Some(Algos.encode(chain(1).id))
-      history.getBestBlockHeightDB shouldBe 1
+    "check history rollback for invalid state" in {
+      rollbackTest(100)
     }
 
-    "check rollback on fat blocks" in {
-
-      val history: History = generateDummyHistory(settings)
-
-      //generate invalid blocks begining from 6 blocks
-      val (genesisState, state, chain) = genChain(10, 5, Some(6))
-
-      chain.foreach(b => println(s"block.header : ${Algos.encode(b.header.id)}"))
-
-      val historyApplicator: TestActorRef[HistoryApplicator] =
-        TestActorRef[HistoryApplicator](
-          HistoryApplicator.props(history, settings, genesisState, wallet, nodeViewHolder.ref, Some(influx.ref))
-        )
-      system.eventStream.subscribe(self, classOf[FullBlockChainIsSynced])
-
-      chain
-        .flatMap(blockToModifiers)
-        .foreach(historyApplicator ! ModifierFromRemote(_))
-
-      checkFullBlockChainIsSynced(10)
-
-      history.getBestBlockHeight shouldBe Height @@ 4
-      history.getBestBlockHeightDB shouldBe Height @@ 4
-      history.getBestBlock.map(b => Algos.encode(b.id)) shouldBe Some(Algos.encode(chain(4).id))
+    "check history rollback on fat blocks" in {
+      rollbackTest(5000)
     }
 
   }
