d199ba0beda1f2eeb187d437bc3f55c1bb6bb18a
aleksandr <alexbromel@gmail.com>
2019-10-18 13:15:55 +0300

    add exhaustiveSearch in cache

--- src/main/scala/encry/view/ModifiersCache.scala
+++ src/main/scala/encry/view/ModifiersCache.scala
@@ -136,7 +136,7 @@ object ModifiersCache extends StrictLogging {
       case Some(id) => history.getHeaderById(id) match {
         case Some(header: Header) if isApplicable(new mutable.WrappedArray.ofByte(header.payloadId)) =>
           List(new mutable.WrappedArray.ofByte(header.payloadId))
-        case _ if history.isHeadersChainSynced => List.empty[Key]
+        case _ if history.isHeadersChainSynced => exhaustiveSearch
         case _ => List.empty[Key]
       }
       case None if isChainSynced =>
