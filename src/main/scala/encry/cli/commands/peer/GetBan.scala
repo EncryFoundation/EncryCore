170e7534e7c11250bb36ec1eb287b7d55d28d6e3
Lior <lior.liviev@mail.ru>
2019-10-25 17:44:57 +0300

    ec-fix

---
+++ src/main/scala/encry/cli/commands/peer/GetBan.scala
@@ -0,0 +1,34 @@
+package encry.cli.commands.peer
+
+import java.net.InetSocketAddress
+
+import akka.actor.ActorRef
+import encry.api.http.DataHolderForApi.{GetConnectedPeersHelper, PeerBanHelper}
+import encry.api.http.routes.PeersApiRoute.PeerInfoResponse
+import encry.cli.{Ast, Response}
+import encry.cli.commands.Command
+import encry.settings.EncryAppSettings
+import encry.utils.NetworkTimeProvider
+import akka.pattern._
+import akka.util.Timeout
+import encry.network.BlackList.BanReason.InvalidNetworkMessage
+import encry.network.PeersKeeper.BanPeerFromAPI
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object GetBan extends Command {
+
+  override def execute(args: Command.Args,
+                       settings: EncryAppSettings,
+                       dataHolder: ActorRef,
+                       nodeId: Array[Byte],
+                       ntp: NetworkTimeProvider): Future[Option[Response]] = {
+    implicit val timeout: Timeout = Timeout(settings.restApi.timeout)
+    val host: String            = args.requireArg[Ast.Str]("host").s
+    val port: Long              = args.requireArg[Ast.Num]("port").i
+    val peer: InetSocketAddress = new InetSocketAddress(host, port.toInt)
+    dataHolder ! PeerBanHelper(peer, "Banned by the user")
+    Future(None)
+  }
+}
