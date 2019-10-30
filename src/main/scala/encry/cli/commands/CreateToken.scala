f50040cedb7420f313638aa72f033253d3520f0b
lior <lior.liviev@mail.ru>
2019-10-30 14:07:44 +0300

    token creation

---
+++ src/main/scala/encry/cli/commands/CreateToken.scala
@@ -0,0 +1,55 @@
+package encry.cli.commands
+
+import akka.actor.ActorRef
+import akka.pattern._
+import akka.util.Timeout
+import encry.EncryApp._
+import encry.api.http.DataHolderForApi.{GetDataFromWallet, GetViewCreateKey}
+import encry.cli.{Ast, Response}
+import encry.modifiers.mempool.TransactionFactory
+import encry.settings.EncryAppSettings
+import encry.utils.NetworkTimeProvider
+import encry.view.mempool.MemoryPool.NewTransaction
+import org.encryfoundation.common.crypto.PrivateKey25519
+import org.encryfoundation.common.modifiers.mempool.transaction.EncryAddress.Address
+import org.encryfoundation.common.modifiers.mempool.transaction.{PubKeyLockedContract, Transaction}
+import org.encryfoundation.common.modifiers.state.box.{AssetBox, EncryBaseBox}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.Try
+
+
+// wallet createToken -fee=100 -amount=50000
+object CreateToken extends Command {
+
+  override def execute(args: Command.Args,
+                       settings: EncryAppSettings,
+                       dataHolder: ActorRef,
+                       nodeId: Array[Byte],
+                       networkTimeProvider: NetworkTimeProvider): Future[Option[Response]] = {
+    implicit val timeout: Timeout = Timeout(settings.restApi.timeout)
+    (dataHolder ?
+      GetDataFromWallet[Option[Transaction]] { wallet =>
+        Try {
+          val secret: PrivateKey25519 = wallet.accountManager.mandatoryAccount
+          val fee: Long               = args.requireArg[Ast.Num]("fee").i
+          val amount: Long            = args.requireArg[Ast.Num]("amount").i
+          val boxes: AssetBox         =     wallet.walletStorage
+            .getAllBoxes().collect { case ab: AssetBox => ab }.head
+          println(boxes + " boxes")
+          TransactionFactory.assetIssuingTransactionScratch(secret,
+            fee,
+            System.currentTimeMillis(),
+            IndexedSeq(boxes).map(_ -> None),
+            PubKeyLockedContract(wallet.accountManager.mandatoryAccount.publicImage.pubKeyBytes).contract,
+            amount)
+        }.toOption
+      }).flatMap {
+      case Some(tx: Transaction) =>
+        memoryPool ! NewTransaction(tx)
+        Future.successful(Some(Response(tx.toString)))
+      case _ => Future.successful(Some(Response("Operation failed. Malformed data.")))
+    }
+  }
+}
