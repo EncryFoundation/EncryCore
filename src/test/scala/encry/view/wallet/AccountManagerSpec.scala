b0037ecc46df6e86daea0f1dcd77d9514f4e1329
GRYE <g.yermolaev@encry.ru>
2019-10-31 13:01:54 +0300

    Changed tests

--- src/test/scala/encry/view/wallet/AccountManagerSpec.scala
+++ src/test/scala/encry/view/wallet/AccountManagerSpec.scala
@@ -1,8 +1,9 @@
 package encry.view.wallet
 
 import encry.crypto.encryption.AES
+import encry.utils.{FileHelper, Mnemonic}
 import encry.view.wallet.AccountManager.MetaInfoPrefix
-import io.iohk.iodb.{ByteArrayWrapper, Store}
+import io.iohk.iodb.{ByteArrayWrapper, LSMStore, Store}
 import org.encryfoundation.common.crypto.PrivateKey25519
 import org.encryfoundation.common.utils.Algos
 import org.scalatest.mockito.MockitoSugar
@@ -24,28 +25,45 @@ class AccountManagerSpec extends PropSpec with Matchers with MockitoSugar {
     PrivateKey25519(privateKey, publicKey)
   }
   val seed = Some("seeeeeeed")
+  val alsoSeed = Some("seeed")
 
   def accountKey(i: Byte): ByteArrayWrapper = ByteArrayWrapper(Array(MetaInfoPrefix, i) ++ Algos.hash("account"))
   def dataKey(i: Byte, pubBytes: PublicKey): ByteArrayWrapper = ByteArrayWrapper(Array(AccountManager.AccountPrefix, i) ++ pubBytes)
 
   property("Restoring existing accounts from storage") {
-    when(storageMock.get(accountKey(0.toByte))).thenReturn(Some(ByteArrayWrapper(accounts(0).publicKeyBytes)))
-    when(storageMock.get(accountKey(1.toByte))).thenReturn(Some(ByteArrayWrapper(accounts(1).publicKeyBytes)))
-    when(storageMock.get(accountKey(2.toByte))).thenReturn(Some(ByteArrayWrapper(accounts(2).publicKeyBytes)))
-    when(storageMock.get(accountKey(3.toByte))).thenReturn(None)
 
-    when(storageMock.get(dataKey(0, accounts(0).publicKeyBytes))).thenReturn(Some(ByteArrayWrapper(AES.encrypt(accounts(0).privKeyBytes, storagePassword))))
-    when(storageMock.get(dataKey(1, accounts(1).publicKeyBytes))).thenReturn(Some(ByteArrayWrapper(AES.encrypt(accounts(1).privKeyBytes, storagePassword))))
-    when(storageMock.get(dataKey(2, accounts(2).publicKeyBytes))).thenReturn(Some(ByteArrayWrapper(AES.encrypt(accounts(2).privKeyBytes, storagePassword))))
+    val dir = FileHelper.getRandomTempDir
+    val accountManagerStore: LSMStore = new LSMStore(dir, keepVersions = 0, keySize = 34)
 
-    val restored: Seq[AccountManager] = AccountManager.restoreAccounts(storageMock, storagePassword)
-    restored.zipWithIndex.foreach { case (am, i) =>
-      assert(
-        am.mandatoryAccount.privKeyBytes.sameElements(accounts(i).privKeyBytes) && am.mandatoryAccount.publicKeyBytes.sameElements(accounts(i).publicKeyBytes)
-      )
-    }
+    val accountManagerOne = AccountManager(accountManagerStore, storagePassword, seed, 0.toByte)
+    val accountManagerTwo = AccountManager(accountManagerStore, storagePassword, alsoSeed, 1.toByte)
 
-    verify(storageMock, times(7)).get(any[ByteArrayWrapper])
+    val numberOfAccountToCreate = 3
+
+    (1 to numberOfAccountToCreate).foreach(i => accountManagerOne.createAccount(Some(i.toString)))
+    (5 to 4 + numberOfAccountToCreate).foreach(i => accountManagerTwo.createAccount(Some(i.toString)))
+
+    val restoredAccounts = AccountManager.restoreAccounts(accountManagerStore, storagePassword)
+    assert(restoredAccounts(0).accounts.size == numberOfAccountToCreate + 1)
+    assert(restoredAccounts(1).accounts.size == numberOfAccountToCreate + 1)
+
+    restoredAccounts(0)
+      .accounts
+      .sortBy(a => Algos.encode(a.privKeyBytes))
+      .zip(accountManagerOne.accounts.sortBy(a => Algos.encode(a.privKeyBytes)))
+      .foreach { case (restored, created) =>
+        assert(restored.publicKeyBytes.sameElements(created.publicKeyBytes) && restored.privKeyBytes.sameElements(created.privKeyBytes))
+      }
+
+    restoredAccounts(1)
+      .accounts
+      .sortBy(a => Algos.encode(a.privKeyBytes))
+      .zip(accountManagerTwo.accounts.sortBy(a => Algos.encode(a.privKeyBytes)))
+      .foreach { case (restored, created) =>
+        assert(restored.publicKeyBytes.sameElements(created.publicKeyBytes) && restored.privKeyBytes.sameElements(created.privKeyBytes))
+      }
+
+    accountManagerStore.close()
   }
 
   property("Creating account from seed") {
@@ -72,5 +90,32 @@ class AccountManagerSpec extends PropSpec with Matchers with MockitoSugar {
     verify(storageMock, times(1))
       .update(any[Long], eq_(Iterable.empty[ByteArrayWrapper]), eq_(Iterable(keyToStore)))
   }
+
+  property("Correctly create, save and retrieve accounts") {
+      val dir = FileHelper.getRandomTempDir
+      val accountManagerStore: LSMStore = new LSMStore(dir, keepVersions = 0, keySize = 34)
+
+      val numberOfAccountToCreate = 3
+      val accountManager = AccountManager(accountManagerStore, storagePassword, seed, 0.toByte)
+
+      val (privateKeyFromSeed: PrivateKey, publicKeyFromSeed: PublicKey) = Curve25519.createKeyPair(Blake2b256(Mnemonic.seedFromMnemonic(seed.get)))
+
+      val createdAccounts = (1 to numberOfAccountToCreate).map { i =>
+        accountManager.createAccount(Some(i.toString))
+      }
+
+      val accountsFromAccountManager = accountManager.accounts
+
+      accountsFromAccountManager
+        .sortBy(a => Algos.encode(a.privKeyBytes))
+        .zip((PrivateKey25519(privateKeyFromSeed, publicKeyFromSeed) +: createdAccounts).sortBy(a => Algos.encode(a.privKeyBytes)))
+        .foreach { case (fromAM, created) =>
+          assert(fromAM.publicKeyBytes.sameElements(created.publicKeyBytes) && fromAM.privKeyBytes.sameElements(created.privKeyBytes))
+        }
+
+      assert(accountsFromAccountManager.size == numberOfAccountToCreate + 1)
+
+      accountManagerStore.close()
+    }
 
 }
