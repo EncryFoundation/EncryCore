d1ce78efdf5c4b658ab6b37e2540ba13408e4bb8
GRYE <g.yermolaev@encry.ru>
2019-11-01 16:52:59 +0300

    CLI command

---
+++ src/main/scala/encry/cli/commands/CreateAccountFromSeed.scala
@@ -0,0 +1,23 @@
+package encry.cli.commands
+
+import akka.util.Timeout
+import akka.pattern._
+import encry.cli.{Ast, Response}
+import encry.settings.EncryAppSettings
+import encry.EncryApp._
+import encry.view.NodeViewHolder.ReceivableMessages.{CreateAccountManagerFromSeed, NewAccountManager}
+
+import scala.concurrent.Future
+
+object CreateAccountFromSeed extends Command {
+  override def execute(args: Command.Args, settings: EncryAppSettings): Future[Option[Response]] = {
+    val seed: String = args.requireArg[Ast.Str]("seed").s
+    implicit val timeout: Timeout = Timeout(settings.restApi.timeout)
+    (nodeViewHolder ? CreateAccountManagerFromSeed(seed)).mapTo[NewAccountManager].map { s =>
+      s.option match {
+        case Some(wallet) => Some(Response(s"Created account manager #${wallet.accountManagers.map(_.number).max}"))
+        case None => Some(Response(s"Failed to create new account manager"))
+      }
+    }
+  }
+}
