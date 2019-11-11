78cc9e8a589b16c10916826f61a7ddb9d37c9aac
GRYE <g.yermolaev@encry.ru>
2019-11-11 16:52:19 +0300

    Additional tests for DM

--- src/test/scala/encry/network/DeliveryManagerTests/DeliveryManagerSpec.scala
+++ src/test/scala/encry/network/DeliveryManagerTests/DeliveryManagerSpec.scala
@@ -7,22 +7,25 @@ import akka.testkit.{TestActorRef, TestKit, TestProbe}
 import encry.consensus.HistoryConsensus.{Fork, Unknown}
 import encry.modifiers.InstanceFactory
 import encry.network.DeliveryManager.FullBlockChainIsSynced
-import encry.network.DeliveryManagerTests.DMUtils.{createPeer, generateBlocks}
+import encry.network.DeliveryManagerTests.DMUtils.{createPeer, generateBlocks, generateDummyHistory}
 import encry.network.NetworkController.ReceivableMessages.DataFromPeer
-import encry.network.NodeViewSynchronizer
-import encry.network.NodeViewSynchronizer.ReceivableMessages.{ChangedHistory, RequestFromLocal}
+import encry.network.{DeliveryManager, NodeViewSynchronizer}
+import encry.network.NodeViewSynchronizer.ReceivableMessages.{ChangedHistory, RequestFromLocal, UpdatedHistory}
 import encry.network.PeerConnectionHandler.{ConnectedPeer, Incoming}
 import encry.network.PeersKeeper.UpdatedPeersCollection
 import encry.network.PrioritiesCalculator.PeersPriorityStatus.PeersPriorityStatus.InitialPriority
 import encry.settings.TestNetSettings
 import encry.view.history.History
+import encry.view.mempool.MemoryPool.StopTransactionsValidation
 import org.encryfoundation.common.modifiers.history.{Block, Header}
-import org.encryfoundation.common.network.BasicMessagesRepo.{Handshake, InvNetworkMessage, RequestModifiersNetworkMessage, SyncInfoNetworkMessage}
+import org.encryfoundation.common.modifiers.mempool.transaction.Transaction
+import org.encryfoundation.common.network.BasicMessagesRepo.{Handshake, InvNetworkMessage, ModifiersNetworkMessage, RequestModifiersNetworkMessage, SyncInfoNetworkMessage}
 import org.encryfoundation.common.network.SyncInfo
+import org.encryfoundation.common.utils.TaggedTypes.ModifierId
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, Matchers, OneInstancePerTest, WordSpecLike}
 
-class RequestModifiersSpec extends TestKit(ActorSystem("RequestModifiersSpec"))
+class DeliveryManagerSpec extends TestKit(ActorSystem("RequestModifiersSpec"))
   with WordSpecLike
   with BeforeAndAfterAll
   with MockitoSugar
@@ -35,7 +38,7 @@ class RequestModifiersSpec extends TestKit(ActorSystem("RequestModifiersSpec"))
     TestKit.shutdownActorSystem(system)
   }
 
-  "NodeViewSynchronizer" should {
+  "DeliveryManager" should {
     "correctly request modifiers from forked node" in {
 
       val nvs: TestActorRef[NodeViewSynchronizer] =
@@ -83,6 +86,31 @@ class RequestModifiersSpec extends TestKit(ActorSystem("RequestModifiersSpec"))
       nvs ! RequestFromLocal(cp, Header.modifierTypeId, idsForLocalRequest)
 
       handler.expectMsg(RequestModifiersNetworkMessage(Header.modifierTypeId -> idsForLocalRequest))
+    }
+
+    "not process transactions if number of transactions in mempool exceeds limit" in {
+
+      val downloadedModifiersValidator = TestProbe()
+      val address = new InetSocketAddress("123.123.123.124", 9001)
+      val handler: TestProbe = TestProbe()
+      val cp: ConnectedPeer = ConnectedPeer(address, handler.ref, Incoming,
+        Handshake(protocolToBytes(testNetSettings.network.appVersion),
+          "123.123.123.124", Some(address), System.currentTimeMillis()))
+
+      val deliveryManager: TestActorRef[DeliveryManager] =
+        TestActorRef[DeliveryManager](DeliveryManager
+          .props(None, TestProbe().ref, TestProbe().ref, TestProbe().ref, TestProbe().ref, downloadedModifiersValidator.ref, settings))
+
+      val history: History = generateDummyHistory(settings)
+
+      deliveryManager ! UpdatedHistory(history)
+      deliveryManager ! StopTransactionsValidation
+
+      val txs: Map[ModifierId, Array[Byte]] = genValidPaymentTxs(100).groupBy(_.id).mapValues(_.head.bytes)
+      val msg = ModifiersNetworkMessage(Transaction.modifierTypeId, txs)
+
+      deliveryManager ! DataFromPeer(msg, cp)
+      downloadedModifiersValidator.expectNoMsg()
     }
   }
 
