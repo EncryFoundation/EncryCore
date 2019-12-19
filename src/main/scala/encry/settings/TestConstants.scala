359db95dc24a95f832503e2017fa112592ce8c76
timofey <gusev.t.s1@gmail.com>
2019-12-19 13:08:02 +0300

    constants is returned

--- src/main/scala/encry/settings/TestConstants.scala
+++ src/main/scala/encry/settings/TestConstants.scala
@@ -59,7 +59,7 @@ case object TestConstants extends Constants {
 
   val PreGenesisHeight: Height = Height @@ (GenesisHeight - 1)
 
-  val MaxRollbackDepth: Int = 5
+  val MaxRollbackDepth: Int = 100
 
   val MaxTimeDrift: Long = 2.hours.toMillis
 
