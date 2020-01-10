f96f3ade2a7e045f36670b4d8cf3551b1bf3c007
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2020-01-10 18:40:37 +0300

    add verification of payload size (#403)

--- src/main/scala/encry/modifiers/history/PayloadUtils.scala
+++ src/main/scala/encry/modifiers/history/PayloadUtils.scala
@@ -1,16 +1,17 @@
 package encry.modifiers.history
 
 import org.encryfoundation.common.modifiers.history.Payload
-
+import org.encryfoundation.common.utils.constants.TestNetConstants
 import org.encryfoundation.common.validation.{ModifierValidator, ValidationResult}
 
 object PayloadUtils {
 
-  //todo add payload size validation
   def syntacticallyValidity(payload: Payload, modifierIdSize: Int): ValidationResult = ModifierValidator.accumulateErrors
     .demand(payload.modifierTypeId == Payload.modifierTypeId,
       s"Modifier's type id should be ${Payload.modifierTypeId}")
     .demand(payload.headerId.size == modifierIdSize,
       s"Modifier's id should be $modifierIdSize bytes")
+    .demand(payload.bytes.length <= TestNetConstants.PayloadMaxSize,
+                 "Incorrect payload size.")
     .result
 }
