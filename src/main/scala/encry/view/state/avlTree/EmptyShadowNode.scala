9101d806564306f9ff5a476b48c8949c563a51d8
aleksandr <alexbromel@gmail.com>
2020-01-24 12:45:29 +0300

    fix hash calculating of EmptyShadow Node

--- src/main/scala/encry/view/state/avlTree/EmptyShadowNode.scala
+++ src/main/scala/encry/view/state/avlTree/EmptyShadowNode.scala
@@ -10,7 +10,7 @@ class EmptyShadowNode[K: Serializer: Monoid, V: Serializer: Monoid] extends Shad
   override val value: V = implicitly[Monoid[V]].empty
   override val height: Int = -1
   override val balance: Int = 0
-
+  override def hash: Array[Byte] = Array.emptyByteArray
   override def restoreFullNode(storage: VersionalStorage): Node[K, V] = EmptyNode[K, V]
 }
 
