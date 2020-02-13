42e757499df2a0a736c2983c579006d601c4fb75
aleksandr <alexbromel@gmail.com>
2020-02-13 16:00:21 +0300

    logger.info -> logger.debug in dm

--- src/main/scala/encry/network/DeliveryManager.scala
+++ src/main/scala/encry/network/DeliveryManager.scala
@@ -174,7 +174,7 @@ class DeliveryManager(influxRef: Option[ActorRef],
         if (typeId != Transaction.modifierTypeId) influxRef
           .foreach(ref => (0 to filteredModifiers.size).foreach(_ => ref ! SerializedModifierFromNetwork(typeId)))
         //todo check this logic
-        logger.info(s"Type of mod: ${typeId}. canProcessTransactions: ${canProcessTransactions}")
+        logger.debug(s"Type of mod: ${typeId}. canProcessTransactions: ${canProcessTransactions}")
         if ((typeId == Transaction.modifierTypeId && canProcessTransactions) || (typeId != Transaction.modifierTypeId))
           downloadedModifiersValidator ! ModifiersForValidating(remote, typeId, filteredModifiers)
 
