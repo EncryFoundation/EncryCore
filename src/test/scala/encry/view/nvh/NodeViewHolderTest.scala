8fa1a9761f1b2cccbc627e2a0cf546509762bdbe
aleksandr <alexbromel@gmail.com>
2020-02-19 15:40:26 +0300

    comment test

--- src/test/scala/encry/view/nvh/NodeViewHolderTest.scala
+++ src/test/scala/encry/view/nvh/NodeViewHolderTest.scala
@@ -4,39 +4,36 @@
 //
 //import akka.actor.ActorSystem
 //import akka.testkit.TestKit
+//import com.typesafe.scalalogging.StrictLogging
 //import encry.modifiers.InstanceFactory
 //import encry.settings.EncryAppSettings
-//import encry.storage.VersionalStorage
+//import encry.storage.RootNodesStorage
 //import encry.storage.VersionalStorage.{StorageKey, StorageValue, StorageVersion}
-//import encry.storage.iodb.versionalIODB.IODBWrapper
 //import encry.storage.levelDb.versionalLevelDB.{LevelDbFactory, VLDBWrapper, VersionalLevelDBCompanion}
 //import encry.utils.CoreTaggedTypes.VersionTag
 //import encry.utils.FileHelper
-//import encry.utils.implicits.UTXO._
-//import encry.utils.implicits.UTXO.combineAll
+//import encry.utils.implicits.UTXO.{combineAll, _}
 //import encry.view.NodeViewHolder.ReceivableMessages.ModifierFromRemote
-//import encry.view.state.UtxoState.{initialStateBoxes, logger}
+//import encry.view.state.UtxoState
+//import encry.view.state.UtxoState.initialStateBoxes
 //import encry.view.state.avlTree.AvlTree
-//import encry.view.state.{BoxHolder, UtxoState}
-//import io.iohk.iodb.LSMStore
-//import org.encryfoundation.common.modifiers.history.{Block, Header, Payload}
-//import org.encryfoundation.common.modifiers.state.box.AssetBox
-//import org.encryfoundation.common.utils.TaggedTypes.{Difficulty, Height}
 //import encry.view.state.avlTree.utils.implicits.Instances._
 //import org.encryfoundation.common.crypto.equihash.EquihashSolution
+//import org.encryfoundation.common.modifiers.history.{Block, Header, Payload}
 //import org.encryfoundation.common.modifiers.mempool.transaction.Transaction
+//import org.encryfoundation.common.modifiers.state.box.AssetBox
 //import org.encryfoundation.common.utils.Algos
+//import org.encryfoundation.common.utils.TaggedTypes.{Difficulty, Height}
 //import org.iq80.leveldb.Options
 //import org.scalatest.{BeforeAndAfterAll, Matchers, OneInstancePerTest, WordSpecLike}
 //import scorex.utils.Random
-//
-//import scala.util.{Random => R}
 //
 //class NodeViewHolderTest extends WordSpecLike
 //  with BeforeAndAfterAll
 //  with Matchers
 //  with InstanceFactory
-//  with OneInstancePerTest {
+//  with OneInstancePerTest
+//  with StrictLogging {
 //
 //  implicit val system: ActorSystem = ActorSystem("NodeViewHolderSpec")
 //
@@ -44,17 +41,24 @@
 //
 //  def genesis(stateDir: File, settings: EncryAppSettings): UtxoState = {
 //    //check kind of storage
-//    val storage =
-//        IODBWrapper(new LSMStore(stateDir, keepVersions = settings.constants.DefaultKeepVersions))
+//    val levelDBInit = LevelDbFactory.factory.open(FileHelper.getRandomTempDir, new Options)
+//    val storage = VLDBWrapper(VersionalLevelDBCompanion(levelDBInit, settings.levelDB.copy(keySize = 33), keySize = 33))
 //    storage.insert(
 //      StorageVersion @@ Array.fill(32)(0: Byte),
-//      initialStateBoxes.map(bx => (StorageKey !@@ bx.id, StorageValue @@ bx.bytes))
+//      initialStateBoxes.map(bx => (StorageKey !@@ AvlTree.elementKey(bx.id), StorageValue @@ bx.bytes))
 //    )
-//    UtxoState(AvlTree[StorageKey, StorageValue](storage), Height @@ 0, settings.constants)
+//    val rootsDir: File = FileHelper.getRandomTempDir
+//    rootsDir.mkdir()
+//    val rootStorage = {
+//      val levelDBInit = LevelDbFactory.factory.open(rootsDir, new Options)
+//      RootNodesStorage[StorageKey, StorageValue](levelDBInit, 3, rootsDir)
+//    }
+//
+//    UtxoState(AvlTree[StorageKey, StorageValue](storage, rootStorage), Height @@ 0, settings.constants, None)
 //  }
 //
 //  def generateGenesisBlockValidForState(state: UtxoState, tree: AvlTree[StorageKey, StorageValue]): (Block, AvlTree[StorageKey, StorageValue]) = {
-//    val txs = List(coinbaseTransaction(0))
+//    val txs = List(coinbaseTransaction(0, privKey.publicImage))
 //    val combinedStateChange: UtxoState.StateChange = combineAll(txs.map(UtxoState.tx2StateChange))
 //    val insertTimestart = System.currentTimeMillis()
 //    val newTree: AvlTree[StorageKey, StorageValue] = tree.insertAndDeleteMany(
@@ -92,9 +96,10 @@
 //          numOfOutputs = splitCoef
 //        )
 //        (boxes.tail, transactionsL :+ tx)
-//    }._2.filter(tx => tx.inputs.forall(input => tree.contains(StorageKey @@ input.boxId))) ++ Seq(coinbaseTransaction(prevBlock.header.height + 1))
+//    }._2.filter(tx => tx.inputs.forall(input => tree.contains(StorageKey @@ input.boxId))) ++ Seq(coinbaseTransaction(prevBlock.header.height + 1, privKey.publicImage))
 //    val combinedStateChange: UtxoState.StateChange = combineAll(transactions.toList.map(UtxoState.tx2StateChange))
 //    val insertTimestart = System.currentTimeMillis()
+//    val prevRootHash = tree.rootNode.hash
 //    val newTree: AvlTree[StorageKey, StorageValue] = tree.insertAndDeleteMany(
 //      StorageVersion @@ Random.randomBytes(),
 //      combinedStateChange.outputsToDb.toList,
@@ -112,6 +117,8 @@
 //      EquihashSolution(Seq(1, 3)),
 //      newTree.rootHash
 //    )
+//    logger.info(s"Creating block at height ${prevBlock.header.height + 1} with id: ${Algos.encode(header.id)}." +
+//      s" Prev root: ${Algos.encode(prevRootHash)}. New: ${Algos.encode(newTree.rootNode.hash)}")
 //    Block(header, Payload(header.id, transactions)) -> newTree
 //  }
 //
@@ -135,21 +142,22 @@
 //      val stateGenerationResults: (List[(Block, Block)], Block, UtxoState, IndexedSeq[AssetBox], List[VersionTag], AvlTree[StorageKey, StorageValue]) =
 //        (0 to 30).foldLeft(List.empty[(Block, Block)], genesisBlock, state, initialBoxes, List.empty[VersionTag], treeToGen) {
 //          case ((blocks, block, stateL, boxes, versions, prevTree), i) =>
-//            println(s"1. Before apply first block: ${Algos.encode(prevTree.storage.currentVersion)}")
+//            println(s"1. Before apply first block: ${Algos.encode(prevTree.avlStorage.currentVersion)}")
 //            val (nextBlockMainChain: Block, newTree) = generateNextBlockForStateWithSpendingAllPreviousBoxes(
 //              block,
 //              stateL,
 //              block.payload.txs.flatMap(_.newBoxes.map(_.asInstanceOf[AssetBox])).toIndexedSeq,
 //              tree = prevTree
 //            )
+//            logger.info("Apply fork block")
 //            val (nextBlockFork: Block, testTree) = generateNextBlockForStateWithSpendingAllPreviousBoxes(
 //              block,
 //              stateL,
 //              block.payload.txs.flatMap(_.newBoxes.map(_.asInstanceOf[AssetBox])).toIndexedSeq,
 //              addDiff = Difficulty @@ BigInt(100),
-//              tree = prevTree.rollbackTo(newTree.storage.versions.take(2).last).get
+//              tree = prevTree.rollbackTo(newTree.avlStorage.versions.drop(2).last, blocks.map(_._1)).get
 //            )
-//            val newAnotherOnceTree = testTree.rollbackTo(testTree.storage.versions.take(2).last).get
+//            val newAnotherOnceTree = testTree.rollbackTo(testTree.avlStorage.versions.take(2).last, List.empty).get
 //            val transactions: Seq[Transaction] = nextBlockMainChain.payload.txs
 //            val combinedStateChange: UtxoState.StateChange = combineAll(transactions.toList.map(UtxoState.tx2StateChange))
 //            val insertTimestart = System.currentTimeMillis()
@@ -159,7 +167,9 @@
 //              combinedStateChange.inputsToDb.toList,
 //              Height @@ (block.header.height + 1)
 //            )
-//            val stateN: UtxoState = stateL.applyModifier(nextBlockMainChain).right.get
+//            val stateRes = stateL.applyModifier(nextBlockMainChain)
+//            logger.info(s"RES: ${stateRes}")
+//            val stateN = stateRes.right.get
 //            (blocks :+ (nextBlockMainChain, nextBlockFork),
 //              nextBlockMainChain,
 //              stateN,
@@ -178,13 +188,13 @@
 ////        stateGenerationResults._1.last.payload.txs.flatMap(_.newBoxes.map(_.asInstanceOf[AssetBox])).toIndexedSeq,
 ////        tree = stateGenerationResults._6
 ////      )
-//
+//      logger.info("==========================Start apply to nvh==========================")
 //      println(s"genesisBlockHeader: ${genesisBlock.header}. Payload id: ${genesisBlock.payload.encodedId}")
 //      println(s"nextBlock: ${chain(1).header}")
 //
 //      chain.dropRight(1).foreach{block =>
 //        nvh ! ModifierFromRemote(block.header)
-//        //nvh ! ModifierFromRemote(block.payload)
+//        nvh ! ModifierFromRemote(block.payload)
 //      }
 //
 ////      chain.foreach{block =>
@@ -196,7 +206,7 @@
 //      println(s"Fork header: ${forkBlock.header}")
 //
 //      nvh ! ModifierFromRemote(forkBlock.header)
-//      //nvh ! ModifierFromRemote(forkBlock.payload)
+//      nvh ! ModifierFromRemote(forkBlock.payload)
 //
 //      val anotherBlock = chain.last
 //
