83f83f028441c4e0daca52d5d283f4b54d7993fe
aleksandr <alexbromel@gmail.com>
2020-03-20 11:55:46 +0300

    add awaitingViewActors: Receive to IntermediaryNVHView

--- src/main/scala/encry/nvg/IntermediaryNVHView.scala
+++ src/main/scala/encry/nvg/IntermediaryNVHView.scala
@@ -1,7 +1,10 @@
 package encry.nvg
 
 import akka.actor.{Actor, ActorRef, Props}
+import cats.syntax.option.none
 import encry.network.NetworkRouter.ModifierFromNetwork
+import encry.nvg.IntermediaryNVHView.IntermediaryNVHViewActions.{RegisterHistory, RegisterState}
+import encry.nvg.NodeViewHolder.NodeView
 import encry.view.history.History
 import encry.view.state.UtxoState
 
@@ -9,11 +12,33 @@ class IntermediaryNVHView() extends Actor {
 
   override def receive: Receive = ???
 
+  def awaitingViewActors(history: Option[ActorRef], state: Option[ActorRef]): Receive = {
+    case RegisterHistory if state.isEmpty =>
+      context.become(awaitingViewActors(Some(sender()), state), discardOld = true)
+    case RegisterHistory =>
+      context.become(viewReceive(sender(), state.get))
+    case RegisterState if history.isEmpty =>
+      context.become(awaitingViewActors(history, Some(sender())), discardOld = true)
+    case RegisterHistory =>
+      context.become(viewReceive(history.get, sender()))
+  }
+
   def viewReceive(history: ActorRef, state: ActorRef): Receive = {
     case ModifierFromNetwork(remote, typeId, modifierId, modifierBytes) => history ! ModifierFromNetwork
+  }
+
+  def restoreState(influxRef: Option[ActorRef] = none): Unit = {
+
   }
 }
 
 object IntermediaryNVHView {
+
+  sealed trait IntermediaryNVHViewActions
+  object IntermediaryNVHViewActions {
+    case object RegisterHistory extends IntermediaryNVHViewActions
+    case object RegisterState extends IntermediaryNVHViewActions
+  }
+
   def props(): Props = Props(new IntermediaryNVHView())
 }
