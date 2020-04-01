dbbd96451e857ae61ff74ecfc472528fe8b493b4
Timofey Gusev <gusev.t.s1@gmail.com>
2020-04-01 19:14:41 +0300

    logging removed

--- src/main/scala/encry/nvg/ModifiersCache.scala
+++ src/main/scala/encry/nvg/ModifiersCache.scala
@@ -110,9 +110,8 @@ object ModifiersCache extends StrictLogging {
               logger.info(s"exhaustiveSearch. Payload. ${p.encodedId}. ${Algos.encode(p.headerId)}. " +
                 s"isApplicableMod: $isApplicableMod.")
               isApplicableMod
-            case sm =>
+            case _ =>
               val isApplicableMod: Boolean = isApplicable(k)
-              logger.info(s"exhaustiveSearch. Something. ${sm.encodedId}. isApplicableMod: $isApplicableMod.")
               isApplicableMod
           }
       }).collect { case Some(v) => v._1 }
